a05dfb141764164cc9ac49c73bc3a5ec67740852
Benoit Cousson <bcousson@baylibre.com>
2014-10-23 12:40:35 +0800

    apps: Add ara/svc main application
    Testing Done: boot on BDB
    Bug Number: SW-209
    Review URL: https://reviews.leaflabs.com/r/166/
    Reviewed by: perry
    
    Signed-off-by: Benoit Cousson <bcousson@baylibre.com>

---
+++ apps/ara/svc/svc_main.c
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2014 Google, Inc.
+ * Author: Benoit Cousson <bcousson@baylibre.com>
+ */
+
+#include <nuttx/config.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void switch_control(int);
+
+#ifdef CONFIG_BUILD_KERNEL
+int main(int argc, FAR char *argv[])
+#else
+int svc_main(int argc, char *argv[])
+#endif
+{
+	int state = 0;
+
+	if (argc < 2)
+		return ERROR;
+
+	state = strtol(argv[1], NULL, 10);
+
+	switch_control(state);
+
+	return 0;
+}
