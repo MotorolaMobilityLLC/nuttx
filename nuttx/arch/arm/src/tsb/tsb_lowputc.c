7d73383b7b286eb4a90d8a82117a9434d7582f87
Benoit Cousson <bcousson@baylibre.com>
2014-10-30 23:39:30 +0800

    tsb: tsb_lowputc: Remove CG_BRIDGE definitions and use SCM API
    Remove the CG_BRIDGE definition from tsb_lowputc.c.
    Use the SCM APIs for clock, reset and pinshare management.
    
    Testing Done: Build and run without regression
    Bug Number: SW-205
    Review URL: https://reviews.leaflabs.com/r/217/
    Reviewed by: fab, pat
    
    Signed-off-by: Benoit Cousson <bcousson@baylibre.com>

--- nuttx/arch/arm/src/tsb/tsb_lowputc.c
+++ nuttx/arch/arm/src/tsb/tsb_lowputc.c
@@ -3,65 +3,7 @@
 #include "chip.h"
 #include "up_arch.h"
 
-#define TSB_SYSCTL_SOFTRESET0           (SYSCTL_BASE + 0x0)
-#define TSB_SYSCTL_SOFTRESETRELEASE0    (SYSCTL_BASE + 0x100)
-#define TSB_SYSCTL_CLOCKGATING0         (SYSCTL_BASE + 0x200)
-#define TSB_SYSCTL_CLOCKENABLE0         (SYSCTL_BASE + 0x300)
-#define TSB_SYSCTL_PINSHARE             (SYSCTL_BASE + 0x800)
-
-/* Clock bits for CLOCKGATING/CLOCKENABLE registers */
-#define TSB_SYSCTL_CLOCK_SDIOSYSCLK (1 << 9)
-#define TSB_SYSCTL_CLOCK_SDIOSDCLK  (1 << 10)
-#define TSB_SYSCTL_CLOCK_MBOXCLK    (1 << 12)
-#define TSB_SYSCTL_CLOCK_GDMACLK    (1 << 13)
-#define TSB_SYSCTL_CLOCK_TMRCLK     (1 << 14)
-#define TSB_SYSCTL_CLOCK_WDTCLK     (1 << 15)
-#define TSB_SYSCTL_CLOCK_TIMERCLK   (1 << 16)
-#define TSB_SYSCTL_CLOCK_PWMODPCLK  (1 << 17)
-#define TSB_SYSCTL_CLOCK_PWMODSCLK  (1 << 18)
-#define TSB_SYSCTL_CLOCK_I2CPCLK    (1 << 19)
-#define TSB_SYSCTL_CLOCK_I2CSCLK    (1 << 20)
-#define TSB_SYSCTL_CLOCK_UARTPCLK   (1 << 21)
-#define TSB_SYSCTL_CLOCK_UARTSCLK   (1 << 22)
-#define TSB_SYSCTL_CLOCK_SPIPCLK    (1 << 23)
-#define TSB_SYSCTL_CLOCK_SPISCLK    (1 << 24)
-#define TSB_SYSCTL_CLOCK_GPIOCLK    (1 << 25)
-
-#define DEFAULT_PERIPHERAL_CLOCK_BITS \
-    (TSB_SYSCTL_CLOCK_SDIOSYSCLK | \
-     TSB_SYSCTL_CLOCK_SDIOSDCLK  | \
-     TSB_SYSCTL_CLOCK_MBOXCLK    | \
-     TSB_SYSCTL_CLOCK_GDMACLK    | \
-     TSB_SYSCTL_CLOCK_TMRCLK     | \
-     TSB_SYSCTL_CLOCK_WDTCLK     | \
-     TSB_SYSCTL_CLOCK_TIMERCLK   | \
-     TSB_SYSCTL_CLOCK_PWMODPCLK  | \
-     TSB_SYSCTL_CLOCK_PWMODSCLK  | \
-     TSB_SYSCTL_CLOCK_I2CPCLK    | \
-     TSB_SYSCTL_CLOCK_I2CSCLK    | \
-     TSB_SYSCTL_CLOCK_UARTPCLK   | \
-     TSB_SYSCTL_CLOCK_UARTSCLK   | \
-     TSB_SYSCTL_CLOCK_SPIPCLK    | \
-     TSB_SYSCTL_CLOCK_SPISCLK    | \
-     TSB_SYSCTL_CLOCK_GPIOCLK)
-
-/* Reset_n bits for SOFTRESET/SOFTRELEASE */
-#define TSB_SOFTRESET_SDIOSYS (1 << 6)
-#define TSB_SOFTRESET_SDIOSD  (1 << 7)
-#define TSB_SOFTRESET_MBOX    (1 << 9)
-#define TSB_SOFTRESET_GDMA    (1 << 10)
-#define TSB_SOFTRESET_TMR     (1 << 11)
-#define TSB_SOFTRESET_WDT     (1 << 12)
-#define TSB_SOFTRESET_TIMER   (1 << 13)
-#define TSB_SOFTRESET_PWMODP  (1 << 14)
-#define TSB_SOFTRESET_PWMODS  (1 << 15)
-#define TSB_SOFTRESET_I2CP    (1 << 16)
-#define TSB_SOFTRESET_I2CS    (1 << 17)
-#define TSB_SOFTRESET_UARTP   (1 << 18)
-#define TSB_SOFTRESET_UARTS   (1 << 19)
-#define TSB_SOFTRESET_SPIP    (1 << 20)
-#define TSB_SOFTRESET_SPIS    (1 << 21)
-#define TSB_SOFTRESET_GPIO    (1 << 22)
+#include "tsb_scm.h"
 
 #define UART_RBR_THR_DLL    (UART_BASE + 0x0)
 #define UART_IER_DLH        (UART_BASE + 0x4)
@@ -84,13 +26,16 @@
 void tsb_lowsetup(void) {
     int i;
 
-    putreg32(DEFAULT_PERIPHERAL_CLOCK_BITS, TSB_SYSCTL_CLOCKGATING0);
-    putreg32(getreg32(TSB_SYSCTL_PINSHARE) | 3, TSB_SYSCTL_PINSHARE);
-    putreg32(TSB_SYSCTL_CLOCK_UARTPCLK | TSB_SYSCTL_CLOCK_UARTSCLK,
-             TSB_SYSCTL_CLOCKENABLE0);
-    putreg32(TSB_SOFTRESET_UARTP | TSB_SOFTRESET_UARTS, TSB_SYSCTL_SOFTRESET0);
-    putreg32(TSB_SOFTRESET_UARTP | TSB_SOFTRESET_UARTS,
-             TSB_SYSCTL_SOFTRESETRELEASE0);
+    /* enable UART RX/TX/RTS/CTS pins */
+    tsb_set_pinshare(TSB_PIN_UART_RXTX | TSB_PIN_UART_CTSRTS);
+
+    /* enable UART clocks */
+    tsb_clk_enable(TSB_CLK_UARTP);
+    tsb_clk_enable(TSB_CLK_UARTS);
+
+    /* reset UART module */
+    tsb_reset(TSB_RST_UARTP);
+    tsb_reset(TSB_RST_UARTS);
 
     /*
      * The controller requires "several cycles" after reset to stabilize before
--- nuttx/arch/arm/src/tsb/tsb_start.c
+++ nuttx/arch/arm/src/tsb/tsb_start.c
@@ -4,6 +4,7 @@
 #include "up_arch.h"
 #include "up_internal.h"
 
+#include "tsb_scm.h"
 #include "tsb_lowputc.h"
 
 #ifdef DEBUG_EARLY_BOOT
@@ -14,6 +15,9 @@
 
 void __start(void) {
     uint32_t *dest;
+
+    /* Configure clocks */
+    tsb_clk_init();
 
     /* Configure the UART so we can get debug output as soon as possible */
     tsb_lowsetup();
