ca84f2457e6ae4613dbfdd7c81a8ab4867f7b40d
Jean Pihet <jean.pihet@newoldbits.com>
2014-11-13 11:28:23 +0100

    SVC: Add I2C feature
    Used to communicate with the switch and the IO Expanders.
    
    Testing Done: Boot on BDB
    Bug Number: SW-209
    Review URL: https://reviews.leaflabs.com/r/168/
    Reviewed by: perry
    
    Signed-off-by: Jean Pihet <jean.pihet@newoldbits.com>

---
+++ nuttx/configs/bdb/svc/src/up_i2c.c
@@ -0,0 +1,94 @@
+/****************************************************************************
+ * configs/bdb/svc/src/up_i2c.c
+ * BDB/SVC I2C support, used for communication with the switch and the
+ *  IO Expanders
+ *
+ * Copyright (C) 2014 Google, Inc.
+ *
+ ****************************************************************************/
+#include <nuttx/config.h>
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <debug.h>
+#include <time.h>
+#include <unistd.h>
+#include <errno.h>
+
+#include <nuttx/i2c.h>
+
+#include <arch/board/board.h>
+
+#include "chip.h"
+#include "up_arch.h"
+#include "up_internal.h"
+#include "up_i2c.h"
+#include "stm32.h"
+
+#include "bdb-internal.h"
+
+/* Communication instance struct */
+FAR struct i2c_dev_s *sw_exp_dev;
+
+
+/*
+ * Initialize the communication from SVC
+ * Allocates internal structs etc. To be done once.
+ */
+inline int i2c_init_comm(uint8_t bus)
+{
+    /* Initialize I2C internal structs */
+    sw_exp_dev = up_i2cinitialize(bus);
+    if (!sw_exp_dev) {
+        printk("%s(): Failed to get bus %d\n", __func__, I2C_SW_BUS);
+        return ERROR;
+    }
+
+    return 0;
+}
+
+/*
+ * Select the destination device to communicate with.
+ * I2C requires a bus address.
+ */
+inline void i2c_select_device(uint8_t addr)
+{
+    /* Write the register address followed by the data (no RESTART) */
+    I2C_SETADDRESS(sw_exp_dev, addr, 7);
+}
+
+/* Send data from SVC to switch */
+inline int i2c_switch_send_msg(uint8_t *buf, unsigned int size)
+{
+    int ret;
+
+    //dbg_print_buf(buf, size);
+
+    i2c_select_device(I2C_ADDR_SWITCH);
+
+    ret = I2C_WRITE(sw_exp_dev, buf, size);
+    if (ret) {
+        printk("%s(): Error %d\n", __func__, ret);
+        return ERROR;
+    }
+
+    return 0;
+}
+
+/* Receive data from switch to SVC */
+inline int i2c_switch_receive_msg(uint8_t *buf, unsigned int size)
+{
+    int ret;
+
+    i2c_select_device(I2C_ADDR_SWITCH);
+
+    ret = I2C_READ(sw_exp_dev, buf, size);
+    if (ret) {
+        printk("%s(): Error %d\n", __func__, ret);
+        return ERROR;
+    }
+
+    //dbg_print_buf(buf, size);
+
+    return 0;
+}
---
+++ nuttx/configs/bdb/svc/src/up_i2c.h
@@ -0,0 +1,30 @@
+/****************************************************************************
+ * configs/bdb/svc/src/up_i2c.h
+ * BDB/SVC I2C support, used for communication with the switch and the
+ *  IO Expanders
+ *
+ * Copyright (C) 2014 Google, Inc.
+ *
+ ****************************************************************************/
+#ifndef __CONFIGS_BDB_INCLUDE_UP_I2C_H
+#define __CONFIGS_BDB_INCLUDE_UP_I2C_H
+
+/* STM32 bus to the switch and IO expanders */
+#define I2C_SW_BUS                  2
+
+/*
+ * I2C2 bus addresses on BDB
+ */
+#define I2C_ADDR_IOEXP_U90          0x20    /* IO Expander U90 */
+#define I2C_ADDR_IOEXP_U96          0x22    /* IO Expander U96 */
+#define I2C_ADDR_IOEXP_U135         0x23    /* IO Expander U135 */
+#define I2C_ADDR_SWITCH             0x44    /* Unipro switch */
+/* I2C flags */
+#define I2C_NOFLAGS                 0x00    /* No specific flags */
+
+extern inline int i2c_init_comm(uint8_t bus);
+extern inline void i2c_select_device(uint8_t addr);
+extern inline int i2c_switch_send_msg(uint8_t *buf, unsigned int size);
+extern inline int i2c_switch_receive_msg(uint8_t *buf, unsigned int size);
+
+#endif // __CONFIGS_BDB_INCLUDE_UP_I2C_H
