bef91f0d4bedd19ad84f85c17ed788b2831bddf4
Perry Hung <perry@leaflabs.com>
2014-11-20 23:29:12 -0500

    tsb: unipro: unipro controller driver core
    This is a first pass at a unipro stack for the Toshiba bridge chips.
    
    The bridge chips are operated in mode 1, a bizarre mode where the sender
    gets to pick the destination address of the packet. This will allow us
    to implement flow control which the hardware does not provide us.
    
    Users can register their driver with the core on a per-cport basis.
    Registered drivers can send data through a cport, or be notified when a
    packet is received on a cport. These callbacks are received in irq
    context.
    
    This patch does not implement flow control, so be careful not to overrun
    your receiver.
    
    Testing Done: send unipro packets back and forth, check that they arrive
        correctly and handlers are called
    Bug Number: SW-106
    Review URL: https://reviews.leaflabs.com/r/190/
    Reviewed by: mgreer

---
+++ nuttx/arch/arm/include/tsb/unipro.h
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2014 Google, Inc.
+ * Google Confidential/Restricted
+ * @author: Perry Hung
+ */
+
+#ifndef _UNIPRO_H_
+#define _UNIPRO_H_
+
+#include <stdlib.h>
+
+#define CPORT_MAX                   (32)
+#define CPORT_BUF_SIZE              (1024)
+
+struct unipro_driver {
+    const char name[32];
+    int (*rx_handler)(unsigned int cportid,  // Called in irq context
+                      void *data,
+                      size_t len);
+};
+
+void unipro_init(void);
+void unipro_info(void);
+int unipro_send(unsigned int cportid, const void *buf, size_t len);
+int unipro_attr_read(uint16_t attr,
+                     uint32_t *val,
+                     uint16_t selector,
+                     int peer,
+                     uint32_t *result_code);
+int unipro_driver_register(struct unipro_driver *drv, unsigned int cportid);
+
+#endif
