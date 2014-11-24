a87c0016abdb42b8dba7c55746feb0ae64f07617
Benoit Cousson <bcousson@baylibre.com>
2014-11-24 16:43:19 +0100

    tsb: tsb_i2c: Add driver for the I2C controller
    Add support for the DesignWare I2C IP included inside the APBridge.
    
    Testing Done: Tested with GPIO expander and Toshiba switch
    Bug Number: SW-202
    Review URL: https://reviews.leaflabs.com/r/219/
    Reviewed by: marti, pat, fabo
    
    Signed-off-by: Benoit Cousson <bcousson@baylibre.com>

---
+++ nuttx/arch/arm/src/tsb/tsb_i2c.c
@@ -0,0 +1,626 @@
+#include <nuttx/config.h>
+
+#include <errno.h>
+#include <debug.h>
+
+#include <nuttx/arch.h>
+#include <nuttx/wdog.h>
+#include <nuttx/i2c.h>
+
+#include <arch/irq.h>
+
+#include "up_arch.h"
+
+#include "chip.h"
+#include "tsb_scm.h"
+#include "tsb_i2c.h"
+
+
+/* disable the verbose debug output */
+#undef lldbg
+#define lldbg(x...)
+
+static struct i2c_dev_s g_dev;        /* Generic Nuttx I2C device */
+static struct i2c_msg_s *g_msgs;      /* Generic messages array */
+static sem_t            g_mutex;      /* Only one thread can access at a time */
+static sem_t            g_wait;       /* Wait for state machine completion */
+static WDOG_ID          g_timeout;    /* Watchdog to timeout when bus hung */
+
+static unsigned int     g_tx_index;
+static unsigned int     g_tx_length;
+static uint8_t          *g_tx_buffer;
+
+static unsigned int     g_rx_index;
+static unsigned int     g_rx_length;
+static uint8_t          *g_rx_buffer;
+
+static unsigned int     g_msgs_count;
+static int              g_cmd_err;
+static int              g_msg_err;
+static unsigned int     g_status;
+static uint32_t         g_abort_source;
+static unsigned int     g_rx_outstanding;
+
+
+/* I2C controller configuration */
+#define TSB_I2C_CONFIG (TSB_I2C_CON_RESTART_EN | \
+                        TSB_I2C_CON_MASTER | \
+                        TSB_I2C_CON_SLAVE_DISABLE | \
+                        TSB_I2C_CON_SPEED_FAST)
+
+#define TSB_I2C_TX_FIFO_DEPTH   8
+#define TSB_I2C_RX_FIFO_DEPTH   8
+
+/* IRQs handle by the driver */
+#define TSB_I2C_INTR_DEFAULT_MASK (TSB_I2C_INTR_RX_FULL | \
+                                   TSB_I2C_INTR_TX_EMPTY | \
+                                   TSB_I2C_INTR_TX_ABRT | \
+                                   TSB_I2C_INTR_STOP_DET)
+
+#define TIMEOUT                     20              /* 20 ms */
+#define TSB_I2C_TIMEOUT  ((1000 * CLK_TCK) / 1000)  /* 1000 ms */
+
+
+static int i2c_interrupt(int irq, void *context);
+static void i2c_timeout(int argc, uint32_t arg, ...);
+
+
+static uint32_t i2c_read(int offset)
+{
+    return getreg32(I2C_BASE + offset);
+}
+
+static void i2c_write(int offset, uint32_t b)
+{
+    putreg32(b, I2C_BASE + offset);
+}
+
+static void tsb_i2c_clear_int(void)
+{
+    i2c_read(TSB_I2C_CLR_INTR);
+}
+
+static void tsb_i2c_disable_int(void)
+{
+    i2c_write(TSB_I2C_INTR_MASK, 0);
+}
+
+static void i2c_set_enable(int enable)
+{
+    int i;
+
+    for (i = 0; i < 50; i++) {
+        i2c_write(TSB_I2C_ENABLE, enable);
+
+        if ((i2c_read(TSB_I2C_ENABLE_STATUS) & 0x1) == enable)
+            return;
+
+        usleep(25);
+    }
+
+    lldbg("timeout!");
+}
+
+/* Enable the controller */
+static void tsb_i2c_enable(void)
+{
+    i2c_set_enable(1);
+}
+
+/* Disable the controller */
+static void tsb_i2c_disable(void)
+{
+    i2c_set_enable(0);
+
+    tsb_i2c_disable_int();
+    tsb_i2c_clear_int();
+}
+
+
+/**
+ * Initialize the TSB I2 controller
+ */
+static void tsb_i2c_init(void)
+{
+    /* Disable the adapter */
+    tsb_i2c_disable();
+
+    /* Set timings for Standard and Fast Speed mode */
+    i2c_write(TSB_I2C_SS_SCL_HCNT, 28);
+    i2c_write(TSB_I2C_SS_SCL_LCNT, 52);
+    i2c_write(TSB_I2C_FS_SCL_HCNT, 47);
+    i2c_write(TSB_I2C_FS_SCL_LCNT, 65);
+
+    /* Configure Tx/Rx FIFO threshold levels */
+    i2c_write(TSB_I2C_TX_TL, TSB_I2C_TX_FIFO_DEPTH - 1);
+    i2c_write(TSB_I2C_RX_TL, 0);
+
+    /* configure the i2c master */
+    i2c_write(TSB_I2C_CON, TSB_I2C_CONFIG);
+}
+
+static int tsb_i2c_wait_bus_ready(void)
+{
+    int timeout = TIMEOUT;
+
+    while (i2c_read(TSB_I2C_STATUS) & TSB_I2C_STATUS_ACTIVITY) {
+        if (timeout <= 0) {
+            lldbg("timeout\n");
+            return -ETIMEDOUT;
+        }
+        timeout--;
+        usleep(1000);
+    }
+
+    return 0;
+}
+
+static void tsb_i2c_start_transfer(void)
+{
+    lldbg("\n");
+
+    /* Disable the adapter */
+    tsb_i2c_disable();
+
+    /* write target address */
+    i2c_write(TSB_I2C_TAR, g_msgs[g_tx_index].addr);
+
+    /* Disable the interrupts */
+    tsb_i2c_disable_int();
+
+    /* Enable the adapter */
+    tsb_i2c_enable();
+
+    /* Clear interrupts */
+    tsb_i2c_clear_int();
+
+    /* Enable interrupts */
+    i2c_write(TSB_I2C_INTR_MASK, TSB_I2C_INTR_DEFAULT_MASK);
+}
+
+/**
+ * Internal function that handles the read or write transfer
+ * It is called from the IRQ handler.
+ */
+static void tsb_i2c_transfer_msg(void)
+{
+    uint32_t intr_mask;
+    uint32_t addr = g_msgs[g_tx_index].addr;
+    uint8_t *buffer = g_tx_buffer;
+    uint32_t length = g_tx_length;
+
+    bool need_restart = false;
+
+    int tx_avail;
+    int rx_avail;
+
+    lldbg("tx_index %d\n", g_tx_index);
+
+    /* loop over the i2c message array */
+    for (; g_tx_index < g_msgs_count; g_tx_index++) {
+
+        if (g_msgs[g_tx_index].addr != addr) {
+            lldbg("invalid target address\n");
+            g_msg_err = -EINVAL;
+            break;
+        }
+
+        if (g_msgs[g_tx_index].length == 0) {
+            lldbg("invalid message length\n");
+            g_msg_err = -EINVAL;
+            break;
+        }
+
+        if (!(g_status & TSB_I2C_STATUS_WRITE_IN_PROGRESS)) {
+            /* init a new msg transfer */
+            buffer = g_msgs[g_tx_index].buffer;
+            length = g_msgs[g_tx_index].length;
+
+            /* force a restart between messages */
+            if (g_tx_index > 0)
+                need_restart = true;
+        }
+
+        /* Get the amount of free space in the internal buffer */
+        tx_avail = TSB_I2C_TX_FIFO_DEPTH - i2c_read(TSB_I2C_TXFLR);
+        rx_avail = TSB_I2C_RX_FIFO_DEPTH - i2c_read(TSB_I2C_RXFLR);
+
+        /* loop until one of the fifo is full or buffer is consumed */
+        while (length > 0 && tx_avail > 0 && rx_avail > 0) {
+            uint32_t cmd = 0;
+
+            if (g_tx_index == g_msgs_count - 1 && length == 1) {
+                /* Last msg, issue a STOP */
+                cmd |= (1 << 9);
+                lldbg("STOP\n");
+            }
+
+            if (need_restart) {
+                cmd |= (1 << 10); /* RESTART */
+                need_restart = false;
+                lldbg("RESTART\n");
+            }
+
+            if (g_msgs[g_tx_index].flags & I2C_M_READ) {
+                if (rx_avail - g_rx_outstanding <= 0)
+                    break;
+
+                i2c_write(TSB_I2C_DATA_CMD, cmd | 1 << 8); /* READ */
+                lldbg("READ\n");
+
+                rx_avail--;
+                g_rx_outstanding++;
+            } else {
+                i2c_write(TSB_I2C_DATA_CMD, cmd | *buffer++);
+                lldbg("WRITE\n");
+            }
+
+            tx_avail--;
+            length--;
+        }
+
+        g_tx_buffer = buffer;
+        g_tx_length = length;
+
+        if (length > 0) {
+            g_status |= TSB_I2C_STATUS_WRITE_IN_PROGRESS;
+            break;
+        } else {
+            g_status &= ~TSB_I2C_STATUS_WRITE_IN_PROGRESS;
+        }
+    }
+
+    intr_mask = TSB_I2C_INTR_DEFAULT_MASK;
+
+    /* No more data to write. Stop the TX IRQ */
+    if (g_tx_index == g_msgs_count)
+        intr_mask &= ~TSB_I2C_INTR_TX_EMPTY;
+
+    /* In case of error, mask all the IRQs */
+    if (g_msg_err)
+        intr_mask = 0;
+
+    i2c_write(TSB_I2C_INTR_MASK, intr_mask);
+}
+
+static void tsb_i2c_read(void)
+{
+    int rx_valid;
+
+    lldbg("rx_index %d\n", g_rx_index);
+
+    for (; g_rx_index < g_msgs_count; g_rx_index++) {
+        uint32_t len;
+        uint8_t *buffer;
+
+        if (!(g_msgs[g_rx_index].flags & I2C_M_READ))
+            continue;
+
+        if (!(g_status & TSB_I2C_STATUS_READ_IN_PROGRESS)) {
+            len = g_msgs[g_rx_index].length;
+            buffer = g_msgs[g_rx_index].buffer;
+        } else {
+            len = g_rx_length;
+            buffer = g_rx_buffer;
+        }
+
+        rx_valid = i2c_read(TSB_I2C_RXFLR);
+
+        for (; len > 0 && rx_valid > 0; len--, rx_valid--) {
+            *buffer++ = i2c_read(TSB_I2C_DATA_CMD);
+            g_rx_outstanding--;
+        }
+
+        if (len > 0) {
+            /* start the read process */
+            g_status |= TSB_I2C_STATUS_READ_IN_PROGRESS;
+            g_rx_length = len;
+            g_rx_buffer = buffer;
+
+            return;
+        } else {
+            g_status &= ~TSB_I2C_STATUS_READ_IN_PROGRESS;
+        }
+    }
+}
+
+static int tsb_i2c_handle_tx_abort(void)
+{
+    unsigned long abort_source = g_abort_source;
+
+    lldbg("%s: 0x%x\n", __func__, abort_source);
+
+    if (abort_source & TSB_I2C_TX_ABRT_NOACK) {
+        lldbg("%s: TSB_I2C_TX_ABRT_NOACK 0x%x\n", __func__, abort_source);
+        return -EREMOTEIO;
+    }
+
+    if (abort_source & TSB_I2C_TX_ARB_LOST)
+        return -EAGAIN;
+    else if (abort_source & TSB_I2C_TX_ABRT_GCALL_READ)
+        return -EINVAL; /* wrong g_msgs[] data */
+    else
+        return -EIO;
+}
+
+/* Perform a sequence of I2C transfers */
+static int up_i2c_transfer(struct i2c_dev_s *idev, struct i2c_msg_s *msgs, int num)
+{
+    int ret;
+    irqstate_t flags;
+
+    lldbg("msgs: %d\n", num);
+
+    sem_wait(&g_mutex);
+    flags = irqsave();
+
+    g_msgs = msgs;
+    g_msgs_count = num;
+    g_tx_index = 0;
+    g_rx_index = 0;
+    g_rx_outstanding = 0;
+
+    g_cmd_err = 0;
+    g_msg_err = 0;
+    g_status = TSB_I2C_STATUS_IDLE;
+    g_abort_source = 0;
+
+    ret = tsb_i2c_wait_bus_ready();
+    if (ret < 0)
+        goto done;
+
+    /*
+     * start a watchdog to timeout the transfer if
+     * the bus is locked up...
+     */
+    wd_start(g_timeout, TSB_I2C_TIMEOUT, i2c_timeout, 1, 0);
+
+    /* start the transfers */
+    tsb_i2c_start_transfer();
+
+    sem_wait(&g_wait);
+
+    wd_cancel(g_timeout);
+
+    if (g_status == TSB_I2C_STATUS_TIMEOUT) {
+        lldbg("controller timed out\n");
+
+        /* Re-init the adapter */
+        tsb_i2c_init();
+        ret = -ETIMEDOUT;
+        goto done;
+    }
+
+    tsb_i2c_disable();
+
+    if (g_msg_err) {
+        ret = g_msg_err;
+        lldbg("error msg_err %x\n", g_msg_err);
+        goto done;
+    }
+
+    if (!g_cmd_err) {
+        ret = 0;
+        lldbg("no error %d\n", num);
+        goto done;
+    }
+
+    /* Handle abort errors */
+    if (g_cmd_err == TSB_I2C_ERR_TX_ABRT) {
+        ret = tsb_i2c_handle_tx_abort();
+        goto done;
+    }
+
+    /* default error code */
+    ret = -EIO;
+    lldbg("unknown error %x\n", ret);
+
+done:
+    irqrestore(flags);
+    sem_post(&g_mutex);
+
+    return ret;
+}
+
+static uint32_t tsb_i2c_read_clear_intrbits(void)
+{
+    uint32_t stat = i2c_read(TSB_I2C_INTR_STAT);
+
+    if (stat & TSB_I2C_INTR_RX_UNDER)
+        i2c_read(TSB_I2C_CLR_RX_UNDER);
+    if (stat & TSB_I2C_INTR_RX_OVER)
+        i2c_read(TSB_I2C_CLR_RX_OVER);
+    if (stat & TSB_I2C_INTR_TX_OVER)
+        i2c_read(TSB_I2C_CLR_TX_OVER);
+    if (stat & TSB_I2C_INTR_RD_REQ)
+        i2c_read(TSB_I2C_CLR_RD_REQ);
+    if (stat & TSB_I2C_INTR_TX_ABRT) {
+        /* IC_TX_ABRT_SOURCE reg is cleared upon read, store it */
+        g_abort_source = i2c_read(TSB_I2C_TX_ABRT_SOURCE);
+        i2c_read(TSB_I2C_CLR_TX_ABRT);
+    }
+    if (stat & TSB_I2C_INTR_RX_DONE)
+        i2c_read(TSB_I2C_CLR_RX_DONE);
+    if (stat & TSB_I2C_INTR_ACTIVITY)
+        i2c_read(TSB_I2C_CLR_ACTIVITY);
+    if (stat & TSB_I2C_INTR_STOP_DET)
+        i2c_read(TSB_I2C_CLR_STOP_DET);
+    if (stat & TSB_I2C_INTR_START_DET)
+        i2c_read(TSB_I2C_CLR_START_DET);
+    if (stat & TSB_I2C_INTR_GEN_CALL)
+        i2c_read(TSB_I2C_CLR_GEN_CALL);
+
+    return stat;
+}
+
+/* I2C interrupt service routine */
+static int i2c_interrupt(int irq, void *context)
+{
+    uint32_t stat, enabled;
+
+    enabled = i2c_read(TSB_I2C_ENABLE);
+    stat = i2c_read(TSB_I2C_RAW_INTR_STAT);
+
+    lldbg("enabled=0x%x stat=0x%x\n", enabled, stat);
+
+    if (!enabled || !(stat & ~TSB_I2C_INTR_ACTIVITY))
+        return -1;
+
+    stat = tsb_i2c_read_clear_intrbits();
+
+    if (stat & TSB_I2C_INTR_TX_ABRT) {
+        lldbg("abort\n");
+        g_cmd_err |= TSB_I2C_ERR_TX_ABRT;
+        g_status = TSB_I2C_STATUS_IDLE;
+
+        tsb_i2c_disable_int();
+        goto tx_aborted;
+    }
+
+    if (stat & TSB_I2C_INTR_RX_FULL)
+        tsb_i2c_read();
+
+    if (stat & TSB_I2C_INTR_TX_EMPTY)
+        tsb_i2c_transfer_msg();
+
+tx_aborted:
+    if (stat & TSB_I2C_INTR_TX_ABRT)
+        lldbg("aborted %x %x\n", stat, g_abort_source);
+
+    if ((stat & (TSB_I2C_INTR_TX_ABRT | TSB_I2C_INTR_STOP_DET)) || g_msg_err) {
+        lldbg("release sem\n");
+        sem_post(&g_wait);
+    }
+
+    return 0;
+}
+
+/**
+ * Watchdog handler for timeout of I2C operation
+ */
+static void i2c_timeout(int argc, uint32_t arg, ...)
+{
+    lldbg("\n");
+
+    irqstate_t flags = irqsave();
+
+    if (g_status != TSB_I2C_STATUS_IDLE)
+    {
+        lldbg("finished\n");
+        /* Mark the transfer as finished */
+        g_status = TSB_I2C_STATUS_TIMEOUT;
+        sem_post(&g_wait);
+    }
+
+    irqrestore(flags);
+}
+
+struct i2c_ops_s dev_i2c_ops;
+
+/**
+ * Initialise an I2C device
+ */
+struct i2c_dev_s *up_i2cinitialize(int port)
+{
+    lldbg("I2C port %d\n", port);
+
+    /* Only one I2C port on TSB */
+    if (port > 0)
+        return NULL;
+
+    sem_init(&g_mutex, 0, 1);
+    sem_init(&g_wait, 0, 0);
+
+    /* enable I2C pins */
+    tsb_clr_pinshare(TSB_PIN_SDIO);
+
+    /* enable I2C clocks */
+    tsb_clk_enable(TSB_CLK_I2CP);
+    tsb_clk_enable(TSB_CLK_I2CS);
+
+    /* reset I2C module */
+    tsb_reset(TSB_RST_I2CP);
+    tsb_reset(TSB_RST_I2CS);
+
+    /* Initialize the I2C controller */
+    tsb_i2c_init();
+
+    /* Allocate a watchdog timer */
+    g_timeout = wd_create();
+    DEBUGASSERT(g_timeout != 0);
+
+    /* Attach Interrupt Handler */
+    irq_attach(TSB_IRQ_I2C, i2c_interrupt);
+
+    /* Enable Interrupt Handler */
+    up_enable_irq(TSB_IRQ_I2C);
+
+    /* Install our operations */
+    g_dev.ops = &dev_i2c_ops;
+
+    return &g_dev;
+}
+
+/**
+ * Uninitialise an I2C device
+ */
+int up_i2cuninitialize(struct i2c_dev_s *dev)
+{
+    lldbg("\n");
+
+    /* Detach Interrupt Handler */
+    irq_detach(TSB_IRQ_I2C);
+
+    wd_delete(g_timeout);
+
+    return 0;
+}
+
+/**
+ * Set the I2C bus frequency (not implemented yet)
+ */
+static uint32_t up_i2c_setfrequency(struct i2c_dev_s *dev, uint32_t frequency)
+{
+    lldbg("%d\n", frequency);
+
+    return frequency;
+}
+
+/**
+ * Set the I2C slave address for a subsequent read/write
+ */
+static int up_i2c_setaddress(struct i2c_dev_s *dev, int addr, int nbits)
+{
+    return -ENOSYS;
+}
+
+/**
+ * Send a block of data on I2C using the previously selected I2C
+ * frequency and slave address.
+ */
+static int up_i2c_write(struct i2c_dev_s *dev, const uint8_t *buffer, int buflen)
+{
+    return -ENOSYS;
+}
+
+/**
+ * Receive a block of data on I2C using the previously selected I2C
+ * frequency and slave address.
+ */
+static int up_i2c_read(struct i2c_dev_s *dev, uint8_t *buffer, int buflen)
+{
+    return -ENOSYS;
+}
+
+/**
+ * Nuttx i2c device operations structure
+ */
+struct i2c_ops_s dev_i2c_ops = {
+    .setfrequency = up_i2c_setfrequency,
+    .setaddress   = up_i2c_setaddress,
+    .write        = up_i2c_write,
+    .read         = up_i2c_read,
+#ifdef CONFIG_I2C_TRANSFER
+    .transfer     = up_i2c_transfer
+#endif
+};
---
+++ nuttx/arch/arm/src/tsb/tsb_i2c.h
@@ -0,0 +1,101 @@
+#ifndef __ARCH_ARM_SRC_TSB_TSB_I2C_H
+#define __ARCH_ARM_SRC_TSB_TSB_I2C_H
+
+/* Registers offset */
+#define TSB_I2C_CON               0x0
+#define TSB_I2C_TAR               0x4
+#define TSB_I2C_DATA_CMD          0x10
+#define TSB_I2C_SS_SCL_HCNT       0x14
+#define TSB_I2C_SS_SCL_LCNT       0x18
+#define TSB_I2C_FS_SCL_HCNT       0x1c
+#define TSB_I2C_FS_SCL_LCNT       0x20
+#define TSB_I2C_INTR_STAT         0x2c
+#define TSB_I2C_INTR_MASK         0x30
+#define TSB_I2C_RAW_INTR_STAT     0x34
+#define TSB_I2C_RX_TL             0x38
+#define TSB_I2C_TX_TL             0x3c
+#define TSB_I2C_CLR_INTR          0x40
+#define TSB_I2C_CLR_RX_UNDER      0x44
+#define TSB_I2C_CLR_RX_OVER       0x48
+#define TSB_I2C_CLR_TX_OVER       0x4c
+#define TSB_I2C_CLR_RD_REQ        0x50
+#define TSB_I2C_CLR_TX_ABRT       0x54
+#define TSB_I2C_CLR_RX_DONE       0x58
+#define TSB_I2C_CLR_ACTIVITY      0x5c
+#define TSB_I2C_CLR_STOP_DET      0x60
+#define TSB_I2C_CLR_START_DET     0x64
+#define TSB_I2C_CLR_GEN_CALL      0x68
+#define TSB_I2C_ENABLE            0x6c
+#define TSB_I2C_STATUS            0x70
+#define TSB_I2C_TXFLR             0x74
+#define TSB_I2C_RXFLR             0x78
+#define TSB_I2C_SDA_HOLD          0x7c
+#define TSB_I2C_TX_ABRT_SOURCE    0x80
+#define TSB_I2C_ENABLE_STATUS     0x9c
+
+/* Interrupts bits */
+#define TSB_I2C_INTR_RX_UNDER     0x0001
+#define TSB_I2C_INTR_RX_OVER      0x0002
+#define TSB_I2C_INTR_RX_FULL      0x0004
+#define TSB_I2C_INTR_TX_OVER      0x0008
+#define TSB_I2C_INTR_TX_EMPTY     0x0010
+#define TSB_I2C_INTR_RD_REQ       0x0020
+#define TSB_I2C_INTR_TX_ABRT      0x0040
+#define TSB_I2C_INTR_RX_DONE      0x0080
+#define TSB_I2C_INTR_ACTIVITY     0x0100
+#define TSB_I2C_INTR_STOP_DET     0x0200
+#define TSB_I2C_INTR_START_DET    0x0400
+#define TSB_I2C_INTR_GEN_CALL     0x0800
+
+
+#define TSB_I2C_STATUS_ACTIVITY       0x1
+#define TSB_I2C_ERR_TX_ABRT           0x1
+#define TSB_I2C_TAR_10BITADDR_MASTER  (1 << 12)
+
+/* I2C configuration bits */
+#define TSB_I2C_CON_MASTER            0x01
+#define TSB_I2C_CON_SPEED_STD         0x02
+#define TSB_I2C_CON_SPEED_FAST        0x04
+#define TSB_I2C_CON_10BITADDR_MASTER  0x10
+#define TSB_I2C_CON_RESTART_EN        0x20
+#define TSB_I2C_CON_SLAVE_DISABLE     0x40
+
+/* status codes */
+#define TSB_I2C_STATUS_IDLE                 0x0
+#define TSB_I2C_STATUS_WRITE_IN_PROGRESS    0x1
+#define TSB_I2C_STATUS_READ_IN_PROGRESS     0x2
+#define TSB_I2C_STATUS_TIMEOUT              0x3
+
+/* hardware abort codes from the TSB_I2C_TX_ABRT_SOURCE register */
+#define ABRT_7B_ADDR_NOACK  0
+#define ABRT_10ADDR1_NOACK  1
+#define ABRT_10ADDR2_NOACK  2
+#define ABRT_TXDATA_NOACK   3
+#define ABRT_GCALL_NOACK    4
+#define ABRT_GCALL_READ     5
+#define ABRT_SBYTE_ACKDET   7
+#define ABRT_SBYTE_NORSTRT  9
+#define ABRT_10B_RD_NORSTRT 10
+#define ABRT_MASTER_DIS     11
+#define ARBT_LOST           12
+
+#define TSB_I2C_TX_ABRT_7B_ADDR_NOACK     (1UL << ABRT_7B_ADDR_NOACK)
+#define TSB_I2C_TX_ABRT_10ADDR1_NOACK     (1UL << ABRT_10ADDR1_NOACK)
+#define TSB_I2C_TX_ABRT_10ADDR2_NOACK     (1UL << ABRT_10ADDR2_NOACK)
+#define TSB_I2C_TX_ABRT_TXDATA_NOACK      (1UL << ABRT_TXDATA_NOACK)
+#define TSB_I2C_TX_ABRT_GCALL_NOACK       (1UL << ABRT_GCALL_NOACK)
+#define TSB_I2C_TX_ABRT_GCALL_READ        (1UL << ABRT_GCALL_READ)
+#define TSB_I2C_TX_ABRT_SBYTE_ACKDET      (1UL << ABRT_SBYTE_ACKDET)
+#define TSB_I2C_TX_ABRT_SBYTE_NORSTRT     (1UL << ABRT_SBYTE_NORSTRT)
+#define TSB_I2C_TX_ABRT_10B_RD_NORSTRT    (1UL << ABRT_10B_RD_NORSTRT)
+#define TSB_I2C_TX_ABRT_MASTER_DIS        (1UL << ABRT_MASTER_DIS)
+#define TSB_I2C_TX_ARB_LOST               (1UL << ARBT_LOST)
+
+#define TSB_I2C_TX_ABRT_NOACK (TSB_I2C_TX_ABRT_7B_ADDR_NOACK | \
+                               TSB_I2C_TX_ABRT_10ADDR1_NOACK | \
+                               TSB_I2C_TX_ABRT_10ADDR2_NOACK | \
+                               TSB_I2C_TX_ABRT_TXDATA_NOACK | \
+                               TSB_I2C_TX_ABRT_GCALL_NOACK)
+
+
+#endif /* __ARCH_ARM_SRC_TSB_TSB_I2C_H */
