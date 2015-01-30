9b5f1ea5feedf1b25bf64386c32df4196f08767f
Alexandre Bailon <abailon@baylibre.com>
2015-01-30 11:01:13 +0100

    gpio: add a gpio chip driver
    Add a gpio chip driver in order use both internal gpio and gpio expender
    with the same api. Each gpio chip must be registered with register_gpio_chip
    method. The api is actually the same defined in tsb driver to keep compability
    with all applications/drivers using gpio.
    
    Testing Done: tested using touchscreen + test gpio test app
    Bug Number: SW-116
    Review URL: https://reviews.leaflabs.com/r/326/
    Reviewed by: fparent

---
+++ nuttx/drivers/gpio/gpio_chip.c
@@ -0,0 +1,218 @@
+/*
+ * Copyright (c) 2014-2015 Google Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from this
+ * software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <errno.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+#include <nuttx/gpio.h>
+#include <nuttx/list.h>
+
+#include "nuttx/arch.h"
+
+struct list_head g_gpio_chip = {
+    .prev = &g_gpio_chip,
+    .next = &g_gpio_chip,
+};
+
+int register_gpio_chip(struct gpio_ops_s *ops, int base)
+{
+    struct list_head *iter;
+    struct gpio_chip_s *chip;
+
+    DEBUGASSERT(ops);
+
+    if (base == -1) {
+        list_foreach(&g_gpio_chip, iter) {
+            chip = list_entry(iter, struct gpio_chip_s, list);
+            base = chip->end;
+        }
+    }
+
+    chip = malloc(sizeof(*chip));
+    if (!chip)
+        return -ENOMEM;
+
+    chip->base = base;
+    chip->end = base + ops->line_count();
+    chip->ops = ops;
+
+    list_add(&g_gpio_chip, &chip->list);
+
+    return 0;
+}
+
+static struct gpio_chip_s *get_gpio_chip(uint8_t *which)
+{
+    struct list_head *iter;
+    struct gpio_chip_s *chip;
+
+    list_foreach(&g_gpio_chip, iter) {
+        chip = list_entry(iter, struct gpio_chip_s, list);
+        if (chip->base <= *which && *which < chip->end) {
+            *which -= chip->base;
+            return chip;
+        }
+    }
+    return NULL;
+}
+
+int gpio_get_direction(uint8_t which)
+{
+    struct gpio_chip_s *chip = get_gpio_chip(&which);
+    return chip->ops->get_direction(which);
+}
+
+void gpio_direction_in(uint8_t which)
+{
+    struct gpio_chip_s *chip = get_gpio_chip(&which);
+
+    DEBUGASSERT(chip);
+    DEBUGASSERT(chip->ops->direction_in);
+    chip->ops->direction_in(which);
+}
+
+void gpio_direction_out(uint8_t which, uint8_t value)
+{
+    struct gpio_chip_s *chip = get_gpio_chip(&which);
+
+    DEBUGASSERT(chip);
+    DEBUGASSERT(chip->ops->direction_out);
+    chip->ops->direction_out(which, value);
+}
+
+void gpio_activate(uint8_t which)
+{
+    struct gpio_chip_s *chip = get_gpio_chip(&which);
+
+    DEBUGASSERT(chip);
+    DEBUGASSERT(chip->ops->activate);
+    chip->ops->activate(which);
+}
+
+uint8_t gpio_get_value(uint8_t which)
+{
+    struct gpio_chip_s *chip = get_gpio_chip(&which);
+
+    DEBUGASSERT(chip);
+    DEBUGASSERT(chip->ops->get_value);
+    return chip->ops->get_value(which);
+}
+
+void gpio_set_value(uint8_t which, uint8_t value)
+{
+    struct gpio_chip_s *chip = get_gpio_chip(&which);
+
+    DEBUGASSERT(chip);
+    DEBUGASSERT(chip->ops->set_value);
+    chip->ops->set_value(which, value);
+}
+
+int gpio_set_debounce(uint8_t which, uint16_t delay)
+{
+    uint8_t initial_reading;
+
+    initial_reading = gpio_get_value(&which);
+    up_udelay(delay);
+    return initial_reading == gpio_get_value(which);
+}
+
+void gpio_deactivate(uint8_t which)
+{
+    struct gpio_chip_s *chip = get_gpio_chip(&which);
+
+    DEBUGASSERT(chip);
+    DEBUGASSERT(chip->ops->deactivate);
+    chip->ops->deactivate(which);
+}
+
+uint8_t gpio_line_count(void)
+{
+    uint8_t count = 0;
+    struct list_head *iter;
+    struct gpio_chip_s *chip;
+
+    list_foreach(&g_gpio_chip, iter) {
+        chip = list_entry(iter, struct gpio_chip_s, list);
+        DEBUGASSERT(chip->ops->line_count);
+        count += chip->ops->line_count();
+    }
+    return count;
+}
+
+int gpio_irqattach(uint8_t which, xcpt_t isr)
+{
+    struct gpio_chip_s *chip = get_gpio_chip(&which);
+
+    DEBUGASSERT(chip);
+    if (chip->ops->irqattach)
+        return chip->ops->irqattach(which, isr, chip->base);
+    return -EINVAL;
+}
+
+int set_gpio_triggering(uint8_t which, int trigger)
+{
+    struct gpio_chip_s *chip = get_gpio_chip(&which);
+
+    DEBUGASSERT(chip);
+    if (chip->ops->set_triggering)
+        return chip->ops->set_triggering(which, trigger);
+    return -EINVAL;
+}
+
+int gpio_mask_irq(uint8_t which)
+{
+    struct gpio_chip_s *chip = get_gpio_chip(&which);
+
+    DEBUGASSERT(chip);
+    if (chip->ops->mask_irq)
+        return chip->ops->mask_irq(which);
+    return -EINVAL;
+}
+
+int gpio_unmask_irq(uint8_t which)
+{
+    struct gpio_chip_s *chip = get_gpio_chip(&which);
+
+    DEBUGASSERT(chip);
+    if (chip->ops->unmask_irq)
+        return chip->ops->unmask_irq(which);
+    return -EINVAL;
+}
+
+int gpio_clear_interrupt(uint8_t which)
+{
+    struct gpio_chip_s *chip = get_gpio_chip(&which);
+
+    DEBUGASSERT(chip);
+    if (chip->ops->clear_interrupt)
+        return chip->ops->clear_interrupt(which);
+    return -EINVAL;
+}
+
---
+++ nuttx/include/nuttx/gpio.h
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) 2014-2015 Google Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from this
+ * software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef _GPIO_CHIP_H_
+#define _GPIO_CHIP_H_
+
+#include <nuttx/irq.h>
+#include <nuttx/list.h>
+
+#define IRQ_TYPE_NONE           0x00000000
+#define IRQ_TYPE_EDGE_RISING    0x00000001
+#define IRQ_TYPE_EDGE_FALLING   0x00000002
+#define IRQ_TYPE_EDGE_BOTH      (IRQ_TYPE_EDGE_FALLING | IRQ_TYPE_EDGE_RISING)
+#define IRQ_TYPE_LEVEL_HIGH     0x00000004
+#define IRQ_TYPE_LEVEL_LOW      0x00000008
+
+struct gpio_ops_s
+{
+    int (*get_direction)(uint8_t which);
+    void (*direction_in)(uint8_t which);
+    void (*direction_out)(uint8_t which, uint8_t value);
+    void (*activate)(uint8_t which);
+    uint8_t (*get_value)(uint8_t which);
+    void (*set_value)(uint8_t which, uint8_t value);
+    void (*deactivate)(uint8_t which);
+    uint8_t (*line_count)(void);
+    int (*irqattach)(uint8_t which, xcpt_t isr, uint8_t base);
+    int (*set_triggering)(uint8_t which, int trigger);
+    int (*mask_irq)(uint8_t which);
+    int (*unmask_irq)(uint8_t which);
+    int (*clear_interrupt)(uint8_t which);
+};
+
+struct gpio_chip_s
+{
+    struct list_head list;
+    struct gpio_ops_s *ops;
+    uint8_t base;
+    uint8_t end;
+};
+
+int gpio_get_direction(uint8_t which);
+void gpio_direction_in(uint8_t which);
+void gpio_direction_out(uint8_t which, uint8_t value);
+void gpio_activate(uint8_t which);
+uint8_t gpio_get_value(uint8_t which);
+void gpio_set_value(uint8_t which, uint8_t value);
+int gpio_set_debounce(uint8_t which, uint16_t delay);
+void gpio_deactivate(uint8_t which);
+uint8_t gpio_line_count(void);
+int gpio_irqattach(uint8_t which, xcpt_t isr);
+int set_gpio_triggering(uint8_t which, int trigger);
+int gpio_mask_irq(uint8_t which);
+int gpio_unmask_irq(uint8_t which);
+int gpio_clear_interrupt(uint8_t which);
+
+int register_gpio_chip(struct gpio_ops_s *ops, int base);
+
+#endif
+
