79ce7f09ed8113ccb6d560b0e5b9245baea6cfb8
Perry Hung <perry@leaflabs.com>
2015-02-04 17:31:42 -0500

    util: Utility functions header file
    Add include/util.h for useful things like ARRAY_SIZE.
    
    Testing Done: greybus testing on bbb with ES1 switch
    Bug Number: SW-400
    Review URL: https://reviews.leaflabs.com/r/305
    Reviewed by: mgreer

---
+++ nuttx/include/nuttx/util.h
@@ -0,0 +1,15 @@
+/**
+ * Copyright (c) 2015 Google, Inc.
+ * @author: Perry Hung
+ */
+
+#ifndef  _UTIL_H_
+#define  _UTIL_H_
+
+#ifndef ARRAY_SIZE
+    #define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
+#endif
+
+#endif
+
+
