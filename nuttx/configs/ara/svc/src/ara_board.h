7b1e26223e0df9b091ed3447d22b879a4d7b964b
Perry Hung <perry@leaflabs.com>
2015-02-04 17:55:13 -0500

    ara/svc: create board description with sample BDB1B description
    This patch creates a new structure that contains board-specific
    information such as the pinouts of interfaces, switch information, gpio
    lines, etc.
    
    The intention is that every Ara board must provide a board_init()
    function which returns the board-specific information.
    
    Testing Done: greybus testing from apb1 to apb2 with I2C
    Bug Number: SW-400
    Review URL: https://reviews.leaflabs.com/r/283/
    Reviewed by: fparent, jpihet

---
+++ nuttx/configs/ara/svc/src/ara_board.h
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2015 Google Inc.
+ * Google Confidential/Restricted
+ */
+
+#ifndef  _ARA_BOARD_H_
+#define  _ARA_BOARD_H_
+
+struct ara_board_info {
+    struct interface **interfaces;
+    size_t nr_interfaces;
+
+    /* Switch data */
+    unsigned int sw_i2c_bus;
+    unsigned int sw_1p1;
+    unsigned int sw_1p8;
+    unsigned int sw_reset;
+    unsigned int sw_irq;
+
+    unsigned int svc_irq;
+};
+
+struct ara_board_info *board_init(void);
+void board_exit(void);
+
+#endif
+
+
