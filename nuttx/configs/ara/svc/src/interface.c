f208dc319e074309136a9d07fee1abe462ca4e3e
Jean Pihet <jean.pihet@newoldbits.com>
2015-02-06 15:42:47 +0100

    SVC: BDB1B/2A: Add WAKEOUT support; Update the interface public API
    Interface API:
    - power supplies control and state,
    - WAKEOUT generation,
    - Wake and Detect state read (not supported yet, for compatibility with Endo),
    - iteration macro (interface_foreach) for use by the power app.
    
    Testing Done: test power supplies and wakeout signals on BDB2A; build for BDB1B
    Bug Number: SW-183
    Review URL: https://reviews.leaflabs.com/r/325/
    Reviewed by: marti, mag
    
    Signed-off-by: Jean Pihet <jean.pihet@newoldbits.com>

--- nuttx/configs/ara/svc/src/interface.c
+++ nuttx/configs/ara/svc/src/interface.c
@@ -15,10 +15,12 @@
 #include "up_debug.h"
 #include "interface.h"
 
-#define POWER_OFF_TIME_IN_US (500000)
+#define POWER_OFF_TIME_IN_US            (500000)
+#define WAKEOUT_PULSE_DURATION_IN_US    (100000)
 
 static struct interface **interfaces;
 static unsigned int nr_interfaces;
+
 
 /**
  * @brief Configure all the voltage regulators associated with an interface
@@ -27,19 +29,35 @@ static unsigned int nr_interfaces;
  */
 static int interface_config(struct interface *iface) {
     unsigned int i;
-    int rc;
+    int rc = 0;
 
     dbg_verbose("Configuring interface %s.\n",
             iface->name ? iface->name : "unknown");
 
     for (i = 0; i < iface->nr_vregs; i++) {
-        rc = stm32_configgpio(iface->vregs[i].gpio);
-        if (rc < 0) {
-            return rc;
+        if (stm32_configgpio(iface->vregs[i].gpio) < 0) {
+            dbg_error("%s: Failed to configure vregs pins for interface %s\n",
+                      __func__, iface->name ? iface->name : "unknown");
+            // Let other pins to be configured
+            rc = -1;
         }
     }
 
-    return 0;
+    /*
+     * Configure WAKEOUT as input, floating so that it does not interfere
+     * with the wake and detect input pin
+     */
+    if (iface->wake_out) {
+        if (stm32_configgpio(iface->wake_out | GPIO_INPUT) < 0) {
+            dbg_error("%s: Failed to configure WAKEOUT pin for interface %s\n",
+                      __func__, iface->name ? iface->name : "unknown");
+            rc = -1;
+        }
+    }
+
+    iface->power_state = false;
+
+    return rc;
 }
 
 
@@ -61,6 +79,9 @@ int interface_pwr_enable(struct interface *iface) {
         stm32_gpiowrite(iface->vregs[i].gpio, iface->vregs[i].active_high);
         up_udelay(iface->vregs[i].hold_time);
     }
+
+    /* Update state */
+    iface->power_state = true;
 
     return 0;
 }
@@ -85,9 +106,86 @@ int interface_pwr_disable(struct interface *iface) {
         stm32_gpiowrite(iface->vregs[i].gpio, !iface->vregs[i].active_high);
     }
 
+    /* Update state */
+    iface->power_state = false;
+
     return 0;
 }
 
+
+/*
+ * @brief Generate a WAKEOUT signal to wake-up/power-up modules.
+ * If assert is true, keep the WAKEOUT lines asserted.
+ *
+ * The corresponding power supplies must already be enabled.
+ */
+int interface_generate_wakeout(struct interface *iface, bool assert)
+{
+    int rc;
+
+    if (!iface) {
+        return -ENODEV;
+    }
+
+   /*
+    * Assert the WAKEOUT line on the interfaces in order to power up the
+    * modules.
+    * When the WAKEOUT signal is de-asserted the bridges have to assert
+    * the PS_HOLD signal asap in order to stay powered up.
+    */
+    dbg_verbose("Generating WAKEOUT on interface %s.\n",
+                iface->name ? iface->name : "unknown");
+
+    if (iface->wake_out) {
+        rc = stm32_configgpio(iface->wake_out | GPIO_OUTPUT | GPIO_OUTPUT_SET);
+        if (rc < 0) {
+            dbg_error("%s: Failed to assert WAKEOUT pin for interface %s\n",
+                      __func__, iface->name ? iface->name : "unknown");
+            return rc;
+        }
+
+        if (!assert) {
+            /* Wait for the bridges to react */
+            usleep(WAKEOUT_PULSE_DURATION_IN_US);
+
+            /* De-assert the lines */
+            rc = stm32_configgpio(iface->wake_out | GPIO_INPUT);
+            if (rc < 0) {
+                dbg_error("%s: Failed to de-assert WAKEOUT pin for interface %s\n",
+                          __func__, iface->name ? iface->name : "unknown");
+                return rc;
+            }
+        }
+    }
+
+    return 0;
+}
+
+
+/*
+ * @brief Get interface power supply state, or false if no interface is supplied
+ */
+bool interface_get_pwr_state(struct interface *iface)
+{
+    if (!iface) {
+        return false;
+    }
+
+    return iface->power_state;
+}
+
+/**
+ * @brief Get the interface struct from the index, as specified in the MDK.
+ *        Index 0 is for the first interface (aka 'A').
+ * @returns: interface* on success, NULL on error
+ */
+struct interface* interface_get(uint8_t index)
+{
+    if ((!interfaces) || (index >= nr_interfaces))
+        return NULL;
+
+    return interfaces[index];
+}
 
 /**
  * @brief Given a table of interfaces, initialize and enable all associated
@@ -135,6 +233,13 @@ int interface_init(struct interface **ints, size_t nr_ints) {
             interface_exit();
             return rc;
         }
+        rc = interface_generate_wakeout(interfaces[i], true);
+        if (rc < 0) {
+            dbg_error("Failed to generate wakeout on interface %s\n",
+                      interfaces[i]->name);
+            interface_exit();
+            return rc;
+        }
     }
 
     return 0;
@@ -167,4 +272,3 @@ void interface_exit(void) {
     interfaces = NULL;
     nr_interfaces = 0;
 }
-
--- nuttx/configs/ara/svc/src/interface.h
+++ nuttx/configs/ara/svc/src/interface.h
@@ -8,6 +8,8 @@
 #ifndef  _INTERFACE_H_
 #define  _INTERFACE_H_
 
+#include <errno.h>
+
 struct vreg_data {
     unsigned int gpio;
     unsigned int hold_time;   // Assertion duration, in us
@@ -19,31 +21,47 @@ struct interface {
     unsigned int switch_portid;
     struct vreg_data *vregs;
     size_t nr_vregs;
+    bool power_state;
+    unsigned int wake_out;
 };
+
+#define interface_foreach(iface, idx)                       \
+        for ((idx) = 0, (iface) = interface_get(idx);       \
+             (iface);                                       \
+             (idx)++, (iface) = interface_get(idx))
 
 int interface_init(struct interface**, size_t nr_interfaces);
 void interface_exit(void);
+struct interface* interface_get(uint8_t index);
 
 int interface_pwr_enable(struct interface*);
 int interface_pwr_disable(struct interface*);
+bool interface_get_pwr_state(struct interface *iface);
+int interface_generate_wakeout(struct interface *, bool assert);
+static inline int interface_read_wake_detect(void)
+{
+    return -EOPNOTSUPP;
+}
 
 /*
  * Macro magic.
  */
-#define INIT_VREG_DATA(g, t) \
-    { \
-        .gpio = (VREG_DEFAULT_MODE | g),      \
-        .hold_time = t,                       \
-        .active_high = 1,                          \
+#define INIT_VREG_DATA(g, t)                                   \
+    {                                                          \
+        .gpio = (VREG_DEFAULT_MODE | g),                       \
+        .hold_time = t,                                        \
+        .active_high = 1,                                      \
     }
 
-#define INIT_SPRING_VREG_DATA(g) \
-    { \
-        .gpio = (SPRING_VREG_DEFAULT_MODE | g), \
-        .hold_time = 0,                         \
-        .active_high = 0,                            \
+#define INIT_SPRING_VREG_DATA(g)                               \
+    {                                                          \
+        .gpio = (SPRING_VREG_DEFAULT_MODE | g),                \
+        .hold_time = 0,                                        \
+        .active_high = 0,                                      \
     }
 
+#define __MAKE_BB_WAKEOUT(n) WAKEOUT_SPRING ## n
+#define MAKE_BB_WAKEOUT(n) __MAKE_BB_WAKEOUT(n)
 #define __MAKE_BB_VREG(n) bb ## n ## _vregs
 #define MAKE_BB_VREG(n) __MAKE_BB_VREG(n)
 #define __MAKE_BB_INTERFACE(n) bb ## n ## _interface
@@ -58,18 +76,18 @@ int interface_pwr_disable(struct interface*);
         .vregs = MAKE_BB_VREG(number),                         \
         .nr_vregs = ARRAY_SIZE(MAKE_BB_VREG(number)),          \
         .switch_portid = portid,                               \
+        .wake_out = MAKE_BB_WAKEOUT(number),                   \
     };
 
 #define __MAKE_INTERFACE(n) n ## _interface
 #define MAKE_INTERFACE(n) __MAKE_INTERFACE(n)
-#define DECLARE_INTERFACE(_name, gpios, portid)                \
+#define DECLARE_INTERFACE(_name, gpios, portid, _wake_out)     \
     static struct interface MAKE_INTERFACE(_name) = {          \
         .name = #_name,                                        \
         .vregs = gpios,                                        \
         .nr_vregs = ARRAY_SIZE(gpios),                         \
         .switch_portid = portid,                               \
+        .wake_out = _wake_out,                                 \
     };
 
 #endif
-
-
