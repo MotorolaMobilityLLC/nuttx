3808fdb1acc97ce38823aab743b86e6ccb122d7d
Jean Pihet <jean.pihet@newoldbits.com>
2015-02-10 18:12:43 +0800

    SVC: Apps: Enable power app for BDB1A/2B
    Using the interface abstraction API.
    Controls the power supply and wakeout signals from the console.
    
    Testing Done: test power supplies and wakeout signals on Endo, BDB2A and BDB1B
    Bug Number: SW-183
    Review URL: https://reviews.leaflabs.com/r/338/
    Reviewed by: marti, mag
    
    Signed-off-by: Jean Pihet <jean.pihet@newoldbits.com>

--- apps/ara/svc_power/svc_power.c
+++ apps/ara/svc_power/svc_power.c
@@ -10,6 +10,8 @@
 #define DBG_COMP DBG_POWER
 #include "up_debug.h"
 #include "up_power.h"
+#include "interface.h"
+
 
 #ifdef CONFIG_BUILD_KERNEL
 int main(int argc, FAR char *argv[])
@@ -20,12 +22,19 @@ int power_main(int argc, char *argv[])
     uint32_t int_nr, state;
     int i;
     char cmd;
+    struct interface *iface;
 
     if (argc < 2) {
         printk("Power: Usage:\n");
-        printk("  power p [interface_nr 0/1] : get/set interface power\n");
-        printk("  power v                    : validate Wake&Detect inputs\n");
-        printk("  power w interface_mask     : generate WAKEOUT pulse\n");
+        printk("  power p [interface_nr 0/1]  : get/set interface power\n");
+        printk("  power v                     : validate Wake&Detect inputs\n");
+        printk("  power w interface_nr assert : assert or pulse WAKEOUT\n");
+        printk("\n interface_nr is defined as:\n");
+        printk("  index\tname\n");
+
+        interface_foreach(iface, i) {
+            printk("  %02d\t%s\n", i, iface->name);
+        }
         return ERROR;
     } else {
         cmd = argv[1][0];
@@ -35,39 +44,55 @@ int power_main(int argc, char *argv[])
     case 'p':
         if (argc == 2) {
             /* Get the power states */
-            for (i = 0; i < PWR_SPRING_NR; i++)
-                printk("Power: Spring %d state = %d\n", i, power_get_power(i));
+            interface_foreach(iface, i) {
+                printk("Power: Interface(%02d) %s state = %d\n",
+                       i,
+                       iface->name,
+                       interface_get_pwr_state(iface));
+            }
             return 0;
         }
         if (argc > 3) {
             /* Set the power state */
             int_nr = strtol(argv[2], NULL, 10);
             state = strtol(argv[3], NULL, 10);
-            printk("Power: set Spring %d state to %d\n", int_nr, state);
-            power_set_power(int_nr, state);
-            return 0;
+
+            iface = interface_get(int_nr);
+            if (!iface)
+                break;
+
+            printk("Power: set interface(%02d) %s state to %d\n",
+                   int_nr, iface->name, state);
+
+            return state ? interface_pwr_enable(iface) :
+                           interface_pwr_disable(iface);
         }
 
         printk("Power: wrong command\n");
         return ERROR;
 
     case 'v':
-        power_read_wake_detect();
+        interface_read_wake_detect();
         break;
 
     case 'w':
         if (argc == 2) {
-            printk("Power: please provide a bitmask value\n");
+            printk("Power: please provide an interface_nr and assert values\n");
             return ERROR;
         }
-        if (argc >= 3) {
-            /* Generate WAKEOUT pulse on the interface given by the bitmask */
-            state = strtoul(argv[2], NULL, 16);
-            for (i = 0; i < PWR_SPRING_NR; i++)
-                if (state & (1 << i))
-                    printk("Power: generate WAKEOUT pulse for Spring %d\n", i);
-            power_set_wakeout(state, false);
-            return 0;
+        if (argc >= 4) {
+            /* Generate WAKEOUT pulse on the given interface */
+            int_nr = strtol(argv[2], NULL, 10);
+            state = strtol(argv[3], NULL, 10);
+
+            iface = interface_get(int_nr);
+            if (!iface)
+                break;
+
+            printk("Power: %s WAKEOUT on interface(%02d) %s\n",
+                   state ? "pulse" : "assert", int_nr, iface->name);
+
+            return interface_generate_wakeout(iface, state);
         }
 
         printk("Power: wrong command\n");
