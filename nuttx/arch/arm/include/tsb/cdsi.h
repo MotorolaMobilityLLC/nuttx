9aff90ea9b282384a3733b03081321c73a11175b
Alexandre Bailon <abailon@baylibre.com>
2015-03-06 16:49:04 +0100

    tsb: add cdsi driver to initialize cdsi
    This is not really a cdsi driver. It only clock the cdsi and a run a callback to perform
    real cdsi initialization. Firmware must provide and register this callback.
    
    Testing Done: dsi is clocked and display work
    Bug Number: SW-116
    Review URL: https://reviews.leaflabs.com/r/375
    Reviewed by: mgreer

---
+++ nuttx/arch/arm/include/tsb/cdsi.h
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2015 Google Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from this
+ * software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef __ARCH_ARM_INCLUDE_TSB_DSI_H
+#define __ARCH_ARM_INCLUDE_TSB_DSI_H
+
+#define TSB_CDSI0 0
+#define TSB_CDSI1 1
+
+#define TSB_CDSI_RX 0
+#define TSB_CDSI_TX 1
+
+struct cdsi_dev
+{
+    uint32_t base;
+    int tx;
+};
+
+struct display_panel
+{
+    /* platform callback to initialize cdsi */
+    void (*cdsi_panel_init)(struct cdsi_dev *dev);
+};
+
+void cdsi_write(struct cdsi_dev *dev, uint32_t addr, uint32_t v);
+uint32_t cdsi_read(struct cdsi_dev *dev, uint32_t addr);
+
+struct cdsi_dev *dsi_initialize(struct display_panel *panel, int dsi, int tx);
+void dsi_uninitialize(struct cdsi_dev *dev);
+
+#endif
+
---
+++ nuttx/arch/arm/src/tsb/tsb_cdsi.c
@@ -0,0 +1,138 @@
+/*
+ * Copyright (c) 2015 Google Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from this
+ * software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <stdlib.h>
+
+#include <nuttx/arch.h>
+#include <arch/tsb/cdsi.h>
+
+#include "chip.h"
+#include "up_arch.h"
+#include "tsb_scm.h"
+
+void cdsi_write(struct cdsi_dev *dev, uint32_t addr, uint32_t v)
+{
+    putreg32(v, dev->base + addr);
+}
+
+uint32_t cdsi_read(struct cdsi_dev *dev, uint32_t addr)
+{
+    return getreg32(dev->base + addr);
+}
+
+static struct cdsi_dev *cdsi_initialize(int cdsi, int tx)
+{
+    struct cdsi_dev *dev;
+
+    dev = malloc(sizeof(struct cdsi_dev));
+    if (!dev)
+        return NULL;
+
+    dev->tx = tx;
+    dev->base = cdsi == TSB_CDSI0 ? CDSI0_BASE : CDSI1_BASE;
+    tsb_clk_enable(cdsi == TSB_CDSI0 ? TSB_CLK_CDSI0_REF : TSB_CLK_CDSI1_REF);
+    if (tx) {
+        if (cdsi == TSB_CDSI0) {
+            tsb_clk_enable(TSB_CLK_CDSI0_TX_SYS);
+            tsb_clk_enable(TSB_CLK_CDSI0_TX_APB);
+            tsb_reset(TSB_RST_CDSI0_TX);
+            tsb_reset(TSB_RST_CDSI0_TX_AIO);
+        } else {
+            tsb_clk_enable(TSB_CLK_CDSI1_TX_SYS);
+            tsb_clk_enable(TSB_CLK_CDSI1_TX_APB);
+            tsb_reset(TSB_RST_CDSI1_TX);
+            tsb_reset(TSB_RST_CDSI1_TX_AIO);
+        }
+    } else {
+        if (cdsi == TSB_CDSI0) {
+            tsb_clk_enable(TSB_CLK_CDSI0_RX_SYS);
+            tsb_clk_enable(TSB_CLK_CDSI0_RX_APB);
+            tsb_reset(TSB_RST_CDSI0_RX);
+            tsb_reset(TSB_RST_CDSI0_RX_AIO);
+        } else {
+            tsb_clk_enable(TSB_CLK_CDSI1_RX_SYS);
+            tsb_clk_enable(TSB_CLK_CDSI1_RX_APB);
+            tsb_reset(TSB_RST_CDSI1_RX);
+            tsb_reset(TSB_RST_CDSI1_RX_AIO);
+        }
+    }
+    return dev;
+}
+
+static void cdsi_uninitialize(struct cdsi_dev *dev)
+{
+    int cdsi = dev->base == CDSI0_BASE ? TSB_CDSI0 : TSB_CDSI1;
+    tsb_clk_disable(cdsi == TSB_CDSI0 ? TSB_CLK_CDSI0_REF : TSB_CLK_CDSI1_REF);
+    if (dev->tx) {
+        if (cdsi == TSB_CDSI0) {
+            tsb_clk_disable(TSB_CLK_CDSI0_TX_SYS);
+            tsb_clk_disable(TSB_CLK_CDSI0_TX_APB);
+        } else {
+            tsb_clk_disable(TSB_CLK_CDSI1_TX_SYS);
+            tsb_clk_disable(TSB_CLK_CDSI1_TX_APB);
+        }
+    } else {
+        if (cdsi == TSB_CDSI0) {
+            tsb_clk_disable(TSB_CLK_CDSI0_RX_SYS);
+            tsb_clk_disable(TSB_CLK_CDSI0_RX_APB);
+        } else {
+            tsb_clk_disable(TSB_CLK_CDSI1_RX_SYS);
+            tsb_clk_disable(TSB_CLK_CDSI1_RX_APB);
+        }
+    }
+    free(dev);
+}
+
+/**
+ * @brief Initialize cdsi in dsi mode
+ * @param panel Panel to register
+ * @param cdsi   The cdsi block to initialize
+ * @param tx    0 if dsi work in rx mode or 1 for tx mode
+ * @return a cdsi_dev pointer or NULL on any faillure.
+ */
+struct cdsi_dev *dsi_initialize(struct display_panel *panel, int cdsi, int tx)
+{
+    struct cdsi_dev *dev;
+
+    dev = cdsi_initialize(cdsi, tx);
+    if (!dev)
+        return NULL;
+
+    panel->cdsi_panel_init(dev);
+
+    return dev;
+}
+
+/**
+ * @brief Uninitialize dsi
+ * @param dev Pointer to cdsi to uninitialize
+ */
+void dsi_uninitialize(struct cdsi_dev *dev)
+{
+    cdsi_uninitialize(dev);
+}
