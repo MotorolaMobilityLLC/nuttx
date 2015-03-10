4ab8893bc9f67a60909ed5dca571823f033bf09b
Mitchell Tasman <tasman@leaflabs.com>
2015-03-09 21:44:06 -0400

    Add a custom idle loop for TSB to avoid USB stall
    On the ES1 Bridge silicon, a tight idle loop appears to have the
    potential to stall an in-progress USB DMA transfer, or at least to
    delay notification of its completion.
    
    This phenomenon was observed on the BDB1B, where Greybus transaction
    throughput was limited by the configured SYS TICK rate on AP Bridge 1.
    It turned out that the SYS TICK interrupt temporarily breaking the Cortex
    M3 out of its idle loop was sufficient to allow the next USB bulk OUT DMA
    Transfer Complete interrupt to pend.  A work-around, which was to disable
    NuttX work queues, may have succeeded because NuttX's housekeeping functions
    were then called from the idle loop, thus avoiding a tight spin, and
    potentially inserting pauses into the CM3's instruction fetch pipeline.
    
    The present commit clones NuttX's default up_idle() function for ARM
    into a TSB-specific version, and builds tsb_idle.c in place of
    up_idle.c.  The custom up_idle() function takes two different approaches
    to working-around the stall problem, depending on CONFIG_DEBUG:
    
    1.  When CONFIG_DEBUG is not defined, we assume a production build,
        and up_idle() invokes the ARM (WFI) instruction.  This is probably
        the best solution, but transitions through WFI degrade and
        potentially destabilize JTAG functionality, since the JTAG interface
        is shut down during WFI, and unexpected traps can result.  A
        partial mitigation is to use the SWD interface rather than the
        JTAG interface (both are available via the same connector).  For the
        J-Link Pro, SWD may be selected by providing the "-if SWD" argument
        to JLinkGDBServer, but occasional traps may still result.
    
    2.  When CONFIG_DEBUG=y, as it is for our present NuttX configurations for
        the bridges, up_idle() will instead execute a sequence of nop instructions.
        This is an approach suggested by Olin Siebert, who theorized that the
        Cortex M3 would fetch two 16-bit nop instructions at a time, but execute
        them sequentially, thus reducing bus bandwidth by half and allowing other
        activities to proceed.  Avoiding WFI preserves full JTAG/SWD functionality.
    
    Testing Done: Built and deployed current bridge firmware to AP Bridge 1
        and AP Bridge 2.  Repeatedly read AP Bridge 2 GPIO 0's value on
        Jetson, using a shell script, and observed the 100 transaction per second
        limit.  Built code for AP Bridge 1 with the patch applied, deployed that
        code to AP Bridge 1, and repeated the test, observing throughput of
        greater than 2000 transactions per second.
    Bug Number: SW-425
    Review URL: https://reviews.leaflabs.com/r/361
    Reviewed by: perry, mark, mbolivar
    
    Signed-off-by: Mitchell Tasman <tasman@leaflabs.com>

---
+++ nuttx/arch/arm/src/tsb/tsb_idle.c
@@ -0,0 +1,163 @@
+/****************************************************************************
+ *  arch/arm/src/common/up_idle.c
+ *
+ *   Copyright (C) 2007-2009, 2011 Gregory Nutt. All rights reserved.
+ *   Author: Gregory Nutt <gnutt@nuttx.org>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name NuttX nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+/*
+ * Copyright (c) 2015 Google Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from this
+ * software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+
+#include <nuttx/arch.h>
+#include "up_internal.h"
+
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Private Data
+ ****************************************************************************/
+
+/****************************************************************************
+ * Private Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Name: up_idle
+ *
+ * Description:
+ *   up_idle() is the logic that will be executed when their is no other
+ *   ready-to-run task.  This is processor idle time and will continue until
+ *   some interrupt occurs to cause a context switch from the idle task.
+ *
+ *   Processing in this state may be processor-specific. e.g., this is where
+ *   power management operations might be performed.
+ *
+ ****************************************************************************/
+
+void up_idle(void)
+{
+#if defined(CONFIG_SUPPRESS_INTERRUPTS) || defined(CONFIG_SUPPRESS_TIMER_INTS)
+  /* If the system is idle and there are no timer interrupts, then process
+   * "fake" timer interrupts. Hopefully, something will wake up.
+   */
+
+  sched_process_timer();
+#else
+  /* SW-425 */
+#ifdef CONFIG_DEBUG
+  /* We theorize that instruction fetch on the bridge silicon may stall an
+   * in-progress USB DMA transfer.  The ideal solution is to halt the processor
+   * during idle via WFI (wait for interrupt), but that degrades the JTAG
+   * debugging experience (see discussion below).
+   *
+   * For debug builds, we'll try a work-around suggested by Olin Siebert, namely
+   * to execute a sequence of 16-bit nop instructions.  The theory is that the CM3
+   * core will fetch two 16-bit instructions at a time, but execute them
+   * sequentially, dropping instruction fetch bandwidth by 50% during idle
+   * periods, and offering USB DMA transfers the opportunity to progress and
+   * complete.
+   */
+  asm("nop");
+  asm("nop");
+  asm("nop");
+  asm("nop");
+  asm("nop");
+  asm("nop");
+  asm("nop");
+  asm("nop");
+  asm("nop");
+  asm("nop");
+  asm("nop");
+  asm("nop");
+  asm("nop");
+  asm("nop");
+  asm("nop");
+  asm("nop");
+  asm("nop");
+  asm("nop");
+  asm("nop");
+  asm("nop");
+#else
+  /*
+   * Sleep until an interrupt occurs to avoid the possibility that a tight
+   * idle loop on the bridge silicon may stall an in-progress USB DMA
+   * transfer, and to save power.
+   *
+   * The JTAG interface is disabled during WFI, and WFI transitions may result in
+   * unexpected traps when a JTAG debugger is connected.  A potential
+   * work-around is to use the SWD interface lines instead of JTAG, e.g. by
+   * supplying the "-if SWD" argument to JLinkGDBServer when using SEGGER products.
+   * This appears to increase stability based on limited experimentation, but may
+   * not be a panacea.  As such, we use the CONFIG_DEBUG option to select between
+   * two approaches to resolving SW-425, with the hueristic that CONFIG_DEBUG
+   * being enabled indicates a debug rather than production environment.
+   */
+  asm("WFI");
+#endif
+#endif
+}
