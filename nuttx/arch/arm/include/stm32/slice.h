00828e12074cdeb723f14b184d4d26a11b573d9d
Greg Meiste <w30289@motorola.com>
2015-03-11 10:54:59 -0500

    stm32: slice: Add BASE_DET_N interrupt
    When the STM32 has the interrupt line to the host asserted, the
    BASE_DET_N interrupt does not work as expected. Since they share
    the same physical pin, BASE_DET_N also asserts, resulting in the
    base being "present" even if it is not.
    
    Change-Id: I0f702cc865370840cd5b0f879ba7691fe07404b2
    Signed-off-by: Greg Meiste <w30289@motorola.com>

--- nuttx/arch/arm/include/stm32/slice.h
+++ nuttx/arch/arm/include/stm32/slice.h
@@ -7,9 +7,10 @@
 #ifndef _STM32_SLICE_H_
 #define _STM32_SLICE_H_
 
-int slice_init(void);
+int slice_init(xcpt_t irqhandler);
 int slice_uninit(void);
 void slice_host_int_set(bool value);
+bool slice_base_det_read(void);
 
 #endif /* _STM32_SLICE_H_ */
 
--- nuttx/arch/arm/src/stm32/stm32_slice.c
+++ nuttx/arch/arm/src/stm32/stm32_slice.c
@@ -7,18 +7,35 @@
 #include <arch/board/board.h>
 #include <arch/stm32/slice.h>
 
-int slice_init(void)
+int slice_init(xcpt_t irqhandler)
 {
-    return stm32_configgpio(GPIO_SLICE_CORE_INT);
+    int ret = stm32_configgpio(GPIO_SLICE_CORE_INT);
+
+    if (ret == OK)
+      {
+        // The gpio is configured inside stm32_gpiosetevent
+        stm32_gpiosetevent(GPIO_SLICE_BASE_DET_N, true, true, true, irqhandler);
+      }
+
+    return ret;
 }
 
 int slice_uninit(void)
 {
+    int ret = stm32_unconfiggpio(GPIO_SLICE_BASE_DET_N);
+    if (ret != OK)
+        return ret;
+
     return stm32_unconfiggpio(GPIO_SLICE_CORE_INT);
 }
 
 void slice_host_int_set(bool value)
 {
     stm32_gpiowrite(GPIO_SLICE_CORE_INT, value);
+}
+
+bool slice_base_det_read(void)
+{
+    return stm32_gpioread(GPIO_SLICE_BASE_DET_N);
 }
 
