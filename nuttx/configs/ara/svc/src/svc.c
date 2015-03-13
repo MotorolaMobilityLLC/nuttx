193e373a4567d47a2634addbbe0ea0edc4159645
Jean Pihet <jean.pihet@newoldbits.com>
2015-03-13 15:57:10 +0100

    SVC: Add static routing and connection tables
    In order to make development easier, provide tables with the deviceIDs
    assignments and connection parameters.
    
    Here is the help text fro svc.c:
    
     Static connections table
    
     The routing and connections setup is made of two tables:
     1) The interface to deviceID mapping table. Every interface is given by
        its name (provided in the board file). The deviceID to associate to
        the interface is freely chosen.
        Cf. the console command 'power' for the interfaces naming.
        The physical port to the switch is retrieved from the board file
        information, there is no need to supply it in the connection tables.
    
     2) The connections table, given by the deviceID and the CPort to setup, on
        both local and remote ends of the link. The routing will be setup in a
        bidirectional way.
    
     Spring interfaces placement on BDB1B/2A:
    
                                 8
                                (9)
              7     5  6
        3  2     4        1
    
    The console output looks like:
    [DBG_SVC] Setting deviceID 1 to interface apb1 (portID 0)
    [DBG_SVC] Setting deviceID 2 to interface apb2 (portID 1)
    [DBG_SVC] Creating connection [0:0]->[1:5]
    [DBG_SVC] Creating connection [0:1]->[1:4]
    
    Testing Done: Check connections setup on BDB1A/2B
    Bug Number: SW-418
    Review URL: https://reviews.leaflabs.com/r/391/
    Reviewed by: marti, alex
    
    Signed-off-by: Jean Pihet <jean.pihet@newoldbits.com>

--- nuttx/configs/ara/svc/src/svc.c
+++ nuttx/configs/ara/svc/src/svc.c
@@ -7,8 +7,11 @@
 
 #include <nuttx/config.h>
 #include <nuttx/arch.h>
+#include <nuttx/util.h>
+
 #include <arch/board/board.h>
 
+#include "string.h"
 #include "ara_board.h"
 #include "up_debug.h"
 #include "interface.h"
@@ -22,10 +25,44 @@ struct svc {
 static struct svc the_svc;
 
 /*
+ * Static connections table
+ *
+ * The routing and connections setup is made of two tables:
+ * 1) The interface to deviceID mapping table. Every interface is given by
+ *    its name (provided in the board file). The deviceID to associate to
+ *    the interface is freely chosen.
+ *    Cf. the console command 'power' for the interfaces naming.
+ *    The physical port to the switch is retrieved from the board file
+ *    information, there is no need to supply it in the connection tables.
+ *
+ * 2) The connections table, given by the deviceID and the CPort to setup, on
+ *    both local and remote ends of the link. The routing will be setup in a
+ *    bidirectional way.
+ *
+ * Spring interfaces placement on BDB1B/2A:
+ *
+ *                             8
+ *                            (9)
+ *          7     5  6
+ *    3  2     4        1
+ */
+struct svc_interface_device_id {
+    char *interface_name;       // Interface name
+    uint8_t device_id;          // DeviceID
+    uint8_t port_id;            // PortID
+    bool found;
+};
+
+struct svc_connection {
+    uint8_t local_device_id;    // Local DeviceID
+    uint8_t local_cport;        // Local CPort
+    uint8_t peer_device_id;     // Peer DeviceID
+    uint8_t peer_cport;         // Peer CPort
+};
+
+/*
  * Default routes used on BDB1B demo
  */
-#define PORT_ID_APB1            (0)
-#define PORT_ID_APB2            (1)
 #define DEV_ID_APB1             (1)
 #define DEV_ID_APB2             (2)
 #define DEMO_GPIO_APB1_CPORT    (0)
@@ -33,46 +70,97 @@ static struct svc the_svc;
 #define DEMO_I2C_APB1_CPORT     (1)
 #define DEMO_I2C_APB2_CPORT     (4)
 
-static int setup_default_routes(struct tsb_switch *sw) {
-    int rc;
+/* Interface name to deviceID mapping table */
+static struct svc_interface_device_id devid[] = {
+    { "apb1", DEV_ID_APB1 },
+    { "apb2", DEV_ID_APB2 },
+};
 
-    /* Setup hard-coded default routes for greybus testing */
-    rc = switch_if_dev_id_set(sw, PORT_ID_APB1, DEV_ID_APB1);
-    if (rc) {
-        dbg_error("Failed to assign device id %u to port %u\n",
-                  DEV_ID_APB1,
-                  PORT_ID_APB1);
-        return -1;
+/* Connections table */
+static struct svc_connection conn[] = {
+    // APB1, CPort 0 <-> APB2, CPort 5, for GPIO
+    { DEV_ID_APB1, DEMO_GPIO_APB1_CPORT, DEV_ID_APB2, DEMO_GPIO_APB2_CPORT },
+    // APB1, CPort 1 <-> APB2, CPort 4, for I2C
+    { DEV_ID_APB1, DEMO_I2C_APB1_CPORT, DEV_ID_APB2, DEMO_I2C_APB2_CPORT },
+};
+
+
+static int setup_default_routes(struct tsb_switch *sw) {
+    int i, j, rc;
+    uint8_t port_id_local, port_id_peer;
+    bool port_id_local_found, port_id_peer_found;
+    struct interface *iface;
+
+    /*
+     * Setup hard-coded default routes from the routing and
+     * connection tables
+     */
+
+    /* Setup Port <-> deviceID and configure the Switch routing table */
+    for (i = 0; i < ARRAY_SIZE(devid); i++) {
+        devid[i].found = false;
+        /* Retrieve the portID from the interface name */
+        interface_foreach(iface, j) {
+            if (!strcmp(iface->name, devid[i].interface_name)) {
+                devid[i].port_id = iface->switch_portid;
+                devid[i].found = true;
+
+                dbg_info("Setting deviceID %d to interface %s (portID %d)\n",
+                         devid[i].device_id, devid[i].interface_name,
+                         devid[i].port_id);
+
+                rc = switch_if_dev_id_set(sw, devid[i].port_id,
+                                          devid[i].device_id);
+                if (rc) {
+                    dbg_error("Failed to assign deviceID %u to interface %s\n",
+                              devid[i].device_id, devid[i].interface_name);
+                    continue;
+                }
+            }
+        }
     }
-    rc = switch_if_dev_id_set(sw, PORT_ID_APB2, DEV_ID_APB2);
-    if (rc) {
-        dbg_error("Failed to assign device id %u to port %u\n",
-                  DEV_ID_APB2,
-                  PORT_ID_APB2);
-        return -1;
+
+    /* Connections setup */
+    for (i = 0; i < ARRAY_SIZE(conn); i++) {
+        /* Look up local and peer portIDs for the given deviceIDs */
+        port_id_local = port_id_peer = 0;
+        port_id_local_found = port_id_peer_found = false;
+        for (j = 0; j < ARRAY_SIZE(devid); j++) {
+            if (!devid[j].found)
+                continue;
+
+            if (devid[j].device_id == conn[i].local_device_id) {
+                port_id_local = devid[j].port_id;
+                port_id_local_found = true;
+            }
+            if (devid[j].device_id == conn[i].peer_device_id) {
+                port_id_peer = devid[j].port_id;
+                port_id_peer_found = true;
+            }
+        }
+
+        /* If found, create the requested connection */
+        if (port_id_local_found && port_id_peer_found) {
+            dbg_info("Creating connection [%u:%u]->[%u:%u]\n",
+                     port_id_local, conn[i].local_cport,
+                     port_id_peer, conn[i].peer_cport);
+            rc = switch_connection_std_create(sw,
+                                              port_id_local,
+                                              conn[i].local_cport,
+                                              port_id_peer,
+                                              conn[i].peer_cport);
+            if (rc) {
+                dbg_error("Failed to create connection [%u:%u]->[%u:%u]\n",
+                          port_id_local, conn[i].local_cport,
+                          port_id_peer, conn[i].peer_cport);
+                return -1;
+            }
+        } else {
+            dbg_error("Cannot find portIDs for deviceIDs %d and %d\n",
+                      port_id_local, port_id_peer);
+        }
     }
-    rc = switch_connection_std_create(sw,
-                                      PORT_ID_APB1,
-                                      DEMO_GPIO_APB1_CPORT,
-                                      PORT_ID_APB2,
-                                      DEMO_GPIO_APB2_CPORT);
-    if (rc) {
-        dbg_error("Failed to create connection [%u:%u]->[%u:%u]\n",
-                  PORT_ID_APB1, DEMO_GPIO_APB1_CPORT,
-                  PORT_ID_APB2, DEMO_GPIO_APB2_CPORT);
-        return -1;
-    }
-    rc = switch_connection_std_create(sw,
-                                      PORT_ID_APB1,
-                                      DEMO_I2C_APB1_CPORT,
-                                      PORT_ID_APB2,
-                                      DEMO_I2C_APB2_CPORT);
-    if (rc) {
-        dbg_error("Failed to create connection [%u:%u]->[%u:%u]\n",
-                  PORT_ID_APB1, DEMO_I2C_APB1_CPORT,
-                  PORT_ID_APB2, DEMO_I2C_APB2_CPORT);
-        return -1;
-    }
+
     switch_dump_routing_table(sw);
 
     return 0;
