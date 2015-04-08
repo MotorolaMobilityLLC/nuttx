628c36f94ad52ee3d4bcd123fee521f139e0cf6a
Mark A. Greer <mgreer@animalcreek.com>
2015-04-07 21:40:16 -0700

    device: tsb: Add device core infrastructure for TSB-based platforms
    Add device core infrastructure to automatically register
    TSB devices and device drivers.  The devices and drivers
    are registered by the GPBridge's bridge_main() routine
    before Greybus drivers Unipro is initialized.  This ensure
    that the TSB drivers have probed the devices and are available
    for use by Unipro/Greybus code.
    
    To add a device, add the relevant information to
    nuttx/arch/arm/src/tsb/tsb_device_table.c:tsb_device_table[].
    To add a driver, add a device_register_driver() call with the
    appropriate driver structure to
    nuttx/arch/arm/src/tsb/tsb_driver.c:tsb_driver_register().
    
    No devices or drivers are added now but will be shortly.
    
    Testing Done: Tested by PLL and I2S drivers
    Bug Number: SW-739
    Review URL: https://reviews.leaflabs.com/r/479/
    Reviewed by: fparent
    
    Signed-off-by: Mark A. Greer <mgreer@animalcreek.com>

--- apps/ara/gpbridge/gpbridge.c
+++ apps/ara/gpbridge/gpbridge.c
@@ -5,6 +5,8 @@
 #include <nuttx/config.h>
 
 #include <arch/tsb/unipro.h>
+#include <arch/tsb/device_table.h>
+#include <arch/tsb/driver.h>
 #include <apps/greybus-utils/utils.h>
 
 int bridge_main(int argc, char *argv[])
@@ -13,6 +15,9 @@ int bridge_main(int argc, char *argv[])
 #ifdef CONFIG_BOARD_HAVE_DISPLAY
     display_init();
 #endif
+
+    tsb_device_table_register();
+    tsb_driver_register();
 
     enable_manifest("MID-1", NULL);
     gb_unipro_init();
