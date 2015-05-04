ff462756cc13a1cca95d69ed8d969fa5c8494867
Alexandre Bailon <abailon@baylibre.com>
2015-05-04 12:56:45 +0200

    apps: fix implicit declaration of function
    apbridge and apbridge unipro use send_ap_id method
    Declare it in svc.h header.
    
    Testing Done: Build
    Bug Number: Trivial
    Review URL: https://reviews.leaflabs.com/r/619/
    Reviewed by: ptitiano, morgang

--- apps/include/greybus-utils/svc.h
+++ apps/include/greybus-utils/svc.h
@@ -33,6 +33,7 @@ void svc_register(int (*handler)(void *data, size_t size));
 void send_svc_handshake(void);
 void send_svc_event(int type, char *name, void *priv);
 int svc_handle(void *payload, int size);
+void send_ap_id(int iid);
 
 #endif
 
