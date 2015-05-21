73a4ca96ab1fe06bd7237138144e2146a7af96c2
Jean Pihet <jean.pihet@newoldbits.com>
2015-05-21 12:14:45 +0200

    Ara: SVC: Add voltage regulator framework
    Manages GPIOs associated to a regulator, keeps track of the regulator
    uses and turns it on/off when needed.
    
    Testing Done: build SVC for BDB2A
    Bug Number: SW-825
    Review URL: https://reviews.leaflabs.com/r/718/
    Reviewed by: ptitiano, perry, mbolivar
    
    Signed-off-by: Jean Pihet <jean.pihet@newoldbits.com>

---
+++ nuttx/configs/ara/svc/src/vreg.c
@@ -0,0 +1,163 @@
+/*
+ * Copyright (c) 2015 Google Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from this
+ * software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ * @brief: Functions and definitions for the voltage regulator framework
+ * @author: Jean Pihet
+ */
+
+#define DBG_COMP DBG_POWER  /* DBG_COMP macro of the component */
+
+#include <nuttx/config.h>
+#include <nuttx/arch.h>
+
+#include <errno.h>
+
+#include "stm32.h"
+#include "up_debug.h"
+#include "vreg.h"
+
+/**
+ * @brief Configure all the GPIOs associated with a voltage regulator
+ * to their default states.
+ * @param vreg regulator to configure
+ */
+int vreg_config(struct vreg *vreg) {
+    unsigned int i;
+    int rc = 0;
+
+    if (!vreg) {
+        return -ENODEV;
+    }
+
+    dbg_verbose("%s %s\n", __func__,  vreg->name ? vreg->name : "unknown");
+
+    for (i = 0; i < vreg->nr_vregs; i++) {
+        if (!&vreg->vregs[i]) {
+            rc = -EINVAL;
+            break;
+        }
+        dbg_insane("%s: %s vreg, gpio %08x\n", __func__,
+                   vreg->name ? vreg->name : "unknown",
+                   vreg->vregs[i].gpio);
+        if (stm32_configgpio(vreg->vregs[i].gpio) < 0) {
+            dbg_error("%s: Failed to configure vregs pins for regulator %s\n",
+                      __func__, vreg->name ? vreg->name : "unknown");
+            // Let other pins to be configured
+            rc = -1;
+        }
+    }
+
+    atomic_init(&vreg->use_count, 0);
+    vreg->power_state = false;
+
+    return rc;
+}
+/**
+ * @brief Manage the regulator state; Turn it on when needed
+ * @returns: 0 on success, <0 on error
+ */
+int vreg_get(struct vreg *vreg) {
+    unsigned int i, rc = 0;
+
+    if (!vreg) {
+        return -ENODEV;
+    }
+
+    dbg_verbose("%s %s\n", __func__, vreg->name ? vreg->name : "unknown");
+
+    /* Enable the regulator on the first use; Update use count */
+    if (atomic_inc(&vreg->use_count) == 1) {
+        for (i = 0; i < vreg->nr_vregs; i++) {
+            if (!&vreg->vregs[i]) {
+                rc = -EINVAL;
+                break;
+            }
+            dbg_insane("%s: %s vreg, gpio %08x to %d, hold %dus\n", __func__,
+                       vreg->name ? vreg->name : "unknown",
+                       vreg->vregs[i].gpio, !!vreg->vregs[i].active_high,
+                       vreg->vregs[i].hold_time);
+            stm32_gpiowrite(vreg->vregs[i].gpio, vreg->vregs[i].active_high);
+            up_udelay(vreg->vregs[i].hold_time);
+        }
+    }
+
+    /* Update state */
+    vreg->power_state = true;
+
+    return rc;
+}
+
+
+/**
+ * @brief Manage the regulator state; Turn it off when unused
+ * @returns: 0 on success, <0 on error
+ */
+int vreg_put(struct vreg *vreg) {
+    unsigned int i, rc = 0;
+
+    if (!vreg) {
+        return -ENODEV;
+    }
+
+    dbg_verbose("%s %s\n", __func__, vreg->name ? vreg->name : "unknown");
+
+    /* If already disabled, do nothing */
+    if (!atomic_get(&vreg->use_count))
+        return rc;
+
+    /* Disable the regulator on the last use; Update use count */
+    if (!atomic_dec(&vreg->use_count)) {
+        for (i = 0; i < vreg->nr_vregs; i++) {
+            if (!&vreg->vregs[i]) {
+                rc = -EINVAL;
+                break;
+            }
+            dbg_insane("%s: %s vreg, gpio %08x to %d\n", __func__,
+                       vreg->name ? vreg->name : "unknown",
+                       vreg->vregs[i].gpio, !vreg->vregs[i].active_high);
+            stm32_gpiowrite(vreg->vregs[i].gpio, !vreg->vregs[i].active_high);
+        }
+
+        /* Update state */
+        vreg->power_state = false;
+    }
+
+    return rc;
+}
+
+/*
+ * @brief Get interface power supply state, or false if no interface is supplied
+ */
+bool vreg_get_pwr_state(struct vreg *vreg) {
+    if (!vreg) {
+        return false;
+    }
+
+    return vreg->power_state;
+}
---
+++ nuttx/configs/ara/svc/src/vreg.h
@@ -0,0 +1,98 @@
+/*
+ * Copyright (c) 2015 Google Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from this
+ * software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ * @brief: Manage a voltage regulator
+ */
+
+#ifndef  _VREG_H_
+#define  _VREG_H_
+
+#include <arch/atomic.h>
+
+#include <errno.h>
+
+
+/* Regulator control GPIO */
+struct vreg_data {
+    unsigned int gpio;
+    unsigned int hold_time;   // Assertion duration, in us
+    unsigned int active_high; // Active-high to assert
+};
+
+/* Voltage regulator management struct */
+struct vreg {
+    const char *name;
+    struct vreg_data *vregs;
+    size_t nr_vregs;
+    bool power_state;
+    atomic_t use_count;
+};
+
+int vreg_config(struct vreg *);
+int vreg_get(struct vreg *);
+int vreg_put(struct vreg *);
+bool vreg_get_pwr_state(struct vreg *);
+
+/*
+ * Macro magic.
+ */
+#define VREG_ACTIVE_HIGH_DEFAULT_MODE   (GPIO_OUTPUT | GPIO_PUSHPULL | \
+                                         GPIO_OUTPUT_CLEAR)
+/* Spring pins are active low */
+#define VREG_ACTIVE_LOW_DEFAULT_MODE    (GPIO_OUTPUT | GPIO_PUSHPULL | \
+                                         GPIO_OUTPUT_SET)
+
+/* Helper for the generic active high regulator */
+#define INIT_ACTIVE_HIGH_VREG_DATA(g, t)                       \
+    {                                                          \
+        .gpio = (VREG_ACTIVE_HIGH_DEFAULT_MODE | g),           \
+        .hold_time = t,                                        \
+        .active_high = 1,                                      \
+    }
+
+/* Helper for the generic active low regulator */
+#define INIT_ACTIVE_LOW_VREG_DATA(g, t)                        \
+    {                                                          \
+        .gpio = (VREG_ACTIVE_LOW_DEFAULT_MODE | g),            \
+        .hold_time = t,                                        \
+        .active_high = 0,                                      \
+    }
+
+/* vreg build helper */
+#define __MAKE_VREG(n) n ## _vreg
+#define MAKE_VREG(n) __MAKE_VREG(n)
+#define DECLARE_VREG(_name, vreg_data)                         \
+    static struct vreg MAKE_VREG(_name) = {                    \
+        .name = #_name,                                        \
+        .vregs = vreg_data,                                    \
+        .nr_vregs = ARRAY_SIZE(vreg_data),                     \
+        .use_count = 0,                                        \
+    };
+
+#endif
