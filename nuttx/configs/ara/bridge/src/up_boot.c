50044818c93a472f0951fdfc8a57fd3e9695fe74
Darryl Newberry <newberry_darryl@projectara.com>
2015-06-25 12:12:45 -0600

    build: enable common firmware library in bridges
    Testing Done: built es2-debug-apbridgea and es2-debug-generic configs
    and tested on BDB2A
    Bug Number: SW-809 Add build information to all firmware binaries
    Review URL: https://reviews.leaflabs.com/r/957/
    Reviewed by: fparent
    
    Signed-off-by: Darryl Newberry newberry_darryl@projectara.com

--- nuttx/configs/ara/bridge/src/up_boot.c
+++ nuttx/configs/ara/bridge/src/up_boot.c
@@ -26,38 +26,9 @@
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <nuttx/config.h>
-#include <nuttx/ara_build_info.h>
-#include <arch/tsb/unipro.h>
-#include <debug.h>
-
-
-#ifdef CONFIG_ARCH_LOWPUTC
-#define early_dbg(fmt, ...) lowsyslog(fmt, ##__VA_ARGS__)
-#endif
-
-#ifndef ARA_FW_VERSION
-#define ARA_FW_VERSION "unknown"
-#endif
-
-#ifndef ARA_BUILD_INFO
-#define ARA_BUILD_INFO "unknown"
-#endif
+#include <ara_build_info.h>
 
 void tsb_boardinitialize(void) {
-    early_dbg("\n********************************************************************************\n");
-    early_dbg("Ara Bridge FW version: %s\n", ARA_FW_VERSION);
-    early_dbg("Build: %s\n", ARA_BUILD_INFO);
-    early_dbg("Chip Revision: %s\n", CONFIG_TSB_CHIP_REV);
-
-#if defined CONFIG_BOOT_COPYTORAM
-    early_dbg("Boot type: SPI flash copy to RAM\n");
-#elif defined CONFIG_BOOT_RUNFROMISRAM
-    early_dbg("Boot type: Internal SRAM\n");
-#else
-#error "Unsupported configuration"
-
-#endif
-    early_dbg("********************************************************************************\n");
+    ara_show_build_info();
 }
 
