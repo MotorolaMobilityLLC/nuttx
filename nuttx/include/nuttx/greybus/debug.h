b9c00d401eacc4ae5b1f9c8808c7a414f39396d4
Alexandre Bailon <abailon@baylibre.com>
2015-07-08 11:39:28 +0200

    greybus log: fix whitespace and coding style
    Testing Done: trivial
    Bug Number: SW-617
    Review URL: https://reviews.leaflabs.com/r/1047/
    Reviewed by: mbolivar
    
    Signed-off-by: Alexandre Bailon <abailon@baylibre.com>

--- nuttx/include/nuttx/greybus/debug.h
+++ nuttx/include/nuttx/greybus/debug.h
@@ -36,33 +36,34 @@
 #ifdef CONFIG_GREYBUS_DEBUG
 static inline void _gb_dump(const char *func, __u8 *buf, size_t size)
 {
-	  int i;
+    int i;
     irqstate_t flags;
 
     flags = irqsave();
     lowsyslog("%s:\n", func);
-	  for (i = 0; i < size; i++)
-		  lowsyslog( "%02x ", buf[i]);
-	  lowsyslog("\n");
-	  irqrestore(flags);
+    for (i = 0; i < size; i++) {
+        lowsyslog( "%02x ", buf[i]);
+    }
+    lowsyslog("\n");
+    irqrestore(flags);
 }
 
 #define gb_dump(buf, size) \
-        _gb_dump(__func__, buf, size)
+    _gb_dump(__func__, buf, size)
 #define gb_debug(fmt, ...) \
-        do { lowsyslog("[D] GB: " fmt, ##__VA_ARGS__); } while (0)
+    do { lowsyslog("[D] GB: " fmt, ##__VA_ARGS__); } while (0)
 #else
 #define gb_dump(buf, size) \
-        do { } while (0)
+    do { } while (0)
 #define gb_debug(fmt, ...) \
-        do { } while (0)
+    do { } while (0)
 #endif
 
 /* debug/info/error macros */
 #define gb_info(fmt, ...)                                            \
-        do { lowsyslog("[I] GB: " fmt, ##__VA_ARGS__); } while (0)
+    do { lowsyslog("[I] GB: " fmt, ##__VA_ARGS__); } while (0)
 #define gb_error(fmt, ...)                                           \
-        do { lowsyslog("[E] GB: " fmt, ##__VA_ARGS__); } while (0)
+    do { lowsyslog("[E] GB: " fmt, ##__VA_ARGS__); } while (0)
 
 #endif
 
