99af2d19fcd55773ae01ebf1b079673f7ba4c4e9
Fabien Parent <fparent@baylibre.com>
2015-07-27 15:25:57 +0200

    ara: bridge: add bdb board file
    This commit adds a board file for the bridges. It is also registering board
    drivers (no driver is added in this commit).
    
    Testing Done: Tested thanks to an upcoming commit.
    Bug Number: trivial
    Review URL: https://reviews.leaflabs.com/r/1219/
    Reviewed by: mgreer, gregkh
    
    Signed-off-by: Fabien Parent <fparent@baylibre.com>

---
+++ nuttx/configs/ara/bridge/src/board-bdb.c
@@ -0,0 +1,57 @@
+/**
+ * Copyright (c) 2015 Google Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from this
+ * software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Author: Fabien Parent <fparent@baylibre.com>
+ */
+
+#include <nuttx/config.h>
+#include <nuttx/device.h>
+#include <nuttx/device_table.h>
+#include <nuttx/util.h>
+
+#ifdef CONFIG_DEVICE_CORE
+static struct device devices[] = {
+};
+
+static struct device_table bdb_device_table = {
+    .device = devices,
+    .device_count = ARRAY_SIZE(devices),
+};
+
+static void bdb_driver_register(void)
+{
+}
+#endif
+
+void board_initialize(void)
+{
+#ifdef CONFIG_DEVICE_CORE
+    device_table_register(&bdb_device_table);
+
+    bdb_driver_register();
+#endif
+}
