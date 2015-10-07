f19ebb696b2bf8db2a3ecd8a83dcb63e300b9068
Greg Meiste <w30289@motorola.com>
2015-10-07 10:30:38 -0500

    arm: stm32: Port Google's high resolution timer from TSB to STM32
    Change-Id: I3398c31ac9345d22785bd99c1e6378e8dafa73c4
    Signed-off-by: Greg Meiste <w30289@motorola.com>
    Reviewed-on: http://gerrit.mot.com/789290
    SME-Granted: SME Approvals Granted
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Reviewed-by: James Wylder <jwylder@motorola.com>

--- nuttx/arch/arm/src/stm32/stm32_timerisr.c
+++ nuttx/arch/arm/src/stm32/stm32_timerisr.c
@@ -1,6 +1,7 @@
 /****************************************************************************
  * arch/arm/src/stm32/stm32_timerisr.c
  *
+ *   Copyright (c) 2014-2015 Google, Inc.
  *   Copyright (C) 2009 Gregory Nutt. All rights reserved.
  *   Author: Gregory Nutt <gnutt@nuttx.org>
  *
@@ -85,13 +86,101 @@
 #  error SYSTICK_RELOAD exceeds the range of the RELOAD register
 #endif
 
+#define USEC_CLOCK_COUNT  (STM32_SYSCLK_FREQUENCY / USEC_PER_SEC)
+
+/*
+ * Timer counts down from SYSTICK_RELOAD to 0 ,'(SYSTICK_RELOAD - clk)'
+ * calculates clock count since reload, '+ (USEC_CLOCK_COUNT/2)' is used
+ * for rounding.
+ */
+#define CLOCK_COUNT2USEC(clk) \
+    (((SYSTICK_RELOAD - (clk)) + (USEC_CLOCK_COUNT / 2)) / USEC_CLOCK_COUNT)
+
+#define SYSTICK_TILL_USEC_ROLLOVER   (0xffffffff / USEC_PER_TICK)
+
+/*
+ * Nano is special because there are 10.41667 nanoseconds per clock tick.
+ * Applying the same procedure as above would lead to large rounding errors.
+ * Use scaling to reduce rounding errors for CLOCK_COUNT2NSEC.
+ *
+ * Use the largest possible scaling value to ensure there's no overflow of
+ * the 32bit value.
+ */
+#define MAX_NANO_SCALING ((0xffffffff / SYSTICK_RELOAD) - 1)
+
+/* Ensure NANO_SCALING_VALUE is not greater than MAX_NANO_SCALING. */
+#define UNSCALED_NANO_PER_CLOCK ((NSEC_PER_SEC / STM32_SYSCLK_FREQUENCY) + 1)
+#define NANO_SCALING_VALUE      (MAX_NANO_SCALING / UNSCALED_NANO_PER_CLOCK)
+
+/*
+ * Now that we have the largest possible scaling value apply it to
+ * NANO_PER_CLOCK value.
+ */
+#define SCALED_NANO_PER_CLOCK \
+            (NSEC_PER_SEC / (STM32_SYSCLK_FREQUENCY/NANO_SCALING_VALUE))
+
+#define CLOCK_COUNT2NSEC(clk) \
+    (((SYSTICK_RELOAD - (clk)) * SCALED_NANO_PER_CLOCK) / NANO_SCALING_VALUE)
+
 /****************************************************************************
  * Private Types
  ****************************************************************************/
 
+static volatile bool htr_rollover = false;
+
 /****************************************************************************
  * Private Function Prototypes
  ****************************************************************************/
+
+/*
+ * Ensure that snapshots of timer values account for rollover of the clock.
+ *
+ * When not in interrupt context:
+ *   If rollover occurs after reading clock_value then tick_count may or may
+ *   not be valid. Reading the values again, just after the rollover, will
+ *   ensure they are correct.
+ *
+ * When in interrupt context detecting an accurate tick_count becomes more
+ * difficult:
+ *   - once in interrupt context the global tickcount will not change until
+ *     processed by the systick ISR
+ *   - rollover can happen any time before or during the current ISR
+ *   - once rollover is detected the tickcount value must be corrected
+ *   - hrt_rollover must track if rollover had previously occurred
+ *
+ * timer_snapshot() is interrupt context safe.
+ */
+static inline void timer_snapshot(uint32_t *tick_count, uint32_t *clock_value)
+{
+    uint32_t systick_ctrl, ticks, clock;
+
+    if (up_interrupt_context()) {
+        systick_ctrl = getreg32(NVIC_SYSTICK_CTRL);
+        if (systick_ctrl & NVIC_SYSTICK_CTRL_COUNTFLAG) {
+            htr_rollover = true;
+        }
+
+        clock = getreg32(NVIC_SYSTICK_CURRENT);
+        ticks = clock_systimer();
+        if (clock < getreg32(NVIC_SYSTICK_CURRENT)) {
+            clock = getreg32(NVIC_SYSTICK_CURRENT);
+            ticks++;
+        } else if (htr_rollover) {
+            ticks++;
+        }
+    } else {
+        clock = getreg32(NVIC_SYSTICK_CURRENT);
+        ticks = clock_systimer();
+
+        if (clock < getreg32(NVIC_SYSTICK_CURRENT)) {
+            clock = getreg32(NVIC_SYSTICK_CURRENT);
+            ticks = clock_systimer();
+        }
+    }
+
+    *tick_count = ticks;
+    *clock_value = clock;
+}
 
 /****************************************************************************
  * Global Functions
@@ -111,6 +200,11 @@ int up_timerisr(int irq, uint32_t *regs)
    /* Process timer interrupt */
 
    sched_process_timer();
+
+   htr_rollover = false;
+   /* Clear the clock rollover bit */
+   getreg32(NVIC_SYSTICK_CTRL);
+
    return 0;
 }
 
@@ -161,4 +255,35 @@ void up_timer_initialize(void)
   /* And enable the timer interrupt */
 
   up_enable_irq(STM32_IRQ_SYSTICK);
+}
+
+/*
+ * Return seconds and nanoseconds since the last reboot of the CPU. This
+ * function is interrupt context safe.
+ */
+void hrt_gettimespec(struct timespec *ts)
+{
+    uint32_t clock_value, tick_count;
+
+    timer_snapshot(&tick_count, &clock_value);
+
+    ts->tv_sec = tick_count / CLOCKS_PER_SEC;
+    ts->tv_nsec = CLOCK_COUNT2NSEC(clock_value);
+    ts->tv_nsec += (tick_count % CLOCKS_PER_SEC) * NSEC_PER_TICK;
+}
+
+/*
+ * Return the micro seconds since the last reboot of the CPU. This function
+ * is interrupt context safe.
+ */
+uint32_t hrt_getusec(void)
+{
+    uint32_t usec, clock_value, tick_count;
+
+    timer_snapshot(&tick_count, &clock_value);
+
+    usec = CLOCK_COUNT2USEC(clock_value);
+    usec += ((tick_count % SYSTICK_TILL_USEC_ROLLOVER) * USEC_PER_TICK);
+
+    return usec;
 }
