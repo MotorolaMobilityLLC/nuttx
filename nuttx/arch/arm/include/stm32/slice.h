7340c14139ac309968e34a2ca3fda1cb4c05d285
Greg Meiste <w30289@motorola.com>
2015-10-12 13:25:52 -0500

    stm32: slice: Add Motorola vendor specific Greybus driver for charging base
    Change-Id: I92abddb21f992a4f9ead8a70d1043e192f5dbfe1
    Signed-off-by: Jim Wylder <jwylder@motorola.com>

--- nuttx/arch/arm/include/stm32/slice.h
+++ nuttx/arch/arm/include/stm32/slice.h
@@ -7,10 +7,13 @@
 #ifndef _STM32_SLICE_H_
 #define _STM32_SLICE_H_
 
+#include <nuttx/irq.h>
+
 int slice_init(xcpt_t irqhandler);
 int slice_uninit(void);
 void slice_host_int_set(bool value);
 bool slice_base_det_read(void);
+void slice_vbus_en_sw(bool value);
 
 #endif /* _STM32_SLICE_H_ */
 
--- nuttx/arch/arm/src/stm32/stm32_slice.c
+++ nuttx/arch/arm/src/stm32/stm32_slice.c
@@ -7,22 +7,45 @@
 #include <arch/board/board.h>
 #include <arch/stm32/slice.h>
 
+#include <unistd.h>
+
 int slice_init(xcpt_t irqhandler)
 {
     int ret = stm32_configgpio(GPIO_SLICE_CORE_INT);
+    if (ret != OK)
+        return ret;
 
-    if (ret == OK)
-      {
-        // The gpio is configured inside stm32_gpiosetevent
-        stm32_gpiosetevent(GPIO_SLICE_BASE_DET_N, true, true, true, irqhandler);
-      }
+    ret = stm32_configgpio(GPIO_SLICE_SL_VBUS_EN_SW);
+    if (ret != OK)
+        goto vbus_en_sw_failed;
 
+    ret = stm32_configgpio(GPIO_SLICE_CHG_CE_N);
+    if (ret != OK)
+        goto chg_ce_n_failed;
+
+    // The gpio is configured inside stm32_gpiosetevent
+    stm32_gpiosetevent(GPIO_SLICE_BASE_DET_N, true, true, true, irqhandler);
+
+    return OK;
+
+chg_ce_n_failed:
+    stm32_unconfiggpio(GPIO_SLICE_SL_VBUS_EN_SW);
+vbus_en_sw_failed:
+    stm32_unconfiggpio(GPIO_SLICE_CORE_INT);
     return ret;
 }
 
 int slice_uninit(void)
 {
     int ret = stm32_unconfiggpio(GPIO_SLICE_BASE_DET_N);
+    if (ret != OK)
+        return ret;
+
+    ret = stm32_unconfiggpio(GPIO_SLICE_SL_VBUS_EN_SW);
+    if (ret != OK)
+        return ret;
+
+    ret = stm32_unconfiggpio(GPIO_SLICE_CHG_CE_N);
     if (ret != OK)
         return ret;
 
@@ -37,5 +60,22 @@ void slice_host_int_set(bool value)
 bool slice_base_det_read(void)
 {
     return stm32_gpioread(GPIO_SLICE_BASE_DET_N);
+}
+
+void slice_vbus_en_sw(bool value)
+{
+    // Enable disable slice charging based on whether slice is charging base
+    if (value)
+        stm32_gpiowrite(GPIO_SLICE_CHG_CE_N, value);
+
+    stm32_gpiowrite(GPIO_SLICE_SL_VBUS_EN_SW, value);
+
+    if (!value)
+      {
+        // To avoid brief moment of charging (and therefore a brief flash of
+        // the green LED, wait before re-enabling slice charging.
+        usleep(100000);
+        stm32_gpiowrite(GPIO_SLICE_CHG_CE_N, value);
+      }
 }
 
