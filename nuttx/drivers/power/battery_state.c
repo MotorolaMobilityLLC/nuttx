7cc2a8a2ac3dd1070dbe3056dc7234c8a06280cb
Dmitriy Gruzman <dgruzman@motorola.com>
2015-10-20 09:56:47 -0500

    power: Add battery temp/level notifier
    Add capability to register callbacks to be notified about changes in battery
    level and temperature.
    
    Change-Id: I0f765c851c2fad6f3f441953a06f463fb47b06d8
    Signed-off-by: Dmitriy Gruzman <dgruzman@motorola.com>
    Reviewed-on: http://gerrit.mot.com/794264
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Gregory Meiste <w30289@motorola.com>
    Reviewed-by: James Wylder <jwylder@motorola.com>

---
+++ nuttx/drivers/power/battery_state.c
@@ -0,0 +1,156 @@
+/*
+ * Copyright (C) 2015 Motorola Mobility, LLC.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from this
+ * software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <debug.h>
+#include <errno.h>
+#include <semaphore.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+#include <nuttx/power/battery_state.h>
+#include <nuttx/list.h>
+
+static sem_t sem = SEM_INITIALIZER(1);
+
+struct notify_node_s
+{
+    batt_callback_t callback;
+    void *arg;
+    struct list_head list;
+};
+
+struct battery_info_s
+{
+    struct batt_state_s batt;
+    struct list_head notify_list;
+};
+
+static struct battery_info_s *g_info;
+
+#ifdef CONFIG_BATTERY_STATE_SIMULATOR
+static void battery_notify(void)
+{
+    struct notify_node_s *node;
+    struct list_head *iter;
+
+    list_foreach(&g_info->notify_list, iter) {
+        node = list_entry(iter, struct notify_node_s, list);
+        node->callback(node->arg, &g_info->batt);
+    }
+}
+
+int set_battery_level(enum batt_level_e level)
+{
+    while (sem_wait(&sem) != OK) {
+        if (errno == EINVAL) {
+            return -EINVAL;
+        }
+    }
+
+    g_info->batt.level = level;
+    battery_notify();
+
+    sem_post(&sem);
+    return 0;
+}
+
+int set_battery_temp(enum batt_temp_e temp)
+{
+    while (sem_wait(&sem) != OK) {
+        if (errno == EINVAL) {
+            return -EINVAL;
+        }
+    }
+
+    g_info->batt.temp = temp;
+    battery_notify();
+
+    sem_post(&sem);
+    return 0;
+}
+#endif
+
+int battery_state_register(batt_callback_t callback, void *arg)
+{
+    struct notify_node_s *node;
+    int retval = 0;
+
+    while (sem_wait(&sem) != OK) {
+        if (errno == EINVAL) {
+            return -EINVAL;
+        }
+    }
+
+    if (!g_info) {
+        retval = -EAGAIN;
+        goto register_done;
+    }
+
+    node = zalloc(sizeof(*node));
+    if (!node) {
+        retval = -ENOMEM;
+        goto register_done;
+    }
+
+    node->callback = callback;
+    node->arg = arg;
+    list_add(&g_info->notify_list, &node->list);
+    callback(arg, &g_info->batt);
+
+register_done:
+    sem_post(&sem);
+    return retval;
+}
+
+int battery_state_init(void)
+{
+    while (sem_wait(&sem) != OK) {
+        if (errno == EINVAL) {
+            dbg("failed to acquire semaphore\n");
+            return -EINVAL;
+        }
+    }
+
+    if (g_info)
+        goto done;
+
+    g_info = zalloc(sizeof(*g_info));
+    if (!g_info) {
+        dbg("failed to allocate memory\n");
+        return -ENOMEM;
+    }
+
+    list_init(&g_info->notify_list);
+
+    g_info->batt.temp = BATTERY_TEMP_NORMAL;
+    g_info->batt.level = BATTERY_LEVEL_NORMAL;
+
+done:
+    sem_post(&sem);
+    return 0;
+}
---
+++ nuttx/include/nuttx/power/battery_state.h
@@ -0,0 +1,95 @@
+/*
+ * Copyright (C) 2015 Motorola Mobility, LLC.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from this
+ * software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef __INCLUDE_NUTTX_POWER_BATTERY_STATE_H
+#define __INCLUDE_NUTTX_POWER_BATTERY_STATE_H
+
+/*
+ * Battery temperature levels
+ */
+enum batt_temp_e {
+    BATTERY_TEMP_NORMAL,
+    BATTERY_TEMP_REDUCED_CHARGING,
+    BATTERY_TEMP_NO_CHARGING,
+    BATTERY_TEMP_NO_DISCHARGING, /* and no charging! */
+};
+
+/*
+ * Battery capacity levels
+ */
+enum batt_level_e {
+    BATTERY_LEVEL_EMPTY,
+    BATTERY_LEVEL_NORMAL,
+    BATTERY_LEVEL_FULL,
+};
+
+/*
+ * Battery state
+ */
+struct batt_state_s {
+    enum batt_temp_e temp;
+    enum batt_level_e level;
+};
+
+/* The type of the battery state callback function */
+typedef void (*batt_callback_t)(void *arg, const struct batt_state_s *batt);
+
+/****************************************************************************
+ * Name: battery_state_init
+ *
+ * Description:
+ *   Initialize battery state changes notifications.
+ *
+ * Input Parameter:
+ *   (None)
+ *
+ * Returned Value:
+ *   0 on success or negative errno on failure
+ *
+ ****************************************************************************/
+
+int battery_state_init(void);
+
+/****************************************************************************
+ * Name: battery_state_register
+ *
+ * Description:
+ *   Register for battery state changes notifications.
+ *
+ * Input Parameters:
+ *   callback - Function to call on changes in battery state.
+ *   arg      - Parameter to pass to callback function.
+ *
+ * Returned Value:
+ *   0 on success or negative errno on failure
+ *
+ ****************************************************************************/
+
+int battery_state_register(batt_callback_t callback, void *arg);
+
+#endif /* __INCLUDE_NUTTX_POWER_BATTERY_STATE_H */
