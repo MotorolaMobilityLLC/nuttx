d8b15cc7eaa7699966de2079da10ff62eccfa52d
Mike Corrigan <corrigan@motorola.com>
2015-11-20 13:46:23 -0600

    greybus: cdsi and camera: Clean-up
    Rename files, functions, and NSH commands to indicate either
    display or camera.
    
    Use the camera_sensor struct for the camera.
    
    Add copyright notices.
    
    Change-Id: I183dbaeaf7c38b0137b6cbdc23ebe44224495a7f
    Signed-off-by: Jim Wylder <jwylder@motorola.com>

--- nuttx/drivers/greybus/csi_camera-gb.c
+++ nuttx/drivers/greybus/csi_camera-gb.c
@@ -1,6 +1,29 @@
 /*
- * Copyright (c) 2015 Motorola Mobility
+ * Copyright (c) 2015 Motorola Mobility, LLC.
  * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from this
+ * software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 #include <errno.h>
@@ -102,7 +125,7 @@ static uint8_t gb_camera_power_on(struct gb_operation *operation)
     int retval = cam_power_on(&cam_dev);
     //TODO:stop cdsi cam tx when power off camera
     //TODO: when detached, ensure camera is stop and off
-    cdsi_camrx_start();
+    cdsi_camera_rx_start();
     lldbg("%s: result %d\n", __func__, retval);
     return gb_operation_errno_map(retval);
 }
