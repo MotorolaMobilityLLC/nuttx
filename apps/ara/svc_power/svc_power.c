7a6f5be9f172b5324da9ac46fbddd40a8d40a061
Marti Bolivar <mbolivar@leaflabs.com>
2015-11-20 00:14:43 -0500

    apps: ara: power: rewrite and update app
    Rewrite the app to use the same style as the 'svc' app, which allows for
    adding subcommands more easily.
    
    Change the usage a bit correspondingly. The new subcommands
    are (analogously to existing ones):
    
        power [h|help]: print this usage and exit
        power [p|power]: get/set interface power
        power [w|wakeout]: pulse WAKEOUT
    
    Note this means 'asserting' wake out is no longer possible. This hasn't
    been needed since the ES1 modules, so removing that is OK.
    
    Subcommand usage for 'power p':
    
        nsh> power p
        power power <interface> <0|1>: usage:
            <interface>: Interface to set power state of.
            <0|1>: specify "0" to power off, "1" to power on.
    
        NOTE: This may interfere with the power subsystem's
              refcounting. Use only if you know what you're doing.
    
    For 'power w':
    
        nsh> power w
        power wakeout <interface>: usage:
           <interface>: Interface to send WAKEOUT to.
    
    Each of the subcommand usages prints help for valid <interface>
    arguments like so (on DB3):
    
        Legal <interface> values on this board:
          "all" -- all interfaces
          apb1  (switch port 3)
          apb2  (switch port 1)
          module_1      (switch port 13)
          module_2      (switch port 11)
          module_3      (switch port 4)
          module_4a     (switch port 8)
          module_4b     (switch port 6)
          module_5      (switch port 10)
    
    Testing Done: tested various combinations of arguments on DB3
    Bug Number: SW-1778
    Review URL: https://reviews.leaflabs.com/r/2053/
    Reviewed by: Jean Pihet
    
    Signed-off-by: Marti Bolivar <mbolivar@leaflabs.com>

--- apps/ara/svc_power/svc_power.c
+++ apps/ara/svc_power/svc_power.c
@@ -32,10 +32,194 @@
 #include <nuttx/config.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "up_power.h"
 #include "interface.h"
+#include "tsb_switch.h"
 
+static const char *progname = PROGNAME;
+
+/*
+ * Helpers etc.
+ */
+
+enum {
+    HELP,
+    SET_POWER,
+    WAKEOUT,
+
+    MAX_CMD,
+};
+
+struct command {
+    const char shortc;
+    const char *longc;
+    const char *help;
+    int (*command_func)(int argc, char *argv[]);
+};
+
+static int cmd_usage(int argc, char *argv[]);
+static int cmd_set_power(int argc, char *argv[]);
+static int cmd_wakeout(int argc, char *argv[]);
+
+static const struct command commands[] = {
+    [HELP] = {'h', "help", "print this usage and exit", cmd_usage},
+    [SET_POWER] = {'p', "power", "get/set interface power", cmd_set_power},
+    [WAKEOUT] = {'w', "wakeout", "pulse WAKEOUT", cmd_wakeout},
+};
+
+static void print_interface_usage(void)
+{
+    int i;
+    struct interface *iface;
+    printf("\nLegal <interface> values on this board:\n");
+
+    printf("  \"all\" -- all interfaces\n");
+    interface_foreach(iface, i) {
+        printf("  %s", iface->name);
+        if (iface->switch_portid != INVALID_PORT) {
+            printf("\t(switch port %d)\n", iface->switch_portid);
+        } else {
+            printf("\t(no switch port)\n");
+        }
+    }
+}
+
+__attribute__((noreturn))
+static void usage(int exit_status)
+{
+    int i;
+    printf("%s: usage:\n", progname);
+    for (i = 0; i < MAX_CMD; i++) {
+        printf("    %s [%c|%s]: %s\n",
+               progname,
+               commands[i].shortc, commands[i].longc, commands[i].help);
+    }
+    exit(exit_status);
+}
+
+/*
+ * Generic "do something to an interface or all interfaces" helper.
+ *
+ * The interfaces on this board are searched for one named
+ * "iface_name". (As a special case, "name" may be "all" to specify
+ * all interfaces).
+ *
+ * The function iface_func is then called for each interface found in
+ * this way, and is handed the given context each time. The first
+ * nonzero value returned from iface_func is returned immediately.
+ *
+ * If iface_func returns 0 each time it is called, this function
+ * returns 0.
+ *
+ * If no interface can be found, this function prints an error and
+ * returns a negative errno.
+ */
+static int do_to_iface(char *iface_name,
+                       int (*iface_func)(struct interface*, void*),
+                       void *context)
+{
+    int i;
+    int rc;
+    struct interface *iface;
+    if (!strcmp(iface_name, "all") || !strcmp(iface_name, "ALL")) {
+        iface = NULL; /* NULL == "all interfaces" */
+    } else {
+        iface = interface_get_by_name(iface_name);
+        if (!iface) {
+            printf("Invalid interface: %s\n", iface_name);
+            print_interface_usage();
+            return -EINVAL;
+        }
+    }
+    if (iface) {
+        rc = iface_func(iface, context);
+    } else {
+        interface_foreach(iface, i) {
+            rc = iface_func(iface, context);
+            if (rc) {
+                break;
+            }
+        }
+    }
+    return rc;
+}
+
+/*
+ * Usage
+ */
+
+static int cmd_usage(int argc, char *argv[])
+{
+    usage(EXIT_SUCCESS);
+}
+
+/*
+ * Interface power control
+ */
+
+static void set_power_usage(int exit_status)
+{
+    printf("%s %s <interface> <0|1>: usage:\n",
+           progname, commands[SET_POWER].longc);
+    printf("    <interface>: Interface to set power state of.\n");
+    printf("    <0|1>: specify \"0\" to power off, \"1\" to power on.\n");
+    printf("\n");
+    printf("NOTE: This may interfere with the power subsystem's\n"
+           "      refcounting. Use only if you know what you're doing.\n");
+    print_interface_usage();
+    exit(exit_status);
+}
+
+static int set_power_func(struct interface *iface, void *context)
+{
+    int enable = (int)context;
+    return enable ? interface_pwr_enable(iface) : interface_pwr_disable(iface);
+}
+
+static int cmd_set_power(int argc, char *argv[])
+{
+    void *context;
+    int enable;
+    if (argc != 4) {
+        set_power_usage(EXIT_FAILURE);
+    }
+    enable = strtol(argv[3], NULL, 10);
+    context = (void*)enable;
+    return do_to_iface(argv[2], set_power_func, context);
+}
+
+/*
+ * Wake out
+ */
+
+static void wakeout_usage(int exit_status)
+{
+    printf("%s %s <interface>: usage:\n",
+           progname, commands[WAKEOUT].longc);
+    printf("   <interface>: Interface to send WAKEOUT to.\n");
+    print_interface_usage();
+    exit(exit_status);
+}
+
+static int wakeout_func(struct interface *iface, void *context)
+{
+    (void)context;
+    return interface_generate_wakeout(iface, false);
+}
+
+static int cmd_wakeout(int argc, char *argv[])
+{
+    if (argc != 3) {
+        wakeout_usage(EXIT_FAILURE);
+    }
+    return do_to_iface(argv[2], wakeout_func, NULL);
+}
+
+/*
+ * main()
+ */
 
 #ifdef CONFIG_BUILD_KERNEL
 int main(int argc, FAR char *argv[])
@@ -43,84 +227,18 @@ int main(int argc, FAR char *argv[])
 int power_main(int argc, char *argv[])
 #endif
 {
-    uint32_t int_nr, state;
+    const char *cmd_str;
     int i;
-    char cmd;
-    struct interface *iface;
 
     if (argc < 2) {
-        printf("Power: Usage:\n");
-        printf("  power p [interface_nr 0/1]  : get/set interface power\n");
-        printf("  power w interface_nr 0/1    : pulse/assert WAKEOUT\n");
-        printf("\n interface_nr is defined as:\n");
-        printf("  index\tname\n");
-
-        interface_foreach(iface, i) {
-            printf("  %02d\t%s\n", i, iface->name);
-        }
-        return ERROR;
-    } else {
-        cmd = argv[1][0];
+        usage(EXIT_FAILURE);
     }
-
-    switch (cmd) {
-    case 'p':
-        if (argc == 2) {
-            /* Get the power states */
-            interface_foreach(iface, i) {
-                printf("Power: Interface(%02d) %s state = %d\n",
-                       i,
-                       iface->name,
-                       interface_get_pwr_state(iface));
-            }
-            return 0;
+    cmd_str = argv[1];
+    for (i = 0; i < MAX_CMD; i++) {
+        if (!strcmp(cmd_str, commands[i].longc) ||
+            (strlen(cmd_str) == 1 && cmd_str[0] == commands[i].shortc)) {
+            return commands[i].command_func(argc, argv);
         }
-        if (argc > 3) {
-            /* Set the power state */
-            int_nr = strtol(argv[2], NULL, 10);
-            state = strtol(argv[3], NULL, 10);
-
-            iface = interface_get(int_nr);
-            if (!iface)
-                break;
-
-            printf("Power: set interface(%02d) %s state to %d\n",
-                   int_nr, iface->name, state);
-
-            return state ? interface_pwr_enable(iface) :
-                           interface_pwr_disable(iface);
-        }
-
-        printf("Power: wrong command\n");
-        return ERROR;
-
-    case 'w':
-        if (argc == 2) {
-            printf("Power: please provide an interface_nr and assert values\n");
-            return ERROR;
-        }
-        if (argc >= 4) {
-            /* Generate WAKEOUT pulse on the given interface */
-            int_nr = strtol(argv[2], NULL, 10);
-            state = strtol(argv[3], NULL, 10);
-
-            iface = interface_get(int_nr);
-            if (!iface)
-                break;
-
-            printf("Power: %s WAKEOUT on interface(%02d) %s\n",
-                   state ? "assert" : "pulse", int_nr, iface->name);
-
-            return interface_generate_wakeout(iface, state);
-        }
-
-        printf("Power: wrong command\n");
-        return ERROR;
-
-    default:
-        printf("Power: wrong command\n");
-        return ERROR;
     }
-
-    return 0;
+    usage(EXIT_FAILURE);
 }
