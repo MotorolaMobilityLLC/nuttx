f99bc166a4a4b6d8dcc0d066df16c5b2aa6c16dc
Fabien Parent <fparent@baylibre.com>
2015-11-26 15:37:46 +0100

    tsb: unipro: remove es2 from unipro filenames
    The UniPro driver for ES3 will be mostly similar to the es2 one. In preparation
    for ES3, lets rename the unipro driver to remove the es2 reference in the
    filename.
    
    Testing Done: compiled.
    Bug Number: trivial
    Review URL: https://reviews.leaflabs.com/r/2116/
    Reviewed by: mbolivar
    
    Signed-off-by: Fabien Parent <fparent@baylibre.com>

--- nuttx/arch/arm/src/tsb/tsb_unipro.c
+++ nuttx/arch/arm/src/tsb/tsb_unipro.c
@@ -25,7 +25,7 @@
  * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- * @brief MIPI UniPro stack for ES2 Bridges
+ * @brief MIPI UniPro stack for Bridges
  */
 
 #include <string.h>
