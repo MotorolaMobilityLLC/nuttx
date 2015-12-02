MotorolaMobilityLLC/nuttx

commit d84467fa92f45f31835c2bd66702ee78a06ba437

gb_timestamp: don't add timestamp if GPBridge responds to AP
The timestamping is done regardless of the source of the packet. Don't
timestamp if the message was sent by GPBridge, as it corrupts the data
and makes loopback drop the package.

Testing Done: gbl application works again
Bug Number: SW-1719
Review URL: https://reviews.leaflabs.com/r/2031/
Reviewed by: Fabien Parent, Patrick Titiano

Signed-off-by: Bartosz Golaszewski <bgolaszewski@baylibre.com>

nuttx/configs/ara/common/src/greybus_timestamp.c
@@ -78,12 +78,16 @@ void gb_timestamp_tag_exit_time(struct gb_timestamp *ts,
 void gb_timestamp_log(struct gb_timestamp *ts, unsigned int cportid,
                       void *payload, size_t len, int id)
 {
+    struct gb_operation_hdr *hdr = payload;
     struct gb_loopback_transfer_response *resp = payload +
                                                  sizeof(struct gb_operation_hdr);
     __le32 latency;
 
     if (len < TAG_SIZE)
         return;
+
+    if (hdr->type & GB_TYPE_RESPONSE_FLAG)
+        return; /* Bail-out if GPBridge is responding to the AP. */
 
     if (tag_active(ts, cportid)) {
         latency = calc_latency(&ts->entry_time,

commit ebb06fbf065fd47c9329e88dab357f61ca95e221

gb_loopback: fix timestamps
Commit d84467f broke the timestamping mechanism by bailing out from
gb_timestamp_log() for every response.

Revert the changes and only omit timestamping for GPB to AP reponses.

Testing Done: Performed a loopback test both from the GPB (gbl app) as
              well as from the AP (loopback_test). Both tests passed.
Bug Number: SW-1719
Review URL: https://reviews.leaflabs.com/r/2100/
Reviewed by: Fabien Parent

Signed-off-by: Bartosz Golaszewski <bgolaszewski@baylibre.com>

nuttx/configs/ara/bridge/src/apbridgea_gadget.c
@@ -437,6 +437,7 @@ static struct apbridge_msg_s *apbridge_dequeue(struct apbridge_dev_s *priv)
 static int _to_usb_submit(struct usbdev_ep_s *ep, struct usbdev_req_s *req,
                           const void *payload, size_t len)
 {
+    struct gb_operation_hdr *gbhdr;
     struct apbridge_dev_s *priv;
     int ret;
     unsigned int cportid;
@@ -448,9 +449,15 @@ static int _to_usb_submit(struct usbdev_ep_s *ep, struct usbdev_req_s *req,
     cportid = get_cportid(payload);
 
     req->buf = (void*) payload;
+
+    gbhdr = (struct gb_operation_hdr *)req->buf;
     gb_timestamp_tag_exit_time(&priv->ts[cportid], cportid);
-    gb_timestamp_log(&priv->ts[cportid], cportid, req->buf, len,
-                          GREYBUS_FW_TIMESTAMP_APBRIDGE);
+
+    /* Skip the timestamping if it's not a response from GPB to AP. */
+    if (gbhdr->type & GB_TYPE_RESPONSE_FLAG) {
+        gb_timestamp_log(&priv->ts[cportid], cportid,
+                         req->buf, len, GREYBUS_FW_TIMESTAMP_APBRIDGE);
+    }
 
     /* Then submit the request to the endpoint */
 

nuttx/configs/ara/common/src/greybus_timestamp.c
@@ -78,16 +78,12 @@ void gb_timestamp_tag_exit_time(struct gb_timestamp *ts,
 void gb_timestamp_log(struct gb_timestamp *ts, unsigned int cportid,
                       void *payload, size_t len, int id)
 {
-    struct gb_operation_hdr *hdr = payload;
     struct gb_loopback_transfer_response *resp = payload +
                                                  sizeof(struct gb_operation_hdr);
     __le32 latency;
 
     if (len < TAG_SIZE)
         return;
-
-    if (hdr->type & GB_TYPE_RESPONSE_FLAG)
-        return; /* Bail-out if GPBridge is responding to the AP. */
 
     if (tag_active(ts, cportid)) {
         latency = calc_latency(&ts->entry_time,