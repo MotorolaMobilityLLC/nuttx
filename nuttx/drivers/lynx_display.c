749a8a1d90b94f55e2bda413913517b3b814c399
Mike Corrigan <corrigan@motorola.com>
2016-02-18 14:12:37 -0600

    nuttx: device: display: lynx: Use GPIO resources
    Use GPIO resources for power, reset, and TE.
    Remove te_count code.
    
    Change-Id: If03d1ff43b982f4670dc362a5a28f8cce6cf6b5e
    Signed-off-by: Mike Corrigan <corrigan@motorola.com>
    Reviewed-on: http://gerrit.mot.com/823374
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Reviewed-by: Kazuhiro Ondo <kazuhiro.ondo@motorola.com>

--- nuttx/drivers/lynx_display.c
+++ nuttx/drivers/lynx_display.c
@@ -53,15 +53,6 @@
 //#define CONFIG_LYNX_DISPLAY_PANEL_ID (1)
 //#define CONFIG_LYNX_DISPLAY_POWER_MODE (1)
 
-#define GPIO_APBE_DISP_PWR1_EN (3) /* backlight EN-P */
-#define GPIO_APBE_DISP_PWR2_EN (4) /* backlight EN-N */
-#define GPIO_APBE_DISP_PWR3_EN (5) /* I2C switch */
-
-#define GPIO_APBE_DISP_RST_N   (7) /* backlight reset */
-#define GPIO_APBE_DISP_RST2_N  (9) /* touch reset */
-
-#define GPIO_APBE_DISP_TE      (8) /* input from panel */
-
 #define LYNX_DISPLAY_POWER_DELAY_US (100000)
 
 #define LYNX_BACKLIGHT_I2C_BUS (0)
@@ -153,14 +144,17 @@ static struct lynx_display
 {
     struct device *display_device;
     struct cdsi_dev *cdsi_dev;
+    uint32_t gpio_pwr1;
+    uint32_t gpio_pwr2;
+    uint32_t gpio_pwr3;
+    uint32_t gpio_rst1;
+    uint32_t gpio_rst2;
+    uint32_t gpio_te;
     display_notification_cb callback;
     uint8_t state;
     uint8_t brightness;
 #if CONFIG_LYNX_DISPLAY_POWER_MODE
     uint8_t power_mode;
-#endif
-#if CONFIG_LYNX_DISPLAY_TE
-    uint32_t te_count;
 #endif
 } g_display;
 
@@ -272,21 +266,40 @@ static int lynx_display_get_state(struct device *dev, uint8_t *state)
 
 static void lynx_display_power_on(void)
 {
-    gpio_direction_out(GPIO_APBE_DISP_PWR1_EN, 1);
-    gpio_direction_out(GPIO_APBE_DISP_PWR2_EN, 1);
-    gpio_direction_out(GPIO_APBE_DISP_PWR3_EN, 1);
+    if (g_display.gpio_pwr1 != -1)
+        gpio_direction_out(g_display.gpio_pwr1, 1);
+
+    if (g_display.gpio_pwr2 != -1)
+        gpio_direction_out(g_display.gpio_pwr2, 1);
+
+    if (g_display.gpio_pwr3 != -1)
+        gpio_direction_out(g_display.gpio_pwr3, 1);
+
     usleep(LYNX_DISPLAY_POWER_DELAY_US);
-    gpio_direction_out(GPIO_APBE_DISP_RST_N, 1);
-    gpio_direction_out(GPIO_APBE_DISP_RST2_N, 1);
+
+    if (g_display.gpio_rst1 != -1)
+        gpio_direction_out(g_display.gpio_rst1, 1);
+
+    if (g_display.gpio_rst2 != -1)
+        gpio_direction_out(g_display.gpio_rst2, 1);
 }
 
 static void lynx_display_power_off(void)
 {
-    gpio_direction_out(GPIO_APBE_DISP_PWR1_EN, 0);
-    gpio_direction_out(GPIO_APBE_DISP_PWR2_EN, 0);
-    gpio_direction_out(GPIO_APBE_DISP_PWR3_EN, 0);
-    gpio_direction_out(GPIO_APBE_DISP_RST_N, 0);
-    gpio_direction_out(GPIO_APBE_DISP_RST2_N, 0);
+    if (g_display.gpio_rst1 != -1)
+        gpio_direction_out(g_display.gpio_rst1, 0);
+
+    if (g_display.gpio_rst2 != -1)
+        gpio_direction_out(g_display.gpio_rst2, 0);
+
+    if (g_display.gpio_pwr1 != -1)
+        gpio_direction_out(g_display.gpio_pwr1, 0);
+
+    if (g_display.gpio_pwr2 != -1)
+        gpio_direction_out(g_display.gpio_pwr2, 0);
+
+    if (g_display.gpio_pwr3 != -1)
+        gpio_direction_out(g_display.gpio_pwr3, 0);
 }
 
 static int lynx_display_set_state_on(void)
@@ -443,34 +456,59 @@ static int lynx_display_unregister_callback(struct device *dev)
 #if CONFIG_LYNX_DISPLAY_TE
 static int lynx_display_te_irq(int irq, FAR void *context)
 {
-#if 0
-    g_display.te_count++;
-    if ((g_display.te_count % 60) == 0)
-      {
-        up_lowputc('#');
-      }
-#endif
-
     gpio_clear_interrupt(irq);
     return 0;
 }
 #endif
 
-static int lynx_display_config_gpios(void)
+static int lynx_display_config_gpios(struct device *dev)
 {
-    gpio_direction_out(GPIO_APBE_DISP_PWR1_EN, 0);
-    gpio_direction_out(GPIO_APBE_DISP_PWR2_EN, 0);
-    gpio_direction_out(GPIO_APBE_DISP_PWR3_EN, 0);
+    g_display.gpio_pwr1 = -1;
+    struct device_resource *rsrc = device_resource_get_by_name(dev, DEVICE_RESOURCE_TYPE_GPIO, "pwr1_en");
+    if (rsrc) {
+        g_display.gpio_pwr1 = rsrc->start;
+        gpio_direction_out(g_display.gpio_pwr1, 0);
+    }
 
-    gpio_direction_out(GPIO_APBE_DISP_RST_N, 0);
-    gpio_direction_out(GPIO_APBE_DISP_RST2_N, 0);
+    g_display.gpio_pwr2 = -1;
+    rsrc = device_resource_get_by_name(dev, DEVICE_RESOURCE_TYPE_GPIO, "pwr2_en");
+    if (rsrc) {
+        g_display.gpio_pwr2 = rsrc->start;
+        gpio_direction_out(g_display.gpio_pwr2, 0);
+    }
+
+    g_display.gpio_pwr3 = -1;
+    rsrc = device_resource_get_by_name(dev, DEVICE_RESOURCE_TYPE_GPIO, "pwr3_en");
+    if (rsrc) {
+        g_display.gpio_pwr3 = rsrc->start;
+        gpio_direction_out(g_display.gpio_pwr3, 0);
+    }
+
+    g_display.gpio_rst1 = -1;
+    rsrc = device_resource_get_by_name(dev, DEVICE_RESOURCE_TYPE_GPIO, "disp_rst1_n");
+    if (rsrc) {
+        g_display.gpio_rst1 = rsrc->start;
+        gpio_direction_out(g_display.gpio_rst1, 0);
+    }
+
+    g_display.gpio_rst2 = -1;
+    rsrc = device_resource_get_by_name(dev, DEVICE_RESOURCE_TYPE_GPIO, "disp_rst2_n");
+    if (rsrc) {
+        g_display.gpio_rst2 = rsrc->start;
+        gpio_direction_out(g_display.gpio_rst2, 0);
+    }
 
 #if CONFIG_LYNX_DISPLAY_TE
-    gpio_direction_in(GPIO_APBE_DISP_TE);
-    set_gpio_triggering(GPIO_APBE_DISP_TE, IRQ_TYPE_EDGE_RISING);
-    gpio_irqattach(GPIO_APBE_DISP_TE, lynx_display_te_irq);
-    gpio_clear_interrupt(GPIO_APBE_DISP_TE);
-    gpio_unmask_irq(GPIO_APBE_DISP_TE);
+    rsrc = device_resource_get_by_name(dev, DEVICE_RESOURCE_TYPE_GPIO, "disp_te");
+    if (rsrc) {
+        g_display.gpio_te = rsrc->start;
+
+        gpio_direction_in(g_display.gpio_te);
+        set_gpio_triggering(g_display.gpio_te, IRQ_TYPE_EDGE_RISING);
+        gpio_irqattach(g_display.gpio_te, lynx_display_te_irq);
+        gpio_clear_interrupt(g_display.gpio_te);
+        gpio_unmask_irq(g_display.gpio_te);
+    }
 #endif
 
     return 0;
@@ -478,16 +516,16 @@ static int lynx_display_config_gpios(void)
 
 static int lynx_display_probe(struct device *dev)
 {
-    int result = lynx_display_config_gpios();
+    memset(&g_display, 0, sizeof(g_display));
+    g_display.display_device = dev;
+    g_display.state = DISPLAY_STATE_OFF;
+
+    int result = lynx_display_config_gpios(dev);
     if (result)
       {
         dbg("ERROR: Failed to configure GPIOS\n");
         return -EINVAL;
       }
-
-    memset(&g_display, 0, sizeof(g_display));
-    g_display.display_device = dev;
-    g_display.state = DISPLAY_STATE_OFF;
 
     return 0;
 }
