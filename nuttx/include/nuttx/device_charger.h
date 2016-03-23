b2e93c5c551d480cb629efb54c63f94567023e46
Dmitriy Gruzman <dgruzman@motorola.com>
2016-03-23 14:18:52 -0500

    include/nuttx: Add charger device
    Add the charger device type that supports syncing and sourcing current.
    
    Change-Id: I84deeb9bfbc7da34891c599cc21d1274a04f6624
    Signed-off-by: Dmitriy Gruzman <dgruzman@motorola.com>
    Reviewed-on: http://gerrit.mot.com/836711
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: James Wylder <jwylder@motorola.com>

---
+++ nuttx/include/nuttx/device_charger.h
@@ -0,0 +1,96 @@
+/*
+ * Copyright (c) 2016 Motorola Mobility, LLC.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from this
+ * software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef __INCLUDE_NUTTX_DEVICE_CHARGER_H
+#define __INCLUDE_NUTTX_DEVICE_CHARGER_H
+
+#include <nuttx/device.h>
+
+#include <errno.h>
+
+#define DEVICE_TYPE_CHARGER_HW  "charger"
+
+/* Limits are in mA and mV */
+struct charger_config {
+    int input_current_limit;
+    int input_voltage_limit;
+    int charge_current_limit;
+    int charge_voltage_limit;
+};
+
+struct device_charger_type_ops {
+    int (*send)(struct device *dev);
+    int (*receive)(struct device *dev, const struct charger_config *cfg);
+    int (*off)(struct device *dev);
+};
+
+static inline int device_charger_send(struct device *dev)
+{
+    DEVICE_DRIVER_ASSERT_OPS(dev);
+
+    if (!device_is_open(dev)) {
+        return -ENODEV;
+    }
+
+    if (!DEVICE_DRIVER_GET_OPS(dev, charger)->send) {
+        return -ENOSYS;
+    }
+
+    return DEVICE_DRIVER_GET_OPS(dev, charger)->send(dev);
+}
+
+static inline int device_charger_receive(struct device *dev, const struct charger_config *cfg)
+{
+    DEVICE_DRIVER_ASSERT_OPS(dev);
+
+    if (!device_is_open(dev)) {
+        return -ENODEV;
+    }
+
+    if (!DEVICE_DRIVER_GET_OPS(dev, charger)->receive) {
+        return -ENOSYS;
+    }
+
+    return DEVICE_DRIVER_GET_OPS(dev, charger)->receive(dev, cfg);
+}
+
+static inline int device_charger_off(struct device *dev)
+{
+    DEVICE_DRIVER_ASSERT_OPS(dev);
+
+    if (!device_is_open(dev)) {
+        return -ENODEV;
+    }
+
+    if (!DEVICE_DRIVER_GET_OPS(dev, charger)->off) {
+        return -ENOSYS;
+    }
+
+    return DEVICE_DRIVER_GET_OPS(dev, charger)->off(dev);
+}
+#endif
