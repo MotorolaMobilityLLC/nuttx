9a204a7f0ebf1f7cce0e7ada6234c06b0a0424f1
David Peyer <David.Peyer@motorola.com>
2016-03-23 17:33:39 -0500

    tsb: Add free running timer driver code for the TSB
    A new free running timer driver allows different low level drivers
    to access a free running version of the 48MHz timer.  This allows
    for unified power management allowing the clock to be shutdown when
    no free running timer is in use.  Please be aware that at this time
    this is not integrated with the free running timer used for the
    tickless version of the kernel.
    
    Change-Id: I90e423af75fab4b0029ed221711e87ede0341aad
    Signed-off-by: David Peyer <David.Peyer@motorola.com>
    Reviewed-on: http://gerrit.mot.com/835532
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: David Peyer <wldp07@motorola.com>
    Reviewed-by: Michael Corrigan <michael.corrigan@motorola.com>
    Reviewed-by: Kazuhiro Ondo <kazuhiro.ondo@motorola.com>

--- nuttx/arch/arm/src/tsb/tsb_tmr.h
+++ nuttx/arch/arm/src/tsb/tsb_tmr.h
@@ -1,4 +1,5 @@
 /*
+ * Copyright (c) 2016 Motorola Mobility, LLC
  * Copyright (c) 2015 Google, Inc.
  * All rights reserved.
  *
@@ -31,6 +32,7 @@
 #ifndef __ARCH_ARM_SRC_TSB_TSB_TMR_H
 #define __ARCH_ARM_SRC_TSB_TSB_TMR_H
 
+#include "chip.h"
 #include <nuttx/irq.h>
 #include <stdint.h>
 
@@ -44,6 +46,33 @@
 
 struct tsb_tmr_ctx;
 
+#define TSB_TMR_LOAD                    (0x0000)
+#define TSB_TMR_CNT                     (0x0004)
+#define TSB_TMR_CTRL                    (0x0008)
+#define TSB_TMR_ISR                     (0x000C)
+#define TSB_TMR_WDT                     (0x0040)
+#define TSB_TMR_RSR                     (0x0080)
+
+#define TSB_TMR1_OFF                    (0x0100)
+#define TSB_TMR2_OFF                    (0x0200)
+#define TSB_TMR3_OFF                    (0x0300)
+#define TSB_TMR4_OFF                    (0x0400)
+
+/*
+ * Macro to calculate the address of the requested register for quick reads.
+ * This will only work for timers which are 0x100 bytes appart in the memory
+ * map.  This is the case for all versions of the TSB up to and including
+ * ES3.
+ */
+#define TSB_TMR_GET_REG_PTR(id, reg) ((volatile uint32_t *)(TMR_BASE+(TSB_TMR1_OFF*(id+1))+(reg)))
+
+/*
+ * The following macro relies on the fact that the timer base frequency on the
+ * TSB is 48MHz.
+ */
+#define TSB_TMR_RAW_TO_USEC(raw)     (uint32_t)(raw/48)
+#define TSB_TMR_USEC_TO_RAW(usec)    (uint32_t)(usec*48)
+
 enum {
     TSB_TMR_TMR1 = 0,
     TSB_TMR_TMR2 = 1,
@@ -56,6 +85,41 @@ enum {
     TSB_TMR_MODE_ONESHOT = 0x05,
     TSB_TMR_MODE_FREERUN = 0x03,
 };
+
+/**
+ * @brief Read the raw value of a timer in ticks.
+ *
+ * Function to read the raw value of a TSB timer.  Please remember that the TSB
+ * timers count down from the loaded value.  To convert the value to micro seconds
+ * use TSB_TMR_RAW_TO_USEC.  If a timer enum value is used directly this code should
+ * compile down to a register read.
+ *
+ * @param tmr Timer handle.
+ *
+ * @return The raw value of the timer.
+ */
+static inline uint32_t tsb_tmr_read_raw(int id)
+{
+    return *TSB_TMR_GET_REG_PTR(id, TSB_TMR_CNT);
+}
+
+/**
+ * @brief Read the elapsed time of a timer in ticks.
+ *
+ * Function to read the time elapsed on a timer in ticks.  This is essentially
+ * the value in the timer value subtracted from the load register.  It may roll
+ * over and result in a shorter time that was anticipated so please keep in mind
+ * the base frequency of the timer before using this.  To convert the value to
+ * micro seconds use TSB_TMR_RAW_TO_USEC.
+ *
+ * @param tmr Timer handle.
+ *
+ * @return The raw value of the timer.
+ */
+static inline uint32_t tsb_tmr_read_elapsed_raw(int id)
+{
+    return *TSB_TMR_GET_REG_PTR(id, TSB_TMR_LOAD) - *TSB_TMR_GET_REG_PTR(id, TSB_TMR_CNT);
+}
 
 struct tsb_tmr_ctx * tsb_tmr_get(int id);
 void tsb_tmr_configure(struct tsb_tmr_ctx *tmr, int mode, xcpt_t isr);
