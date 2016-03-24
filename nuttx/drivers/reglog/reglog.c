29486736a0080cf060a0e01ae8e6658fc3db0f94
David Peyer <David.Peyer@motorola.com>
2016-03-24 11:33:48 -0500

    drivers: nuttx: tsb: Add reglog capability
    Add logging to ram for address and value pairs.  This can be used during
    debugging to log data in real time without the overhead of lldbg or printf.
    The data is logged with a time stamp, and can be viewed through the debugger
    or via /dev/reglog.  The functionality is controlled by CONFIG_REGLOG.
    
    Change-Id: If99ca90a5f499fd4aa2b33f06be17be4121f7821
    Signed-off-by: David Peyer <David.Peyer@motorola.com>
    Reviewed-on: http://gerrit.mot.com/837260
    SME-Granted: SME Approvals Granted
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Reviewed-by: David Peyer <wldp07@motorola.com>
    Reviewed-by: Michael Corrigan <michael.corrigan@motorola.com>
    Reviewed-by: Kazuhiro Ondo <kazuhiro.ondo@motorola.com>

---
+++ nuttx/drivers/reglog/reglog.c
@@ -0,0 +1,209 @@
+/*
+ * Copyright (c) 2016 Motorola Mobility, LLC.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its
+ * may be used to endorse or promote products derived from this
+ * software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This module is used to dump a log of time, address and value pairs.  This
+ * buffer is commonly used to log register or data writes with little impact
+ * to real time.  The data can then be dumped through the /dev file system
+ * or viewed on a debugger.  No semaphores are used in the dumping of the log
+ * so that the write code can operate within interrupts without any issue.  This
+ * means that is is possible for the output to be corrupted if the head pointer
+ * is updated before the tail can advance.  This is deemeed acceptable, since it
+ * should be rare and real time performance is more important than data
+ * output.
+ */
+#include <nuttx/config.h>
+
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <nuttx/arch.h>
+#include <reglog.h>
+#include <nuttx/clock.h>
+#include <nuttx/fs/fs.h>
+#include <nuttx/compiler.h>
+
+/*
+ * Define the length of a reglog output line.  Currently it has the form
+ * tttttttt:  aaaaaaaa vvvvvvvv
+ *
+ * Where:
+ *   t is the time the log entry was added (8 hex digits)
+ *   a is the address of the log entry (8 hex digits)
+ *   v is the value of the log entry (8 hex digits)
+ *
+ * This results in 8*3 (digits) + 2 (spaces) + 1 (colon) + 1 (newline)
+ * + 1 (null).
+ */
+#define REGLOG_LINE_LEN  29
+
+struct reglog_s g_reglog;
+
+static ssize_t reglog_read(FAR struct file *filep, FAR char *buffer, size_t len)
+{
+    struct inode *inode_p  = filep->f_inode;
+    struct reglog_s *reglog_p;
+    struct reglog_value_s *value_p;
+    irqstate_t flags;
+    ssize_t i;
+    ssize_t nout;
+
+    /* Some sanity checking */
+    DEBUGASSERT(inode_p && inode_p->i_private);
+    reglog_p = inode_p->i_private;
+
+    nout = 0;
+    /* Warn that the data has overflowed. */
+    if (reglog_p->tail+REGLOG_DEPTH < reglog_p->head)
+    {
+        i = sprintf(buffer, "Reglog has overflowed by %u entries since the last dump.\n",
+                    reglog_p->head-reglog_p->tail-REGLOG_DEPTH);
+        nout = i;
+        len -= i;
+        buffer += i;
+        reglog_p->tail = reglog_p->head-REGLOG_DEPTH;
+    }
+    /* Output until data is gone. */
+    while ((reglog_p->head != reglog_p->tail) && (len > REGLOG_LINE_LEN))
+    {
+        value_p = &reglog_p->log[reglog_p->tail++&(REGLOG_DEPTH-1)];
+        /* If this format is updated, REGLOG_LINE_LEN must also be updated. */
+        i = snprintf(buffer, REGLOG_LINE_LEN, "%08x: %08x %08x\n",
+                     value_p->time, value_p->addr, value_p->val);
+        len -= i;
+        buffer += i;
+        nout += i;
+    }
+    /* If all was output and in stack mode, reset the head pointer. */
+    if ((g_reglog.mode == REGLOG_MODE_STACK) &&
+        (nout == 0))
+    {
+        flags = irqsave();
+        reglog_p->head = 0;
+        reglog_p->tail = 0;
+        irqrestore(flags);
+    }
+    return nout;
+}
+
+static ssize_t reglog_write(FAR struct file *filep, FAR const char *buffer, size_t len)
+{
+    if (len > 0)
+    {
+        switch (buffer[0])
+        {
+            case 'F':
+            case 'f':
+                /* Swtich to fifo mode. */
+                g_reglog.mode = REGLOG_MODE_FIFO;
+                break;
+
+            case 'S':
+            case 's':
+                /* Switch to statck mode. */
+                g_reglog.mode = REGLOG_MODE_STACK;
+                break;
+
+            default:
+                break;
+        }
+    }
+    return len;
+}
+
+static const struct file_operations g_reglogfops =
+{
+    NULL,             /* open */
+    NULL,             /* close */
+    reglog_read,      /* read */
+    reglog_write,     /* write */
+    NULL,             /* seek */
+    NULL              /* ioctl */
+#ifndef CONFIG_DISABLE_POLL
+    , NULL            /* poll */
+#endif
+};
+
+/**
+ * @brief Provide a pointer to the next location to place log data.
+ *
+ * The pointer provided can be directly updated to add the values into the log.
+ * The head will be advanced when the pointer is provided, unless in stack
+ * mode and at the end of the buffer.  In this case the last entry will be
+ * returned and no advance will be done.
+ *
+ * @return A pointer to the log entry to be updated.
+ */
+static inline struct reglog_value_s *reglog_get_next_entry(void)
+{
+    if ((g_reglog.mode == REGLOG_MODE_STACK) &&
+        (g_reglog.head >= REGLOG_DEPTH))
+    {
+        return (&g_reglog.log[REGLOG_DEPTH-1]);
+    }
+    return (&g_reglog.log[(g_reglog.head++)&(REGLOG_DEPTH-1)]);
+}
+
+/**
+ * @brief Function to handle logging.
+ *
+ * This function handles placing values into the log array.  The schedule clock
+ * is used as the timer, unless it was overloaded by a lower lever driver.  This
+ * can be done via a call to the reglog_register() function.
+ *
+ * @param reg The address to place in the log.
+ * @param val The value to place in the log.
+ */
+void reglog_log(uint32_t reg, uint32_t val)
+{
+    struct reglog_value_s *reglog_p;
+
+    reglog_p = reglog_get_next_entry();
+    reglog_p->time = g_reglog.get_time_fcn();
+    reglog_p->addr = reg;
+    reglog_p->val = val;
+}
+
+/**
+ * @brief Provide a default clock function.
+ *
+ * This is necessary since clock_systimer is a macro.
+ */
+uint32_t reglog_get_time_default(void)
+{
+    return clock_systimer();
+}
+
+/**
+ * @brief Initlaize the reglog structures and low lever driver if necessary.
+ */
+void reglog_initialize(void)
+{
+    memset(&g_reglog, 0, sizeof(g_reglog));
+    g_reglog.get_time_fcn = reglog_get_time_default;
+    up_reglog_init();
+    register_driver("/dev/reglog", &g_reglogfops, 0666, &g_reglog);
+}
