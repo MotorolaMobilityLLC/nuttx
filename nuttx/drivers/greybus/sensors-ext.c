1af05c17ec5d0e3ac274ea51e8b36ff02e9e20b6
Makarand karvekar <mkarvekar@motorola.com>
2016-04-01 13:32:40 -0500

    greybus: sensors-ext: replace operation types, attr and report
    operation type attr replaced with count
    operation type report replaced with flush
    report structure updated
    update minor version to 2
    
    Change-Id: I16b01f48d778c6ae193313fddc13b00264212eaf
    Reviewed-on: http://gerrit.mot.com/840442
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Reviewed-by: Gregory Meiste <w30289@motorola.com>
    Reviewed-by: James Wylder <jwylder@motorola.com>

--- nuttx/drivers/greybus/sensors-ext.c
+++ nuttx/drivers/greybus/sensors-ext.c
@@ -43,7 +43,7 @@
 #include "sensors-ext-gb.h"
 
 #define GB_SENSORS_EXT_VERSION_MAJOR 0
-#define GB_SENSORS_EXT_VERSION_MINOR 1
+#define GB_SENSORS_EXT_VERSION_MINOR 2
 
 
 struct gb_sensors_ext_info {
@@ -69,27 +69,25 @@ static struct gb_sensors_ext_info *get_dev_from_id(uint8_t sensor_id)
     return NULL;
 }
 
-static int event_callback(uint8_t se_id, struct report_info *rinfo)
+static int event_callback(uint8_t se_id, struct report_info_data *rinfo_data,
+                            uint16_t payload_size)
 {
     struct gb_operation *operation;
-    struct gb_sensors_ext_event_report *request;
+    struct gb_sensors_ext_report_data *request;
 
     operation = gb_operation_create(sensors_ext_cport, GB_SENSORS_EXT_TYPE_EVENT,
-                                    (sizeof(*request)));
+                                    (sizeof(*request) + payload_size));
     if (!operation) {
         gb_error("%s: gb_operation_create failed !\n",  __func__);
         return -ENOMEM;
     }
 
     request = gb_operation_get_request_payload(operation);
-    request->id = se_id;
-    request->readings = cpu_to_le16(rinfo->readings);
 
-    memcpy((void *)&request->reference_time[0],
-            (void *)&rinfo->reference_time[0], sizeof(uint64_t));
+    request->num_sensors_reporting = rinfo_data->num_sensors_reporting;
 
-    memcpy((void *)&request->data_payload[0],
-            (void *)&rinfo->data_payload[0], SENSOR_DATA_PAYLOAD_SIZE);
+    memcpy((void *)&request->event_report[0],
+            (void *)&rinfo_data->reportinfo[0], payload_size);
 
     gb_operation_send_request(operation, NULL, false);
     gb_operation_destroy(operation);
@@ -113,9 +111,9 @@ static uint8_t gb_sensors_ext_protocol_version(struct gb_operation *operation)
     return GB_OP_SUCCESS;
 }
 
-static uint8_t gb_sensors_ext_sensor_attr(struct gb_operation *operation)
+static uint8_t gb_sensors_ext_sensor_count(struct gb_operation *operation)
 {
-    struct gb_sensors_ext_sensor_attr_response *response;
+    struct gb_sensors_ext_sensor_count_response *response;
 
     response = gb_operation_alloc_response(operation, sizeof(*response));
     if (!response) {
@@ -123,10 +121,8 @@ static uint8_t gb_sensors_ext_sensor_attr(struct gb_operation *operation)
     }
 
     response->count = list_count(&s_device_list);
-    response->event_report_size = cpu_to_le16(sizeof(struct gb_sensors_ext_event_report));
+    gb_debug("sensor count %d\n", response->count);
 
-    gb_debug("count %d, report_size %d\n", response->count,
-                response->event_report_size);
     return GB_OP_SUCCESS;
 }
 
@@ -162,7 +158,6 @@ static uint8_t gb_sensors_ext_get_sensor_info(struct gb_operation *operation)
         return GB_OP_NO_MEMORY;
     }
 
-    response->size = cpu_to_le16(information.size);
     response->version = cpu_to_le32(information.version);
     response->type = cpu_to_le32(information.type);
     response->max_range = cpu_to_le32(information.max_range);
@@ -172,11 +167,42 @@ static uint8_t gb_sensors_ext_get_sensor_info(struct gb_operation *operation)
     response->max_delay = cpu_to_le32(information.max_delay);
     response->fifo_rec = cpu_to_le32(information.fifo_rec);
     response->fifo_mec = cpu_to_le32(information.fifo_mec);
-    response->flag = cpu_to_le32(information.flag);
-    response->scale = cpu_to_le32(information.scale);
-    response->offset = cpu_to_le32(information.offset);
-    response->reading_size = information.reading_size;
-    memcpy(&response->name[0], &information.name[0], sizeof(information.name));
+    response->flags = cpu_to_le32(information.flags);
+    response->scale_int = cpu_to_le32(information.scale_int);
+    response->scale_nano = cpu_to_le32(information.scale_nano);
+    response->offset_int = cpu_to_le32(information.offset_int);
+    response->offset_nano = cpu_to_le32(information.offset_nano);
+    response->channels = information.channels;
+
+    response->name_len = cpu_to_le16(information.name_len);
+    if (response->name_len) {
+        if ((response->name_len > sizeof(response->name)))
+            response->name_len = sizeof(response->name);
+        memcpy(&response->name[0], &information.name[0], response->name_len);
+    } else {
+        memset(&response->name[0], 0, sizeof(response->name));
+    }
+
+    response->vendor_len = cpu_to_le16(information.vendor_len);
+    if (response->vendor_len) {
+        if (response->vendor_len > sizeof(response->vendor))
+            response->vendor_len = sizeof(response->vendor);
+
+        memcpy(&response->vendor[0], &information.vendor[0], response->vendor_len);
+    } else {
+        memset(&response->vendor[0], 0, sizeof(response->vendor));
+    }
+
+    response->string_type_len = cpu_to_le16(information.string_type_len);
+    if (response->string_type_len) {
+        if (response->string_type_len > sizeof(response->string_type))
+            response->string_type_len = sizeof(response->string_type);
+
+        memcpy(&response->string_type[0], &information.string_type[0],
+            response->string_type_len);
+    } else {
+        memset(&response->string_type[0], 0, sizeof(response->string_type));
+    }
 
     return GB_OP_SUCCESS;
 }
@@ -210,11 +236,9 @@ static uint8_t gb_sensors_ext_start_reporting(struct gb_operation *operation)
 }
 
 
-static uint8_t gb_sensors_ext_get_report(struct gb_operation *operation)
+static uint8_t gb_sensors_ext_flush(struct gb_operation *operation)
 {
-    struct gb_sensors_ext_get_report_request *request;
-    struct gb_sensors_ext_event_report *response;
-    struct report_info rinfo;
+    struct gb_sensors_ext_flush_request *request;
     struct gb_sensors_ext_info *se_info;
     int ret = 0;
 
@@ -231,23 +255,10 @@ static uint8_t gb_sensors_ext_get_report(struct gb_operation *operation)
         return GB_OP_INVALID;
     }
 
-    ret = device_sensors_ext_get_report(se_info->dev, request->id, &rinfo);
+    ret = device_sensors_ext_flush(se_info->dev, request->id);
     if (ret) {
         return gb_errno_to_op_result(ret);
     }
-
-    response = gb_operation_alloc_response(operation, sizeof(*response));
-    if (!response) {
-        return GB_OP_NO_MEMORY;
-    }
-
-    response->id = rinfo.id;
-    response->readings = cpu_to_le16(rinfo.readings);
-    memcpy((void *)&response->reference_time[0],
-            (void *)&rinfo.reference_time[0], sizeof(uint64_t));
-
-    memcpy((void *)&response->data_payload[0],
-            (void *)&rinfo.data_payload[0], SENSOR_DATA_PAYLOAD_SIZE);
 
     return GB_OP_SUCCESS;
 }
@@ -373,10 +384,10 @@ static void gb_sensors_ext_exit(unsigned int cport)
 
 static struct gb_operation_handler gb_sensors_ext_handlers[] = {
     GB_HANDLER(GB_SENSORS_EXT_TYPE_PROTOCOL_VERSION, gb_sensors_ext_protocol_version),
-    GB_HANDLER(GB_SENSORS_EXT_TYPE_SENSOR_ATTR, gb_sensors_ext_sensor_attr),
+    GB_HANDLER(GB_SENSORS_EXT_TYPE_SENSOR_COUNT, gb_sensors_ext_sensor_count),
     GB_HANDLER(GB_SENSORS_EXT_TYPE_SENSOR_INFO, gb_sensors_ext_get_sensor_info),
     GB_HANDLER(GB_SENSORS_EXT_TYPE_START_REPORTING, gb_sensors_ext_start_reporting),
-    GB_HANDLER(GB_SENSORS_EXT_TYPE_REPORT, gb_sensors_ext_get_report),
+    GB_HANDLER(GB_SENSORS_EXT_TYPE_FLUSH, gb_sensors_ext_flush),
     GB_HANDLER(GB_SENSORS_EXT_TYPE_STOP_REPORTING, gb_sensors_ext_stop_reporting),
 };
 
