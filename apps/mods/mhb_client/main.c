fd079c1ac1339fc2a6544b82932711feca9dde73
David Peyer <David.Peyer@motorola.com>
2016-04-10 22:08:05 -0500

    apps: mods: mhb_client: Update mhb client for i2s tunneling and reglog
    Update the mhb client to handle messages to configure i2s tunneling as
    well as send the register log from both the APBA and APBE to the MUC.
    
    Change-Id: Ic452d24bb58ac9d90c17cb8c69a14e5ca2765f81
    Signed-off-by: David Peyer <David.Peyer@motorola.com>
    Reviewed-on: http://gerrit.mot.com/842995
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: David Peyer <wldp07@motorola.com>
    Reviewed-by: Michael Corrigan <michael.corrigan@motorola.com>
    Reviewed-by: Kazuhiro Ondo <kazuhiro.ondo@motorola.com>

--- apps/mods/mhb_client/main.c
+++ apps/mods/mhb_client/main.c
@@ -29,6 +29,7 @@
 #include <nuttx/config.h>
 
 #include <errno.h>
+#include <reglog.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -36,10 +37,14 @@
 #include <arch/byteorder.h>
 
 #include <nuttx/device.h>
+#include <nuttx/device_slave_pwrctrl.h>
 #include <nuttx/util.h>
+
+#include <nuttx/i2s_tunnel/i2s_unipro.h>
 
 #include <nuttx/mhb/device_mhb.h>
 #include <nuttx/mhb/mhb_protocol.h>
+#include <nuttx/mhb/mhb_utils.h>
 
 #include <nuttx/unipro/unipro.h>
 
@@ -50,6 +55,73 @@ struct command {
 };
 
 static struct device *g_dev;
+
+static const struct command *mhb_find_cmd(const char *str, const struct command *cmds, size_t num_cmds)
+{
+    while (num_cmds--) {
+        if (!strcmp(cmds->str, str))
+            return cmds;
+        cmds++;
+    }
+    return NULL;
+}
+
+static void mhb_print_cmds(const char *header,
+                           const struct command *cmds,
+                           size_t num_cmds,
+                           const char *trailer)
+{
+    if (header != NULL) {
+        printf("%s [ ", header);
+    }
+    while (num_cmds--) {
+        printf(" %s %c", cmds->str, num_cmds > 0 ? '|' : ' ');
+        cmds++;
+    }
+    printf("]");
+    if (trailer != NULL) {
+        printf(trailer);
+    }
+}
+
+static int mhb_handle_sub_cmd(int argc,
+                              char *argv[],
+                              struct device *dev,
+                              const char *str,
+                              const struct command *cmds,
+                              size_t num_cmds)
+{
+    static const struct command *cmd;
+    int ret = OK;
+
+    cmd = mhb_find_cmd(argv[0], cmds, num_cmds);
+    if (cmd == NULL) {
+        printf("Error: Unknown %s subcommand: %s\n", str, argv[0]);
+        mhb_print_cmds(str, cmds, num_cmds, "\n");
+        return -EINVAL;
+    }
+    ret = cmd->fp(argc-1, &argv[1], dev);
+    printf("%s returned: %d\n", cmd->str, ret);
+    return ret;
+}
+
+static int mhb_send_diag_reg_log_apbe_req_na(struct device *dev)
+{
+    struct mhb_hdr hdr;
+
+    hdr.addr = MHB_ADDR_DIAG;
+    hdr.type = MHB_TYPE_DIAG_REG_LOG_APBE_REQ;
+    return device_mhb_send(dev, &hdr, NULL, 0, 0);
+}
+
+static int mhb_send_diag_reg_log_apba_req_na(struct device *dev)
+{
+    struct mhb_hdr hdr;
+
+    hdr.addr = MHB_ADDR_DIAG;
+    hdr.type = MHB_TYPE_DIAG_REG_LOG_APBA_REQ;
+    return device_mhb_send(dev, &hdr, NULL, 0, 0);
+}
 
 static int mhb_handle_unipro(struct device *dev, struct mhb_hdr *hdr,
                uint8_t *payload, size_t payload_length)
@@ -87,9 +159,34 @@ static int mhb_handle_unipro(struct device *dev, struct mhb_hdr *hdr,
     }
 }
 
-static int mhb_handle_diag(struct device *dev, struct mhb_hdr *hdr,
-               uint8_t *payload, size_t payload_length)
+static int mhb_handle_diag_print_reglog(struct reglog_value_s *entries, size_t n, const char *str)
 {
+    static unsigned int entry_num = 0;
+
+    if (n != 0)
+    {
+        do
+        {
+            FROM_LE(entries->time);
+            FROM_LE(entries->addr);
+            FROM_LE(entries->val);
+            printf("%s %04d - 0x%08x: 0x%08x 0x%08x\n", str, entry_num++, entries->time,
+                   entries->addr, entries->val);
+            entries++;
+        } while (--n);
+    }
+    else
+    {
+        entry_num = 0;
+    }
+    return entry_num;
+}
+
+static int mhb_handle_diag(struct device *dev, struct mhb_hdr *hdr,
+                          uint8_t *payload, size_t payload_length)
+{
+    int ret = OK;
+
     printf("addr=0x%02x, type=0x%02x, result=0x%02x\n",
         hdr->addr, hdr->type, hdr->result);
 
@@ -99,16 +196,93 @@ static int mhb_handle_diag(struct device *dev, struct mhb_hdr *hdr,
     case MHB_TYPE_DIAG_LOG_RSP:
     case MHB_TYPE_DIAG_LAST_RSP:
         printf((char *)payload);
-        return 0;
+        break;
     case MHB_TYPE_DIAG_MODE_RSP:
         printf("mode %s\n", res);
-        return 0;
+        break;
     case MHB_TYPE_DIAG_LOOP_RSP:
-        return 0;
+        break;
+    case MHB_TYPE_DIAG_REG_LOG_APBE_RSP:
+        if (mhb_handle_diag_print_reglog((struct reglog_value_s *)payload,
+                                         payload_length/sizeof(struct reglog_value_s),
+                                         "APBE") > 0) {
+            /* Request more data. */
+            ret = mhb_send_diag_reg_log_apbe_req_na(dev);
+        }
+        else {
+            /* Request APBA data. */
+            ret = mhb_send_diag_reg_log_apba_req_na(dev);
+        }
+        break;
+    case MHB_TYPE_DIAG_REG_LOG_APBA_RSP:
+        if (mhb_handle_diag_print_reglog((struct reglog_value_s *)payload,
+                                         payload_length/sizeof(struct reglog_value_s),
+                                         "APBA") > 0) {
+            /* Request more data. */
+            ret = mhb_send_diag_reg_log_apba_req_na(dev);
+        }
+        break;
     default:
-        printf("ERROR: unknown diag event\n");
+        printf("ERROR: unknown client diag event: %d\n", hdr->type);
+        ret = -EINVAL;
+        break;
+    }
+    return ret;
+}
+
+void mhb_print_i2s_status(const char *header, struct i2s_tunnel_info_s *info)
+{
+    FROM_LE(info->enabled);
+    FROM_LE(info->is_master);
+    FROM_LE(info->bclk_rate);
+    FROM_LE(info->bytes_per_sample);
+    FROM_LE(info->roundtrip_timer_ticks);
+    FROM_LE(info->timer_offset);
+    FROM_LE(info->i2s_tunnel_rx_packets);
+    FROM_LE(info->i2s_tunnel_packet_size);
+    FROM_LE(info->i2s_tx_samples_dropped);
+    FROM_LE(info->i2s_tx_samples_retransmitted);
+    FROM_LE(info->i2s_tx_buffers_dropped);
+
+    printf("%s:\n", header);
+    printf("  Enabled:                      %u\n", info->enabled);
+    printf("  Master:                       %u\n", info->is_master);
+    printf("  Clock Rate:                   %u\n", info->bclk_rate);
+    printf("  Bytes per sample:             %u\n", info->bytes_per_sample);
+    printf("  Round trip 48MHz timer ticks: %u\n", info->roundtrip_timer_ticks);
+    printf("  Timer offset ticks:           %d\n", info->timer_offset);
+    printf("  Packets received:             %u\n", info->i2s_tunnel_rx_packets);
+    printf("  Packet size:                  %u\n", info->i2s_tunnel_packet_size);
+    printf("  Samples removed:              %u\n", info->i2s_tx_samples_dropped);
+    printf("  Samples repeated:             %u\n", info->i2s_tx_samples_retransmitted);
+    printf("  Packets dropped:              %u\n", info->i2s_tx_buffers_dropped);
+}
+
+static int mhb_handle_i2s_rx(struct device *dev, struct mhb_hdr *hdr,
+               uint8_t *payload, size_t payload_length)
+{
+    printf("addr=0x%02x, type=0x%02x, result=0x%02x\n",
+        hdr->addr, hdr->type, hdr->result);
+
+    const char *res = hdr->result == MHB_RESULT_SUCCESS ? "success" : "failure";
+
+    switch (hdr->type) {
+    case MHB_TYPE_I2S_CONFIG_RSP:
+        printf("I2S Config: %s.\n", res);
+        break;
+    case MHB_TYPE_I2S_CONTROL_RSP:
+        printf("I2S Cmd: %s\n", res);
+        break;
+    case MHB_TYPE_I2S_STATUS_RSP:
+        mhb_print_i2s_status("APBA Info:", (struct i2s_tunnel_info_s *)&payload[0]);
+        mhb_print_i2s_status("APBE Info:",
+                             (struct i2s_tunnel_info_s *)&payload[sizeof(struct i2s_tunnel_info_s)]);
+        break;
+    default:
+        printf("ERROR: unknown i2s response\n");
         return -EINVAL;
     }
+    return OK;
 }
 
 static int mhb_send_gear_req(int argc, char *argv[], struct device *dev)
@@ -281,7 +455,7 @@ usage:
     return -EINVAL;
 }
 
-static int mhb_send_log_req(int argc, char *argv[], struct device *dev)
+static int mhb_send_diag_log_req(int argc, char *argv[], struct device *dev)
 {
     struct mhb_hdr hdr;
     memset(&hdr, 0, sizeof(hdr));
@@ -291,7 +465,7 @@ static int mhb_send_log_req(int argc, char *argv[], struct device *dev)
     return device_mhb_send(dev, &hdr, NULL, 0, 0);
 }
 
-static int mhb_send_mode_req(int argc, char *argv[], struct device *dev)
+static int mhb_send_diag_mode_req(int argc, char *argv[], struct device *dev)
 {
     struct mhb_hdr hdr;
     struct mhb_diag_mode_req req;
@@ -320,37 +494,267 @@ usage:
 
 }
 
-const static struct command COMMANDS[] = { \
-    /* UniPro */
-    { "gear", mhb_send_gear_req },
-    { "status", mhb_send_status_req },
-    { "read", mhb_send_read_attr_req },
-    { "write", mhb_send_write_attr_req },
-    /* Diag */
-    { "log", mhb_send_log_req },
-    { "mode", mhb_send_mode_req },
-};
-
-static void usage(char *argv0)
+static int mhb_send_i2s_enable(int argc, char *argv[], struct device *dev)
 {
-    size_t i;
+    struct mhb_hdr hdr;
+    struct mhb_i2s_control_req i2s_cmd;
+    (void)argc;
+    (void)argv;
 
-    printf("usage: %s [", argv0);
+    hdr.addr = MHB_ADDR_I2S;
+    hdr.type = MHB_TYPE_I2S_CONTROL_REQ;
+    hdr.result = 0;
+    i2s_cmd.command = MHB_I2S_COMMAND_ENABLE;
+    return device_mhb_send(dev, &hdr, (const uint8_t *)&i2s_cmd, sizeof(i2s_cmd), 0);
+}
 
-    for (i=0; i < ARRAY_SIZE(COMMANDS); i++) {
-        printf("%s|", COMMANDS[i].str);
+static int mhb_send_i2s_disable(int argc, char *argv[], struct device *dev)
+{
+    struct mhb_hdr hdr;
+    struct mhb_i2s_control_req i2s_cmd;
+    (void)argc;
+    (void)argv;
+
+    hdr.addr = MHB_ADDR_I2S;
+    hdr.type = MHB_TYPE_I2S_CONTROL_REQ;
+    hdr.result = 0;
+    i2s_cmd.command = MHB_I2S_COMMAND_DISABLE;
+    return device_mhb_send(dev, &hdr, (const uint8_t *)&i2s_cmd, sizeof(i2s_cmd), 0);
+}
+
+static int mhb_send_i2s_config(int argc, char *argv[], struct device *dev)
+{
+    struct mhb_hdr hdr;
+    struct mhb_i2s_config i2s_config;
+
+    hdr.addr = MHB_ADDR_I2S;
+    hdr.type = MHB_TYPE_I2S_CONFIG_REQ;
+    hdr.result = 0;
+    /* Set the defaults. */
+    i2s_config.sample_rate  = 48000;
+    i2s_config.protocol     = MHB_I2S_PROTOCOL_LR_STEREO;
+    i2s_config.sample_size  = 16;
+    i2s_config.num_channels = 2;
+    i2s_config.tx_edge      = MHB_I2S_EDGE_RISING;
+    i2s_config.rx_edge      = MHB_I2S_EDGE_FALLING;
+    i2s_config.wclk_edge    = MHB_I2S_EDGE_RISING;
+    i2s_config.clk_role     = MHB_I2S_ROLE_MASTER;
+    switch (argc) {
+        case 8:
+            if (strcmp(argv[7], "falling") == 0)
+                i2s_config.wclk_edge = MHB_I2S_EDGE_FALLING;
+        case 7:
+            if (strcmp(argv[6], "rising") == 0)
+                i2s_config.rx_edge = MHB_I2S_EDGE_RISING;
+        case 6:
+            if (strcmp(argv[5], "falling") == 0)
+                i2s_config.tx_edge = MHB_I2S_EDGE_FALLING;
+        case 5:
+            i2s_config.num_channels = atoi(argv[3]);
+        case 4:
+            i2s_config.sample_size = atoi(argv[3]);
+        case 3:
+            if (strcmp(argv[2], "mono") == 0)
+                i2s_config.protocol = MHB_I2S_PROTOCOL_PCM;
+        case 2:
+            if (strcmp(argv[1], "slave") == 0)
+                 i2s_config.clk_role = MHB_I2S_ROLE_SLAVE;
+        case 1:
+            if (strcmp(argv[0], "help") == 0) {
+                printf("config <rate> <direction (master|slave)> <protocol (lr_stereo|mono)> <size bits> "
+                       "<channels (1|2)> tx edge (rising|fallinb)> <rx edge (rising|falling)> "
+                       "<lr edge (rising|falling)>\n"
+                       "Options can stop at any point if the detaults are acceptable for the rest of"
+                       "the options.\n");
+                return OK;
+            }
+            i2s_config.sample_rate = atoi(argv[0]);
     }
+    return device_mhb_send(dev, &hdr, (const uint8_t *)&i2s_config, sizeof(i2s_config), 0);
+}
 
-    printf("]\n");
+static int mhb_send_i2s_arm(int argc, char *argv[], struct device *dev)
+{
+    struct mhb_hdr hdr;
+    struct mhb_i2s_control_req i2s_cmd;
+    (void)argc;
+    (void)argv;
+
+    hdr.addr = MHB_ADDR_I2S;
+    hdr.type = MHB_TYPE_I2S_CONTROL_REQ;
+    hdr.result = 0;
+    i2s_cmd.command = MHB_I2S_COMMAND_ARM;
+    return device_mhb_send(dev, &hdr, (const uint8_t *)&i2s_cmd, sizeof(i2s_cmd), 0);
+}
+
+static int mhb_send_i2s_disarm(int argc, char *argv[], struct device *dev)
+{
+    struct mhb_hdr hdr;
+    struct mhb_i2s_control_req i2s_cmd;
+    (void)argc;
+    (void)argv;
+
+    hdr.addr = MHB_ADDR_I2S;
+    hdr.type = MHB_TYPE_I2S_CONTROL_REQ;
+    hdr.result = 0;
+    i2s_cmd.command = MHB_I2S_COMMAND_DISARM;
+    return device_mhb_send(dev, &hdr, (const uint8_t *)&i2s_cmd, sizeof(i2s_cmd), 0);
+}
+
+static int mhb_send_i2s_trigger(int argc, char *argv[], struct device *dev)
+{
+    struct mhb_hdr hdr;
+    struct mhb_i2s_control_req i2s_cmd;
+    (void)argc;
+    (void)argv;
+
+    hdr.addr = MHB_ADDR_I2S;
+    hdr.type = MHB_TYPE_I2S_CONTROL_REQ;
+    hdr.result = 0;
+    i2s_cmd.command = MHB_I2S_COMMAND_TRIGGER;
+    return device_mhb_send(dev, &hdr, (const uint8_t *)&i2s_cmd, sizeof(i2s_cmd), 0);
+}
+
+static int mhb_send_i2s_info(int argc, char *argv[], struct device *dev)
+{
+    struct mhb_hdr hdr;
+    (void)argc;
+    (void)argv;
+
+    hdr.addr = MHB_ADDR_I2S;
+    hdr.type = MHB_TYPE_I2S_STATUS_REQ;
+    hdr.result = 0;
+    return device_mhb_send(dev, &hdr, NULL, 0, 0);
+}
+
+static int mhb_subcmd_i2s(int argc, char *argv[], struct device *dev)
+{
+    static const struct command i2s_cmd_tb[] = {
+        { "enable",  mhb_send_i2s_enable  },
+        { "disable", mhb_send_i2s_disable },
+        { "config",  mhb_send_i2s_config  },
+        { "arm",     mhb_send_i2s_arm     },
+        { "disarm",  mhb_send_i2s_disarm  },
+        { "trigger", mhb_send_i2s_trigger },
+        { "info",    mhb_send_i2s_info    }
+    };
+
+    return mhb_handle_sub_cmd(argc, argv, dev, "i2s", i2s_cmd_tb, ARRAY_SIZE(i2s_cmd_tb));
+}
+
+static int mhb_send_pwr_on(int argc, char *argv[], struct device *dev)
+{
+    (void)argc;
+    (void)argv;
+    struct device *slave_pwr_ctrl;
+    int ret = -ENODEV;
+
+    slave_pwr_ctrl = device_open(DEVICE_TYPE_SLAVE_PWRCTRL_HW, MHB_ADDR_DIAG);
+    if (slave_pwr_ctrl != NULL) {
+        device_slave_pwrctrl_send_slave_state(slave_pwr_ctrl, 1);
+        device_close(slave_pwr_ctrl);
+        ret = OK;
+    }
+    return ret;
+}
+
+static int mhb_send_pwr_off(int argc, char *argv[], struct device *dev)
+{
+    (void)argc;
+    (void)argv;
+    struct device *slave_pwr_ctrl;
+    int ret = -ENODEV;
+
+    slave_pwr_ctrl = device_open(DEVICE_TYPE_SLAVE_PWRCTRL_HW, MHB_ADDR_DIAG);
+    if (slave_pwr_ctrl != NULL) {
+        device_slave_pwrctrl_send_slave_state(slave_pwr_ctrl, 0);
+        device_close(slave_pwr_ctrl);
+        ret = OK;
+    }
+    return ret;
+}
+
+static int mhb_subcmd_pwr(int argc, char *argv[], struct device *dev)
+{
+    static const struct command pwr_cmd_tb[] = {
+        { "on",   mhb_send_pwr_on  },
+        { "off",  mhb_send_pwr_off }
+    };
+
+    return mhb_handle_sub_cmd(argc, argv, dev, "pwr", pwr_cmd_tb, ARRAY_SIZE(pwr_cmd_tb));
+}
+
+static int mhb_send_diag_reg_log_apbe_req(int argc, char *argv[], struct device *dev)
+{
+    (void)argc;
+    (void)argv;
+
+    return mhb_send_diag_reg_log_apbe_req_na(dev);
+}
+
+static int mhb_send_diag_reg_log_fifo_req(int argc, char *argv[], struct device *dev)
+{
+    struct mhb_hdr hdr;
+    struct mhb_diag_mode_req diag_req;
+
+    (void)argc;
+    (void)argv;
+
+    hdr.addr = MHB_ADDR_DIAG;
+    hdr.type = MHB_TYPE_DIAG_CONTROL_REQ;
+    hdr.result = 0;
+    diag_req.mode = MHB_DIAG_CONTROL_REGLOG_FIFO;
+    return device_mhb_send(dev, &hdr, (const uint8_t *)&diag_req, sizeof(diag_req), 0);
+}
+
+static int mhb_send_diag_reg_log_stack_req(int argc, char *argv[], struct device *dev)
+{
+    struct mhb_hdr hdr;
+    struct mhb_diag_mode_req diag_req;
+
+    (void)argc;
+    (void)argv;
+
+    hdr.addr = MHB_ADDR_DIAG;
+    hdr.type = MHB_TYPE_DIAG_CONTROL_REQ;
+    hdr.result = 0;
+    diag_req.mode = MHB_DIAG_CONTROL_REGLOG_STACK;
+    return device_mhb_send(dev, &hdr, (const uint8_t *)&diag_req, sizeof(diag_req), 0);
+}
+
+static int mhb_subcmd_diag(int argc, char *argv[], struct device *dev)
+{
+    static const struct command pwr_cmd_tb[] = {
+        { "reglog",       mhb_send_diag_reg_log_apbe_req  },
+        { "reglog_fifo",  mhb_send_diag_reg_log_fifo_req  },
+        { "reglog_stack", mhb_send_diag_reg_log_stack_req },
+    };
+
+    return mhb_handle_sub_cmd(argc, argv, dev, "diag", pwr_cmd_tb, ARRAY_SIZE(pwr_cmd_tb));
 }
 
 int mhb_client_main(int argc, char *argv[])
 {
-    size_t i;
     const char *str;
+    const static struct command COMMANDS[] = { \
+        /* UniPro */
+        { "gear",   mhb_send_gear_req },
+        { "status", mhb_send_status_req },
+        { "read",   mhb_send_read_attr_req },
+        { "write",  mhb_send_write_attr_req },
+        /* Diag */
+        { "log",    mhb_send_diag_log_req },
+        { "mode",   mhb_send_diag_mode_req },
+        { "diag",   mhb_subcmd_diag },
+        /* I2S */
+        { "i2s",    mhb_subcmd_i2s },
+        /* Power control. */
+        { "pwr",    mhb_subcmd_pwr }
+    };
+
 
     if (argc == 1) {
-        usage(CONFIG_MODS_MHB_CLIENT_PROGNAME);
+        mhb_print_cmds("usage: " CONFIG_MODS_MHB_CLIENT_PROGNAME, COMMANDS, ARRAY_SIZE(COMMANDS), "\n");
         return -EAGAIN;
     }
 
@@ -364,16 +768,15 @@ int mhb_client_main(int argc, char *argv[])
 
         device_mhb_register_receiver(dev, MHB_ADDR_UNIPRO, mhb_handle_unipro);
         device_mhb_register_receiver(dev, MHB_ADDR_DIAG, mhb_handle_diag);
+        device_mhb_register_receiver(dev, MHB_ADDR_I2S, mhb_handle_i2s_rx);
         g_dev = dev;
     }
 
     str = argv[1];
-    for (i=0; i < ARRAY_SIZE(COMMANDS); i++) {
-        const struct command *command = &COMMANDS[i];
-        if (!strcmp(command->str, str)) {
-            printf(CONFIG_MODS_MHB_CLIENT_PROGNAME " %s:\n", str);
-            return command->fp(argc - 2, &argv[2], g_dev);
-        }
+    const struct command *command = mhb_find_cmd(str, COMMANDS, ARRAY_SIZE(COMMANDS));
+    if (command) {
+        printf(CONFIG_MODS_MHB_CLIENT_PROGNAME " %s:\n", str);
+        return command->fp(argc - 2, &argv[2], g_dev);
     }
 
     printf("ERROR: invalid command: %s\n", str);
