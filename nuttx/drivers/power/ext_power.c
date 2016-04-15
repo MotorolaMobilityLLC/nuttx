edf46d0b21dda4b3017cd003408bd6fbbb8a5cb8
Dmitriy Gruzman <dgruzman@motorola.com>
2016-04-15 14:12:56 -0500

    power: Add external power devices notifier
    Open DEVICE_TYPE_EXT_POWER_HW devices with the IDs in the range from 0
    to (EXT_POWER_MAX_NUMBER_OF_SOURCES - 1) and notify registered clients
    when state of any of them changes. Provide handles to the opened devices
    to the clients so they can read the new state.
    
    Change-Id: Iea78708baeb164505ce37bf297f5b2238b6bd57f
    Signed-off-by: Dmitriy Gruzman <dgruzman@motorola.com>
    Reviewed-on: http://gerrit.mot.com/845619
    SME-Granted: SME Approvals Granted
    Reviewed-by: Gregory Meiste <w30289@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Min Shi <e13386@motorola.com>
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Reviewed-by: James Wylder <jwylder@motorola.com>

---
+++ nuttx/drivers/power/ext_power.c
@@ -0,0 +1,128 @@
+/*
+ * Copyright (C) 2015 Motorola Mobility, LLC.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from this
+ * software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <debug.h>
+#include <errno.h>
+#include <semaphore.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <nuttx/device.h>
+#include <nuttx/device_ext_power.h>
+#include <nuttx/power/ext_power.h>
+#include <nuttx/list.h>
+
+static sem_t sem = SEM_INITIALIZER(1);
+static LIST_DECLARE(notify_list);
+static struct device *dev[EXT_POWER_NUMBER_OF_SOURCES];
+
+struct notify_node_s
+{
+    ext_power_notification callback;
+    void *arg;
+    struct list_head list;
+};
+
+static void ext_power_state_changed(void *arg)
+{
+    struct notify_node_s *node;
+    struct list_head *iter;
+
+    while (sem_wait(&sem) != OK) {
+        if (errno == EINVAL) {
+            return;
+        }
+    }
+
+    list_foreach(&notify_list, iter) {
+        node = list_entry(iter, struct notify_node_s, list);
+        node->callback(node->arg, dev);
+    }
+
+    sem_post(&sem);
+}
+
+static int ext_power_init(void)
+{
+    int i, j, retval;
+
+    for (i = 0; i < EXT_POWER_NUMBER_OF_SOURCES; i++) {
+        dev[i] = device_open(DEVICE_TYPE_EXT_POWER_HW, i);
+        if (dev[i]) {
+            retval = device_ext_power_register_callback(dev[i], ext_power_state_changed, NULL);
+            if (retval) {
+                dbg("failed to register callback for ext power source %d\n", i);
+                for (j = 0; j <= i; j++) {
+                    device_close(dev[i]);
+                    dev[i] = NULL;
+                }
+                return retval;
+            }
+        } else
+            dbg("external power source %d is not supported\n", i);
+    }
+
+    return 0;
+}
+
+int ext_power_register_callback(ext_power_notification callback, void *arg)
+{
+    static bool initialized = false;
+    struct notify_node_s *node;
+    int retval = 0;
+
+    while (sem_wait(&sem) != OK) {
+        if (errno == EINVAL) {
+            return -EINVAL;
+        }
+    }
+
+    if (!initialized) {
+        retval = ext_power_init();
+        if (retval)
+            goto register_done;
+        else
+            initialized = true;
+    }
+
+    node = zalloc(sizeof(*node));
+    if (!node) {
+        retval = -ENOMEM;
+        dbg("failed to allocate memory for node\n");
+        goto register_done;
+    }
+
+    node->callback = callback;
+    node->arg = arg;
+    list_add(&notify_list, &node->list);
+    callback(arg, dev);
+
+register_done:
+    sem_post(&sem);
+    return retval;
+}
