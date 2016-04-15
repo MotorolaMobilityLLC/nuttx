cdeb713c285deebcbac262260ca4ce2f2fc336fc
Dmitriy Gruzman <dgruzman@motorola.com>
2016-04-15 13:26:55 -0500

    nuttx: device_ext_power: Do not report device current in callback
    Instead of reporting device maximum current output in the callback,
    add method to get the value. This will allow anyone with the device
    handle to get the value, which was not possible before since only
    one callback could be registered with the device.
    
    Change-Id: I7028eeb25bf31b184d8482073698523131122d96
    Signed-off-by: Dmitriy Gruzman <dgruzman@motorola.com>
    Reviewed-on: http://gerrit.mot.com/845615
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Reviewed-by: James Wylder <jwylder@motorola.com>
    Reviewed-by: Gregory Meiste <w30289@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>

--- nuttx/include/nuttx/device_ext_power.h
+++ nuttx/include/nuttx/device_ext_power.h
@@ -30,38 +30,36 @@
 #define __INCLUDE_NUTTX_DEVICE_EXT_POWER_H
 
 #include <nuttx/device.h>
-
 #include <errno.h>
-#include <stdbool.h>
 
 #define DEVICE_TYPE_EXT_POWER_HW  "ext_power"
 
-/** The type of the external power changed callback function
+/** External power device state changed notification callback
  *
- * @param arg user provided value that will be returned in callback
- * @param current external power source max current output in mA.
+ * @param arg value provided during callback registration
  */
-typedef void (*ext_power_changed)(void *arg, int current);
+typedef void (*device_ext_power_notification)(void *arg);
 
 /**
- * external power device driver operations
+ * External power device driver operations
  */
 struct device_ext_power_type_ops {
-    /** Register for external power changes notifications */
-    int (*register_callback)(struct device *dev, ext_power_changed cb, void *arg);
+    /** Register for external power state change notifications */
+    int (*register_callback)(struct device *dev, device_ext_power_notification cb, void *arg);
+    /** Get maximum current output */
+    int (*get_current)(struct device *dev, int *current)
 };
 
 /**
- * @brief Register external power changed notification callback
+ * @brief Register for external power device state change notifications
  *
  * @param dev pointer to structure of device data
- * @param callback callback function to notify when external power changes
- * @param arg user provided value that will be returned in callback
+ * @param cb function to run when external power device state changes
+ * @param arg value that will be returned in callback
  * @return 0 on success, negative errno on error
  */
 static inline int device_ext_power_register_callback(struct device *dev,
-                                                     ext_power_changed cb,
-                                                     void* arg)
+                             device_ext_power_notification cb, void *arg)
 {
     DEVICE_DRIVER_ASSERT_OPS(dev);
 
@@ -74,5 +72,27 @@ static inline int device_ext_power_register_callback(struct device *dev,
     }
 
     return DEVICE_DRIVER_GET_OPS(dev, ext_power)->register_callback(dev, cb, arg);
+}
+
+/**
+ * @brief Get maximum output current of external power device
+ *
+ * @param dev pointer to structure of device data
+ * @param current maximum output current in mA
+ * @return 0 on success, negative errno on error
+ */
+static inline int device_ext_power_get_current(struct device *dev, int *current)
+{
+    DEVICE_DRIVER_ASSERT_OPS(dev);
+
+    if (!device_is_open(dev)) {
+        return -ENODEV;
+    }
+
+    if (!DEVICE_DRIVER_GET_OPS(dev, ext_power)->get_current) {
+        return -ENOSYS;
+    }
+
+    return DEVICE_DRIVER_GET_OPS(dev, ext_power)->get_current(dev, current);
 }
 #endif
