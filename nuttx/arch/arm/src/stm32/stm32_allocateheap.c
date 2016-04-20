MotorolaMobilityLLC/nuttx

commit 0759c510eb8dcfe30788b87fd1eb0b321738aa80

arm: stm32f446: don't register invalid CCM ram bank
The STM32F446 does not include the CCM RAM bank, but this bank is always
registered as heap even when the chip does not have it.
Check that the chip has the CCM ram bank before registering it.

This issue caused crashed in the SVC and erroneous -ENOMEM errors.

Testing Done: svc not crashing anymore.
Bug Number: trivial
Review URL: https://reviews.leaflabs.com/r/1935/
Reviewed by: mbolivar, mgreer, jpihet

Signed-off-by: Fabien Parent <fparent@baylibre.com>

nuttx/arch/arm/src/stm32/stm32_allocateheap.c
@@ -561,7 +561,7 @@ void up_allocate_kheap(FAR void **heap_start, size_t *heap_size)
 #if CONFIG_MM_REGIONS > 1
 void up_addregion(void)
 {
-#ifndef CONFIG_STM32_CCMEXCLUDE
+#if defined(CONFIG_STM32_HAVE_CCM) && !defined(CONFIG_STM32_CCMEXCLUDE)
 #if defined(CONFIG_BUILD_PROTECTED) && defined(CONFIG_MM_KERNEL_HEAP)
 
   /* Allow user-mode access to the STM32F20xxx/STM32F40xxx CCM SRAM heap */

commit 103791b479aa781d1c974efc84d2973c984a729e

arm: stm32: allocateheap: Restore SRAM2 support
Commit 0759c510 broke SRAM2 support on chips without CCM RAM.
Instead of setting CONFIG_STM32_CCMEXCLUDE for the STM32F446 as
Nuttx expects, the SRAM2 code was conditionally compiled with
CONFIG_STM32_HAVE_CCM, which breaks chips that have SRAM2 that
is not considered CCM.

This patch reverts the change made in commit 0759c510 and sets
CONFIG_STM32_CCMEXCLUDE for the STM32F446.

Change-Id: I12107f697ca5f588cf9734f78a078fd6ffee3343
Signed-off-by: Greg Meiste <w30289@motorola.com>
Reviewed-on: http://gerrit.mot.com/812686
SME-Granted: SME Approvals Granted
SLTApproved: Slta Waiver <sltawvr@motorola.com>
Submit-Approved: Jira Key <jirakey@motorola.com>
Tested-by: Jira Key <jirakey@motorola.com>
Reviewed-by: James Wylder <jwylder@motorola.com>

nuttx/arch/arm/src/stm32/stm32_allocateheap.c
@@ -320,9 +320,10 @@
 
 #elif defined(CONFIG_STM32_STM32F20XX) || defined(CONFIG_STM32_STM32F40XX)
 
-   /* The STM32 F2 and the STM32 F401 have no CCM SRAM */
+   /* The STM32 F2, STM32 F401 and the STM32 F466 have no CCM SRAM */
 
-#  if defined(CONFIG_STM32_STM32F20XX) || defined(CONFIG_STM32_STM32F401)
+#  if defined(CONFIG_STM32_STM32F20XX) || defined(CONFIG_STM32_STM32F401) || \
+      defined(CONFIG_STM32_STM32F446)
 #    undef CONFIG_STM32_CCMEXCLUDE
 #    define CONFIG_STM32_CCMEXCLUDE 1
 #  endif
@@ -631,7 +632,7 @@ void up_allocate_kheap(FAR void **heap_start, size_t *heap_size)
 #if CONFIG_MM_REGIONS > 1
 void up_addregion(void)
 {
-#if defined(CONFIG_STM32_HAVE_CCM) && !defined(CONFIG_STM32_CCMEXCLUDE)
+#ifndef CONFIG_STM32_CCMEXCLUDE
 #if defined(CONFIG_BUILD_PROTECTED) && defined(CONFIG_MM_KERNEL_HEAP)
 
   /* Allow user-mode access to the STM32F20xxx/STM32F40xxx CCM SRAM heap */