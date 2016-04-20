MotorolaMobilityLLC/nuttx

commit 2169fff4c9f8bd81b438214c6a3537e68bee02b2

time: use DIV_ROUND_CLOSEST() to avoid rounding errors
Use the recently introduced DIV_ROUND_CLOSEST() macro in various time
helpers in order to avoid rounding errors.

Testing Done: trivial
Bug Number: trivial
Review URL: https://reviews.leaflabs.com/r/2090/
Reviewed by: Marti Bolivar

Signed-off-by: Bartosz Golaszewski <bgolaszewski@baylibre.com>

nuttx/include/nuttx/time.h
@@ -42,6 +42,7 @@
 
 #include <nuttx/config.h>
 #include <nuttx/clock.h>
+#include <nuttx/util.h>
 
 #include <stdbool.h>
 #include <time.h>
@@ -139,7 +140,7 @@ static inline useconds_t msec_to_usec(unsigned ms)
  ****************************************************************************/
 static inline useconds_t timespec_to_usec(const struct timespec *ts)
 {
-    return (ts->tv_sec * USEC_PER_SEC) + (ts->tv_nsec / NSEC_PER_USEC);
+    return (ts->tv_sec * USEC_PER_SEC) + DIV_ROUND_CLOSEST(ts->tv_nsec, NSEC_PER_USEC);
 }
 
 /****************************************************************************
@@ -175,7 +176,7 @@ static inline useconds_t timeval_to_usec(const struct timeval *tv)
  ****************************************************************************/
 static inline void usec_to_timespec(uint32_t usec, struct timespec *ts)
 {
-    ts->tv_sec = usec / USEC_PER_SEC;
+    ts->tv_sec = DIV_ROUND_CLOSEST(usec, USEC_PER_SEC);
     ts->tv_nsec = (usec % USEC_PER_SEC) * NSEC_PER_USEC;
 }
 
@@ -188,7 +189,7 @@ static inline void usec_to_timespec(uint32_t usec, struct timespec *ts)
  ****************************************************************************/
 static inline void nsec_to_timespec(uint64_t ns, struct timespec *ts)
 {
-    ts->tv_sec = ns / NSEC_PER_SEC;
+    ts->tv_sec = DIV_ROUND_CLOSEST(ns, NSEC_PER_SEC);
     ts->tv_nsec = ns % NSEC_PER_SEC;
 }
 

commit 2525b12ec760141886cd0d76cfdc718cc862414d

time: Fix time conversion functions broken by commit 2169fff4
This patch partially reverts commit 2169fff4. These time
conversion functions cannot be changed to round the seconds up
since the remainder can be found in the nsec field of the same
structure.

For example:
12687753819170 nanoseconds != 12688 seconds, 753819170 nanoseconds

Change-Id: I38844719142936a40a9dd93b67942d4032064826
Signed-off-by: Greg Meiste <w30289@motorola.com>
Reviewed-on: http://gerrit.mot.com/812253
SLTApproved: Slta Waiver <sltawvr@motorola.com>
SME-Granted: SME Approvals Granted
Submit-Approved: Jira Key <jirakey@motorola.com>
Tested-by: Jira Key <jirakey@motorola.com>
Reviewed-by: James Wylder <jwylder@motorola.com>

nuttx/include/nuttx/time.h
@@ -176,7 +176,7 @@ static inline useconds_t timeval_to_usec(const struct timeval *tv)
  ****************************************************************************/
 static inline void usec_to_timespec(uint32_t usec, struct timespec *ts)
 {
-    ts->tv_sec = DIV_ROUND_CLOSEST(usec, USEC_PER_SEC);
+    ts->tv_sec = usec / USEC_PER_SEC;
     ts->tv_nsec = (usec % USEC_PER_SEC) * NSEC_PER_USEC;
 }
 
@@ -189,7 +189,7 @@ static inline void usec_to_timespec(uint32_t usec, struct timespec *ts)
  ****************************************************************************/
 static inline void nsec_to_timespec(uint64_t ns, struct timespec *ts)
 {
-    ts->tv_sec = DIV_ROUND_CLOSEST(ns, NSEC_PER_SEC);
+    ts->tv_sec = ns / NSEC_PER_SEC;
     ts->tv_nsec = ns % NSEC_PER_SEC;
 }
 