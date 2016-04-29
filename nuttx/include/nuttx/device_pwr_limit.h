8bbd99b4403c693535ade0a4b3760f3863517b90
Jim Wylder <jwylder@motorola.com>
2016-04-29 15:36:41 -0500

    device_pwr_limit: Add Current Reservation Message
    Add message to allow the mod to notify the base that
    they are going to use the full power (above 1A). This
    should only be sent when the pwr_limit is set above
    the 1A limit.  If the mod is using the higher limit
    it should wait until the current_rsv_ack message
    is received before pulling the full current.  If
    release the current reservation, it is expected the
    mod is already lowered it's usage.
    
    The core should not reject this reservation, it has
    already granted the power limit though other means.
    This is only a notification.
    
    Change-Id: I004d8105ded513c66ead14bd7d1fcfd6bc342a70
    Signed-off-by: Jim Wylder <jwylder@motorola.com>
    Reviewed-on: http://gerrit.mot.com/850649
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Reviewed-by: Gregory Meiste <w30289@motorola.com>

--- nuttx/include/nuttx/device_pwr_limit.h
+++ nuttx/include/nuttx/device_pwr_limit.h
@@ -53,12 +53,20 @@ typedef int (*pwr_limit_send_capability_change_cb)(
     uint8_t reason,
     uint16_t vendor);
 
+typedef int (*pwr_limit_current_rsv_cb)(
+    struct device *dev,
+    uint8_t rsv);
+
 struct device_pwr_limit_type_ops {
     int (*set_limit)(struct device *dev, uint8_t lmt);
     uint8_t (*get_limit)(struct device *dev);
+    void (*current_rsv_ack)(struct device *dev, uint8_t rsv);
     int (*register_capability_change_cb)(struct device *dev,
             pwr_limit_send_capability_change_cb cb);
     int (*unregister_capability_change_cb)(struct device *dev);
+    int (*register_current_rsv_cb)(struct device *dev,
+            pwr_limit_current_rsv_cb cb);
+    int (*unregister_current_rsv_cb)(struct device *dev);
 };
 
 /**
@@ -155,4 +163,71 @@ device_pwr_limit_unregister_capability_change_cb(struct device *dev)
             ->unregister_capability_change_cb(dev);
 }
 
+/**
+ * @brief Power Limit Register Current Reservation
+ *
+ * @param dev pointer to structure of device data
+ * @param callback function
+ * @return 0 on success, negative errno on error
+ */
+static inline int
+device_pwr_limit_register_current_rsv(struct device *dev,
+        pwr_limit_current_rsv_cb cb)
+{
+    DEVICE_DRIVER_ASSERT_OPS(dev);
+
+    if (!device_is_open(dev)) {
+        return -ENODEV;
+    }
+
+    if (!DEVICE_DRIVER_GET_OPS(dev, pwr_limit)->register_current_rsv_cb) {
+        return -ENOSYS;
+    }
+
+    return DEVICE_DRIVER_GET_OPS(dev, pwr_limit)->register_current_rsv_cb(dev, cb);
+}
+
+/**
+ * @brief Power Limit Unregister Capability Change Callback
+ *
+ * @param dev pointer to structure of device data
+ * @return 0 on success, negative errno on error
+ */
+static inline int
+device_pwr_limit_unregister_current_rsv(struct device *dev)
+{
+    DEVICE_DRIVER_ASSERT_OPS(dev);
+
+    if (!device_is_open(dev)) {
+        return -ENODEV;
+    }
+
+    if (!DEVICE_DRIVER_GET_OPS(dev, pwr_limit)->unregister_current_rsv_cb) {
+        return -ENOSYS;
+    }
+
+    return DEVICE_DRIVER_GET_OPS(dev, pwr_limit)->unregister_current_rsv_cb(dev);
+}
+
+/**
+ * @brief Power Limit Current Reservation Complete
+ *
+ * @param dev pointer to structure of device data
+ * @param limit level
+ * @return 0 on success, negative errno on error
+ */
+static inline void device_pwr_limit_current_rsv_ack(struct device *dev, uint8_t rsv)
+{
+    DEVICE_DRIVER_ASSERT_OPS(dev);
+
+    if (!device_is_open(dev)) {
+        return -ENODEV;
+    }
+
+    if (!DEVICE_DRIVER_GET_OPS(dev, pwr_limit)->current_rsv_ack) {
+        return -ENOSYS;
+    }
+
+    DEVICE_DRIVER_GET_OPS(dev, pwr_limit)->current_rsv_ack(dev, rsv);
+}
 #endif
