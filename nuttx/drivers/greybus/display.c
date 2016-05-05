8ecc8b0cbc1b0497a753e8b176f0b203dbd91b7c
Jim Wylder <jwylder@motorola.com>
2016-05-05 17:10:36 -0500

    greybus: Update Copyright
    Change-Id: I54a5993ede1e304f46dabeacf816dd9b475fd993
    Signed-off-by: Jim Wylder <jwylder@motorola.com>
    Reviewed-on: http://gerrit.mot.com/853070
    SME-Granted: SME Approvals Granted
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    SLTApproved: Slta Waiver <sltawvr@motorola.com>

--- nuttx/drivers/greybus/display.c
+++ nuttx/drivers/greybus/display.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015 Motorola, LLC.
+ * Copyright (c) 2015-2016 Motorola Mobility, LLC.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
