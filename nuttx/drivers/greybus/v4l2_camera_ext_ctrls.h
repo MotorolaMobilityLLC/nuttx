d54a10ff9e841247063de246cc17e55310773754
Mao Bing <mbing@motorola.com>
2016-05-08 15:30:23 -0500

    driver: camera_ext: update v4l2_camera_ext_ctrls.h
    add control for shutter lock
    add control for model number
    add control for firmware version
    add extension AE control
        for AE off with flash on
    add externsion scene control
        for AUTO HDR
    
    Change-Id: I91120251796ea90052bb30999dcb47b87f69963e
    Reviewed-on: http://gerrit.mot.com/853920
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Kazuhiro Ondo <kazuhiro.ondo@motorola.com>

--- nuttx/drivers/greybus/v4l2_camera_ext_ctrls.h
+++ nuttx/drivers/greybus/v4l2_camera_ext_ctrls.h
@@ -1,29 +1,14 @@
 /*
- * Copyright (c) 2015-16, Motorola Mobility LLC
- * All rights reserved.
+ * Copyright (c) 2015-16 Motorola Mobility, LLC.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *     - Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     - Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     - Neither the name of Motorola Mobility nor the
- *       names of its contributors may be used to endorse or promote products
- *       derived from this software without specific prior written permission.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL MOTOROLA MOBILITY LLC BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
- * DAMAGE.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  */
 
 #ifndef __CAMERA_EXT_CTRLS_H
@@ -737,5 +722,33 @@ enum {
 
 /* boolean */
 #define CAM_EXT_CID_ZOOM_LOCK_1X (CID_CAM_EXT_CLASS_BASE + 105)
+
+/* boolean */
+#define CAM_EXT_CID_SHUTTER_LOCK (CID_CAM_EXT_CLASS_BASE + 106)
+
+/* string read only */
+#define CAM_EXT_CID_MODEL_NUMBER (CID_CAM_EXT_CLASS_BASE + 107)
+
+/* string read only */
+#define CAM_EXT_CID_FIRMWARE_VERSION (CID_CAM_EXT_CLASS_BASE + 108)
+
+/* int menu */
+#define CAM_EXT_CID_AE_MODE_EXT (CID_CAM_EXT_CLASS_BASE + 109)
+enum {
+	CAM_EXT_AE_MODE_EXT_NULL,
+	CAM_EXT_AE_MODE_EXT_OFF_FLASH,
+	CAM_EXT_AE_MODE_EXT_OFF_FLASH_REDEYE,
+
+	CAM_EXT_AE_MODE_EXT_MAX = CAM_EXT_AE_MODE_EXT_OFF_FLASH_REDEYE,
+};
+
+/* int menu */
+#define CAM_EXT_CID_SCENE_MODE_EXT (CID_CAM_EXT_CLASS_BASE + 110)
+enum {
+	CAM_EXT_SCENE_MODE_EXT_NULL,
+	CAM_EXT_SCENE_MODE_EXT_AUTO_HDR,
+
+	CAM_EXT_SCENE_MODE_EXT_MAX = CAM_EXT_SCENE_MODE_EXT_AUTO_HDR,
+};
 
 #endif /* __CAMERA_EXT_CTRLS_H */
