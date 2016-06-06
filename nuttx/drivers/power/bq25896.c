aa5313a9fd73cce41aa78d6e84a8252c1d6f84f2
Greg Meiste <w30289@motorola.com>
2016-06-06 15:04:36 -0500

    power: bq24292/bq25896: Filter out momentary boost faults
    When switching the charge direction, momentary boost faults are
    seen. Previously, this boost fault was immediately reported to the
    system, which resulting in the charge current being shut off.
    
    With this patch, a boost fault needs to be seen for 100ms before
    it is reported. If greater than 100ms, it is likely to be a real
    fault.
    
    Unfortunately, there is no interrupt generated when the boost
    fault condition is cleared, so the usual debouncing method will
    not work.
    
    Change-Id: I4f676782b901014c0f17ff4ab22d661ff2c8cdf8
    Signed-off-by: Greg Meiste <w30289@motorola.com>
    Reviewed-on: http://gerrit.mot.com/864577
    SME-Granted: SME Approvals Granted
    Reviewed-by: Dmitriy Gruzman <dgruzman@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Reviewed-by: Weiqi Tang <woodtang@motorola.com>

--- nuttx/drivers/power/bq25896.c
+++ nuttx/drivers/power/bq25896.c
@@ -55,10 +55,13 @@ static LIST_DECLARE(notify_list);
 static sem_t sem = SEM_INITIALIZER(1);
 static struct work_s int_work;
 static struct work_s pg_work;
+static struct work_s fault_work;
 
 static bool pg_n_connected;
 
 static FAR struct i2c_dev_s *i2c;
+
+#define BQ25896_FAULT_DELAY         MSEC2TICK(100)
 
 int bq25896_register_callback(bq25896_callback cb, void *arg)
 {
@@ -88,6 +91,35 @@ static void pg_worker(FAR void *arg)
     }
 }
 
+static int read_fault_reg(void)
+{
+    /*
+     * From the spec:
+     * "To read the current fault status, the host has to read REG0C two times
+     * consecutively. The 1st read reports the pre-existing fault register
+     * status and the 2nd read reports the current fault register status."
+     */
+    (void) bq25896_reg_read(BQ25896_REG0C);
+    return bq25896_reg_read(BQ25896_REG0C);
+}
+
+static void fault_worker(FAR void *arg)
+{
+    struct notify_node *node;
+    struct list_head *iter;
+    int regval;
+
+    regval = read_fault_reg();
+    if (regval > 0 && regval & BQ25896_REG0C_BOOST_FAULT) {
+        list_foreach(&notify_list, iter) {
+            node = list_entry(iter, struct notify_node, list);
+            node->callback(BOOST_FAULT, node->arg);
+        }
+    } else {
+        dbg("fault gone\n");
+    }
+}
+
 static void int_worker(FAR void *arg)
 {
     struct notify_node *node;
@@ -95,11 +127,12 @@ static void int_worker(FAR void *arg)
     int regval;
 
     /* Boost fault */
-    regval = bq25896_reg_read(BQ25896_REG0C);
+    regval = read_fault_reg();
     if (regval > 0 && regval & BQ25896_REG0C_BOOST_FAULT) {
-        list_foreach(&notify_list, iter) {
-            node = list_entry(iter, struct notify_node, list);
-            node->callback(BOOST_FAULT, node->arg);
+        vdbg("possible fault\n");
+        if (work_available(&fault_work)) {
+            work_queue(LPWORK, &fault_work, fault_worker, NULL,
+                       BQ25896_FAULT_DELAY);
         }
     }
 
