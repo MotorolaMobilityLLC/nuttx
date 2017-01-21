73176f119e4ac4b1335f7317d1c6ecd6e067f6bd
Jim Wylder <jim@wylder.us>
2017-01-21 11:14:30 -0600

    Update stm32_usbext_usb2.c

--- nuttx/configs/hdk/muc/src/stm32_usbext_usb2.c
+++ nuttx/configs/hdk/muc/src/stm32_usbext_usb2.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 Motorola Mobility, LLC.
+ * Copyright (c) 2017 Motorola Mobility, LLC.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
